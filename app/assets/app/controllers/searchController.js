8e8e50eecf6179e960cdb3b6bcaa3f58d05f9d90
glopez <glopez@fluid.la>
2016-08-31 17:38:50 -0500

    se implemento la modal para ver hallazgos

--- app/assets/app/controllers/searchController.js
+++ app/assets/app/controllers/searchController.js
@@ -1,10 +1,61 @@
-
+integrates.vulnbynameFormatter = function(value, row, index){
+    return '<div class="btn-group">' +
+                '<button type="button" class="btn btn-default btnVer" ng-click="openModalVer('+index+')"><i class="glyphicon glyphicon-eye-open"></i></button>' +
+                '<button type="button" class="btn btn-primary"><i class="glyphicon glyphicon-pencil"></i></button>' +
+                '<button type="button" class="btn btn-warning"><i class="glyphicon glyphicon-trash"></i></button>' +
+           '</div>';
+};
 integrates.controller("searchController", function($scope,$uibModal, searchFactory) {
 
     $scope.init = function(){
         $("#search_section").hide();
         $(".loader").hide();
     }
+
+    $scope.openModalVer = function(){
+        var sel = $("#vulnerabilities").bootstrapTable('getSelections');
+        if(sel.length == 0){
+            $.gritter.add({
+                title: 'Error',
+                text: 'Debes seleccionar un hallazgo',
+                class_name: 'color warning',
+                    sticky: false,
+            });
+            return false;
+        }else{
+            $scope.currentVulnerability = sel[0];
+        }
+        //console.log($scope.currentVulnerability);
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'ver.html',
+            windowClass: 'ver-modal',
+            controller: function($scope, $uibModalInstance, currentVulnerability){
+                $scope.vuln = currentVulnerability;
+                /*
+                if($scope.vuln.amenaza == undefined) $scope.vuln.amenaza = "";
+                if($scope.vuln.riesgo == undefined) $scope.vuln.riesgo = "";*/
+                if($scope.vuln.nivel == "General"){
+                    $scope.esDetallado = "hide-detallado";
+                    $scope.rows = "4";
+                    $scope.cols = "12";
+                }else{
+                    $scope.esDetallado = "show-detallado";  
+                    $scope.rows = "3";
+                    $scope.cols = "6";
+                }
+                
+                $scope.closeModalAvance = function(){
+                    $uibModalInstance.dismiss('cancel');
+                }
+            },
+            resolve: {
+                currentVulnerability: function(){
+                    return $scope.currentVulnerability;
+                }
+            }
+        });
+    };
 
     $scope.openModalAvance = function(){
         var modalInstance = $uibModal.open({
@@ -21,7 +72,6 @@ integrates.controller("searchController", function($scope,$uibModal, searchFacto
                 ok: true
             }
         });
-    
     };
 
     $scope.searchVulnByName = function(){
@@ -39,11 +89,16 @@ integrates.controller("searchController", function($scope,$uibModal, searchFacto
                 if(data.error == false){
                     var cardinalidad = 0; 
                     data.data.forEach(function(i){ cardinalidad+= parseInt(i.cardinalidad); });
+                    
+                    //CONFIGURACION DE TABLA
+                    $("#vulnerabilities").bootstrapTable('destroy');
+                    $("#vulnerabilities").bootstrapTable(data);
+                    $("#vulnerabilities").bootstrapTable('refresh');
+                    //MANEJO DEL UI
                     $("#total_cardinalidad").html(cardinalidad);
                     $("#total_hallazgos").html(data.data.length);
                     $("#search_section").show();
-                    $("#vulnerabilities").bootstrapTable(data);
-                    $("#vulnerabilities").bootstrapTable('refresh');
+                    $('[data-toggle="tooltip"]').tooltip(); 
                 }else{
                     if (data.message == "Project doesn't exist"){
                         $.gritter.add({
