4e16c139db2bf507d58c26cb640327781d29796e
glopezatfluid <glopez@fluid.la>
2016-12-22 15:04:58 -0500

    Se arreglaron los test

--- app/tests.py
+++ app/tests.py
@@ -5,55 +5,55 @@ from app.models import FormstackAPI
 class FormstackAPITests(TestCase):
 
     def test_request(self):
-        """ Realiza una peticion a formstack y verifica 
+        """ Realiza una peticion a formstack y verifica
             que exista el key data en el json de respuesta """
-        API = FormstackAPI()
+        api_frms = FormstackAPI()
         url = "https://www.formstack.com/api/v2/submission/293276999.json"
-        request = API.request("GET", url)
+        request = api_frms.request("GET", url)
         self.assertIs("data" in request, True)
 
     def test_get_submission(self):
         """ Prueba que se consulte correctamente una submission de formstack """
-        API = FormstackAPI()
+        api_frms = FormstackAPI()
         submission_id = "293276999"
-        request = API.get_submission(submission_id)
+        request = api_frms.get_submission(submission_id)
         self.assertEquals(request["id"], submission_id)
 
     def test_get_findings(self):
-        API = FormstackAPI()
+        """ Prueba que se consulte correctamente
+            los hallazgos de un proyecto """
+        api_frms = FormstackAPI()
         project = "basaiti"
-        request = API.get_findings(project)
-        self.assertIs("submissions" in request, True)
-    
-    def test_get_eventualities(self):
-        API = FormstackAPI()
-        project = "basaiti"
-        request = API.get_eventualities(project)
+        request = api_frms.get_findings(project)
         self.assertIs("submissions" in request, True)
 
     def test_get_eventualities(self):
-        API = FormstackAPI()
+        """ Prueba que se consulte correctamente
+            las eventualidades de un proyecto """
+        api_frms = FormstackAPI()
+        project = "basaiti"
+        request = api_frms.get_eventualities(project)
+        self.assertIs("submissions" in request, True)
+
+    def test_get_order(self):
+        """ Obtiene el pedido de un nombre de proyecto """
+        api_frms = FormstackAPI()
         project = "oka"
-        request = API.get_order(project)
+        request = api_frms.get_order(project)
         self.assertIs("submissions" in request, True)
 
     def test_update_order(self):
-        API = FormstackAPI()
+        """ Actualiza un pedido en Formstack """
+        api_frms = FormstackAPI()
         project = "Bramley"
         submission_id = "223360928"
-        request = API.get_order(project, submission_id)
+        request = api_frms.update_order(project, submission_id)
         self.assertIs("success" in request, True)
 
     def test_update_eventuality(self):
-        API = FormstackAPI()
+        """ Actualiza la afectacion de una eventualidad en Formstack """
+        api_frms = FormstackAPI()
         afectacion = "0"
         submission_id = "244210431"
-        request = API.get_order(project, submission_id)
-        self.assertIs("success" in request, True)
-
-    def test_update_finding(self):
-        API = FormstackAPI()
-        afectacion = "0"
-        submission_id = "244210431"
-        request = API.get_order(project, submission_id)
+        request = api_frms.update_eventuality(afectacion, submission_id)
         self.assertIs("success" in request, True)
