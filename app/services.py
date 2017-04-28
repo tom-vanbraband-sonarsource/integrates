ebcaeb304d0654172a190fb8da67609b12e1b270
Andres Roldan <aroldan@fluid.la>
2017-04-28 15:49:47 -0500

    Added mock user

--- app/services.py
+++ app/services.py
@@ -18,8 +18,18 @@ def login(request):
         username = fmanager.post(request, "user")
         password = fmanager.post(request, "pass")
         if not OneLoginAPI(username, password).login():
-            fmanager.error(100)
+            # FIXME: This is only for testing purposes
+            test_user = 'customer@bancolombia.com.co'
+            test_pass = 'uso4Suzi'
+            test_company = 'Bancolombia'
+            test_role = 'admin'
+            if username == test_user and password == test_pass:
+                pass
+            else:
+                fmanager.error(100)
     except (SecureParamsException, LogicException) as expt:
         return util.response([], str(expt), True)
     request.session['username'] = username
+    request.session['company'] = test_company
+    request.session['role'] = test_role
     return util.response([], 'Bienvenido ' + username, False)
