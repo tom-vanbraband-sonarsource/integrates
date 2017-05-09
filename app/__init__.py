8c2a7fa91e089c0290a24cedf0eaec1bae763d4a
Andres Roldan <aroldan@fluid.la>
2017-05-09 10:27:51 -0500

    Run prospector with custon pylintrc file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,1 @@
-"""
-    Configuracion de logs para Integrates
-"""
-"""
-import logging
 
-logging.basicConfig(
-    filename='/var/www/fluid-integrates/logs/error.err',
-    format='%(levelname)s: %(asctime)s %(message)s',
-    level=logging.DEBUG
-)
-"""
