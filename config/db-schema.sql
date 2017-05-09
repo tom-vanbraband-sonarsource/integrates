67a9a4ef3364a1f7cd237c4f3184178721965b9d
Andres Roldan <aroldan@fluid.la>
2017-05-09 16:04:42 -0500

    Fixed schema.

--- config/db-schema.sql
+++ config/db-schema.sql
@@ -5,6 +5,7 @@ USE fluidintegrates;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     username varchar(255) DEFAULT NULL,
+    registered char(1) DEFAULT 'N',
     last_name varchar(255) DEFAULT NULL,
     first_name varchar(255) DEFAULT NULL,
     email varchar(255) NOT NULL,
@@ -14,11 +15,11 @@ CREATE TABLE users (
 
 CREATE TABLE roles (
     user_id INT NOT NULL,
-    role varchar(255) NOT NULL,
+    role varchar(255) NOT NULL
 ) ENGINE=INNODB;
 
 CREATE TABLE projects (
     user_id INT NOT NULL,
-    project varchar(255) NOT NULL,
+    project varchar(255) NOT NULL
 ) ENGINE=INNODB;
 
