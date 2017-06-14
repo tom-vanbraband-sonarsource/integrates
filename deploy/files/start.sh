cf355af49351d9829d72311166ca86fd8fd09879
Andres Roldan <aroldan@fluid.la>
2017-06-14 13:16:21 -0500

    Use FLUID certificate to deploy integrates

--- deploy/files/start.sh
+++ deploy/files/start.sh
@@ -2,4 +2,4 @@
 
 echo "Starting FLUIDIntegrates"
 INTEGRATES_PATH="/usr/src/app"
-exec python ${INTEGRATES_PATH}/manage.py runsslserver 0.0.0.0:8000
+exec python ${INTEGRATES_PATH}/manage.py runsslserver 0.0.0.0:8000 --certificate ${INTEGRATES_PATH}/config/fluidla.crt --key ${INTEGRATES_PATH}/config/fluidla.key
