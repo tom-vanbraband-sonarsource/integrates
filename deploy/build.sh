c8b6fa346b1afa7fe5f2e24a90eed197044da391
jrestrepoatfluid <jrestrepo@fluid.la>
2017-06-16 14:20:49 -0500

    - Circle v2.0 - Serverspec tests

---
+++ deploy/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# habilitar depuración
+if [ -n "$VERBOSE" ]; then
+  set -x
+fi
+
+# Salir inmediatamente si algun comando retorna diferente de cero.
+set -e
+
+SERVER="integrates"
+
+# Mensaje de inicio
+echo "---### [${SERVER}] Compilando contenedor."
+
+# construir la imagen
+cp ~/.vault.txt containers/${SERVER}/
+sudo docker build -t technologyatfluid/integrates:$CIRCLE_BRANCH deploy/
+rm deploy/.vault.txt
+docker run --detach --name="$SERVER" -p 8000:443 technologyatfluid/integrates:$CIRCLE_BRANCH
