af9ed65c682f0e595be3f9640f75b4e423e519a6
German Lopez <glopez@fluid.la>
2017-07-12 14:43:45 -0700

    Add cssv2 for update findings

---
+++ app/assets/app/CSSV2.js
@@ -0,0 +1,104 @@
+var attackVector = [{
+    value: "0.646",
+    name: "Red adyacente: Explotable desde el mismo segmento de red"
+}, {
+    value: "1.000",
+    name: "Red: Explotable desde Internet"
+}, {
+    value: "0.395",
+    name: "Local: Explotable con acceso local al objetivo"
+}];
+
+var accessComplexity = [{
+    value: "0.350",
+    name: "Alto: Se requieren condiciones especiales como acceso administrativo"
+},{
+    value: "0.610",
+    name: "Medio: Se requieren algunas condiciones como acceso al sistema"
+},{
+    value: "0.710",
+    name: "Bajo: No se requiere ninguna condición especial"
+}];
+
+var authentication = [{
+    name: "Ninguna: No se requiere autenticación",
+    value: "0.704"
+}, {
+    name: "Única: Único punto de autenticación",
+    value: "0.560"
+}, {
+    name: "Multiple: Multiples puntos de autenticación",
+    value: "0.450"
+}];
+
+var confidenciality = [{
+    name: "Ninguno: No se presenta ningún impacto",
+    value: "0"
+}, {
+    name: "Parcial: Se obtiene acceso a la información pero no control sobre ella",
+    value: "0.275"
+}, {
+    name: "Completo: Se controla toda la información relacionada con el objetivo",
+    value: "0.660"
+}];
+
+var integrity = [{
+    name: "Ninguno: No se presenta ningún impacto",
+    value: "0"
+}, {
+    name: "Parcial: Es posible modificar cierta información del objetivo",
+    value: "0.275"
+}, {
+    name: "Completo: Es posible modificar toda la información del objetivo",
+    value: "0.660"
+}];
+
+var disponibility = [{
+    name: "Ninguno: No se presenta ningún impacto",
+    value: "0"
+}, {
+    name: "Parcial: Se presenta intermitencia en el acceso al objetivo",
+    value: "0.275"
+}, {
+    name: "Completo: Hay una caída total del objetivo",
+    value: "0.660"
+}];
+
+var explotability = [{
+    name: "Improbable: No existe un exploit",
+    value: "0.850"
+}, {
+    name: "Conceptual: Existen pruebas de laboratorio",
+    value: "0.900"
+}, {
+    name: "Funcional: Existe exploit",
+    value: "0.950"
+}, {
+    name: "Alta: No se requiere exploit o se puede automatizar",
+    value: "1.000"
+}];
+
+var resolutionLevel = [{
+    name: "Paliativa: Existe un parche que no fue publicado por el fabricante",
+    value: "0.950"
+}, {
+    name: "Oficial: Existe un parche disponible por el fabricante",
+    value: "0.870"
+}, {
+    name: "Temporal: Existen soluciones temporales",
+    value: "0.900"
+}, {
+    name: "Inexistente: No existe solución",
+    value: "1.000"
+}];
+
+var realiabilityLevel = [{
+    name: "No confirmado: Existen pocas fuentes que reconocen la vulnerabilidad",
+    value: "0.900"
+}, {
+    name: "No corroborado: La vulnerabilidad es reconocida por fuentes no oficiales",
+    value: "0.950"
+}, {
+    name: "Confirmado: La vulnerabilidad es reconocida por el fabricante",
+    value: "1.000"
+}];
--- app/assets/app/controllers/findingResumeController.js
+++ app/assets/app/controllers/findingResumeController.js
@@ -12,12 +12,60 @@
  * @return {undefined}
  */
 integrates.controller("FindingResumeController", function($scope, $stateParams,
-                                                          $uibModal, $translate) {
+                                                          $uibModal, $translate,
+                                                          findingFactory) {                                                            
+    $scope.headerBuilding = function(){
+        console.log($scope.finding);
+        $scope.header = {};
+        $scope.header.findingTitle = $scope.finding.hallazgo;
+        $scope.header.findingType = $scope.finding.tipo_prueba;
+        $scope.header.findingRisk = "";
+        $scope.header.findingValue = $scope.finding.criticidad;
+        var findingValue = parseFloat($scope.finding.criticidad);
+        console.log(parseFloat(findingValue));
+        if(findingValue >= 7){
+            $scope.header.findingValueDescription = " Alto";
+            $scope.header.findingValueColor = $scope.colors.critical;
+        }else if(findingValue >= 4 && findingValue <= 6.9){
+            $scope.header.findingValueDescription = " Moderado";
+            $scope.header.findingValueColor = $scope.colors.moderate;
+        }else{
+            $scope.header.findingValueDescription = " Tolerable";
+            $scope.header.findingValueColor = $scope.colors.tolerable;
+        }
+        $scope.header.findingCount = $scope.finding.cardinalidad;
+        console.log($scope.header.findingValueDescription);
+    };                                                           
+    $scope.loadFindingByID = function(id){
+        if(isNaN(id)){
+           window.close();  //avoid fake paths or id
+        }else{
+            var req = findingFactory.getVulnById(id);
+            req.then(function(response){
+                if(!response.error){
+                    $scope.finding = response.data;
+                    $scope.headerBuilding();
+                }else{
+                    $.gritter.add({
+                        title: 'Error!', text: response.message,
+                        class_name: 'color warning', sticky: false,
+                    });
+                }
+            });
+        }
+    };
+    $scope.colorPalette = function(){
+        $scope.colors = {};
+        $scope.colors.critical = "background-color: #f12;";  //red
+        $scope.colors.moderate = "background-color: #f72;";  //orange
+        $scope.colors.tolerable = "background-color: #ff2;"; //yellow
+    };
     $scope.init = function(){
+        $scope.colorPalette();
         $scope.finding = {};
         $scope.finding.id = $stateParams.id;
-        if(isNaN($scope.finding.id)) //avoid fake paths or id
-            window.close();
+        $scope.loadFindingByID($scope.finding.id);
+        $scope.list = {};
     };
 
     $scope.init();
