0200732d37efd2be8c14b9415e42ba2baf3e8c55
Andres Roldan <aroldan@fluid.la>
2017-07-26 16:15:15 -0500

    Send the resume email every monday at 8am. Closes #12

--- fluidintegrates/settings.py
+++ fluidintegrates/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = (
 )
 
 CRONJOBS = [
-    ('0 8 * * *', 'app.scheduler.get_new_findings')
+    ('0 8 * * 1', 'app.scheduler.get_new_findings')
 ]
 
 AUTHENTICATION_BACKENDS = (
