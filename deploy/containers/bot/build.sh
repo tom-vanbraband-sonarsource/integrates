1e7286355c338f2c195fa8c25e5f80860dbdffcf
Andres Roldan <aroldan@fluid.la>
2017-10-20 09:21:19 -0500

    Use symlink to common dir.

--- deploy/containers/bot/build.sh
+++ deploy/containers/bot/build.sh
@@ -17,6 +17,8 @@ echo "---### [${SERVER}] Compilando contenedor."
 
 # construir la imagen
 cp ~/.vault.txt deploy/containers/bot/
+ln -sf ../common
 sudo docker build --build-arg circle_branch=$CIRCLE_BRANCH -t 205810638802.dkr.ecr.us-east-1.amazonaws.com/integratesbot:$MASTER_BRANCH deploy/containers/bot
 rm deploy/containers/bot/.vault.txt
 docker run --detach --name="$SERVER" -p 8000:443 205810638802.dkr.ecr.us-east-1.amazonaws.com/integratesbot:$MASTER_BRANCH
+rm common
--- deploy/containers/integrates/build.sh
+++ deploy/containers/integrates/build.sh
@@ -17,6 +17,8 @@ echo "---### [${SERVER}] Compilando contenedor."
 
 # construir la imagen
 cp ~/.vault.txt deploy/containers/integrates/
+ln -sf ../common
 sudo docker build --build-arg circle_branch=$CIRCLE_BRANCH -t 205810638802.dkr.ecr.us-east-1.amazonaws.com/integrates:$MASTER_BRANCH deploy/containers/integrates
 rm deploy/containers/integrates/.vault.txt
 docker run --detach --name="$SERVER" -p 8000:443 205810638802.dkr.ecr.us-east-1.amazonaws.com/integrates:$MASTER_BRANCH
+rm common
