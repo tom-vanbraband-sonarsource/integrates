7b0b5bb84bd5e33c8aa4f6657de595d4074a7238
cgomezatfluid <cgomez@fluid.la>
2017-11-24 17:00:12 -0500

    Partial fix issue #139

--- app/assets/app/controllers/projectCtrl.js
+++ app/assets/app/controllers/projectCtrl.js
@@ -489,6 +489,24 @@ integrates.controller(
                 //Control de campos editables
                 $scope.onlyReadableTab1 = true;
                 $scope.onlyReadableTab2 = true;
+                //Inicializar galeria de evidencias
+                $('.popup-gallery').magnificPopup({
+                    delegate: 'a',
+                    type: 'image',
+                    tLoading: 'Loading image #%curr%...',
+                    mainClass: 'mfp-img-mobile',
+                    gallery: {
+                        enabled: true,
+                        navigateByImgClick: true,
+                        preload: [0,1]
+                    },
+                    image: {
+                        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                        titleSrc: function(item) {
+                            return item.el.attr('title');
+                        }
+                    }
+                });
                 //Tracking mixpanel
                 mixPanelDashboard.trackReadFinding(userEmail, $scope.finding.id);
                 $timeout($scope.goUp, 200);
