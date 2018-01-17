2dc7fc8a2b1b21e32266a8e42149f0dc89a08171
jrestrepoatfluid <jrestrepo@fluid.la>
2018-01-17 11:27:14 -0500

    Revert "Fixed Issue #218 (Unfinished)"
    This reverts commit 5b2bab4e4c9beed12662bbc5cc81f0b42dd7d37c.

--- app/api/formstack.py
+++ app/api/formstack.py
@@ -3,18 +3,17 @@
 
 import json
 import requests
-# pylint: disable=E0402
-from .. import FORMSTACK_TOKENS
 from requests.exceptions import ConnectionError
 from retrying import retry
 from random import randint
+# pylint: disable=E0402
 
 requests.adapters.DEFAULT_RETRIES = 10
 
 class FormstackAPI(object):
 
     headers_config = {}
-    ltokens= FORMSTACK_TOKENS.split(',')
+    ltokens=
     TOKEN = ltokens[randint(0,(len(ltokens)-1))]
     SUBMISSION_URL = "https://www.formstack.com/api/v2/submission/:id.json"
     #Finding URL
