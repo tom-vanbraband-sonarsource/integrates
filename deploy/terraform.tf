b764c07af27d0a899e9ae6025c200645a826bc43
cgomezatfluid <cgomez@fluid.la>
2018-02-23 10:53:18 -0500

    update dynamo tables in terraform

--- deploy/terraform.tf
+++ deploy/terraform.tf
@@ -20,28 +20,34 @@ resource "aws_dynamodb_table" "comments" {
   }  
 }
 
-resource "aws_dynamodb_table" "comments_test" {
-  name           = "comments_test"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "finding_id"
-  range_key      = "user_id"
-
-  attribute {
-    name = "finding_id"
-    type = "N"
-  }
-
-  attribute {
-    name = "user_id"
-    type = "N"
-  }  
-}
-
 resource "aws_dynamodb_table" "remediated" {
   name           = "remediated"
   read_capacity  = 10
   write_capacity = 10
+  hash_key       = "finding_id"
+  
+  attribute {
+    name = "finding_id"
+    type = "N"
+  }
+}
+
+resource "aws_dynamodb_table" "toe" {
+  name           = "toe"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "project"
+
+  attribute {
+    name = "project"
+    type = "S"
+  }  
+}
+
+resource "aws_dynamodb_table" "findings" {
+  name           = "findings"
+  read_capacity  = 5
+  write_capacity = 5
   hash_key       = "finding_id"
   
   attribute {
