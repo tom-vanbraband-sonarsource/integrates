740f9e2854ba09ac6856da8f502e6f85ad13613d
oparada <oparada@fluid.la>
2018-03-09 12:54:02 -0500

    Solución de error en visualización
    Eliminación de codigo repetido
    
    Eliminación de prints
    
    Valor por defecto en las alertas

--- app/assets/app/controllers/projectCtrl.js
+++ app/assets/app/controllers/projectCtrl.js
@@ -83,16 +83,12 @@ integrates.controller(
         $scope.alertHeader = function(company, project){
           var req = projectFtry.getAlerts(company, project);
           req.then(function(response){
-              if(!response.error){
-                if (response.data[0].status_act=='1'){
+              if(!response.error && response.data.length > 0){
+                if (response.data.status_act=='1'){
                    var html = '<div class="alert alert-danger-2">';
                    html += '<strong>Atención! </strong>' + response.data[0].message + '</div>';
                    document.getElementById('header_alert').innerHTML = html;
                  }
-               }else{
-                 var error_ac1 = $translate.instant('proj_alerts.error_textsad');
-                 Rollbar.error("Error: An error occurred getting company alerts");
-                 $msg.error(error_ac1);
                }
              });
            };
