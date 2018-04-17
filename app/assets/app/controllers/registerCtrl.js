7b9874e5b2946469c19eb9d3a98c15532a2b587b
cgomezatfluid <cgomez@fluid.la>
2018-04-17 13:16:26 -0500

    Fix issue #268

---
+++ app/assets/app/controllers/registerCtrl.js
@@ -0,0 +1,56 @@
+/* global
+integrates, $, BASE, mixpanel, userMail, $xhr, Organization, userEmail, mixPanelDashboard, userName, projectData:true, eventsData:true, findingData:true
+*/
+/**
+ * @file registerCtrl.js
+ * @author engineering@fluidattacks.com
+ */
+/**
+ * Crea el controlador de las funciones del registro
+ * @name registerController
+ * @param {Object} $scope
+ * @param {Object} $uibModal
+ * @return {undefined}
+ */
+/** @export */
+integrates.controller("registerCtrl", function dashboardCtrl (
+  $scope, $uibModal, $timeout,
+  $state, $stateParams, $q,
+  $translate
+) {
+  $scope.logout = function logout () {
+    const modalInstance = $uibModal.open({
+      "animation": true,
+      "controller" ($scope, $uibModalInstance) {
+        $scope.closeModalLogout = function closeModalLogout () {
+          $uibModalInstance.close();
+        };
+        $scope.okModalLogout = function okModalLogout () {
+          projectData = [];
+          eventsData = [];
+          findingData = {};
+          window.location = `${BASE.url}logout`;
+        };
+      },
+      "resolve": {"done": true},
+      "templateUrl": "logout.html",
+      "windowClass": "modal avance-modal"
+    });
+  };
+
+  /**
+   * Cambia el lenguaje del dashboard
+   * @function changeLang
+   * @member integrates.dashboardCtrl
+   * @return {undefined}
+   */
+  $scope.changeLang = function changeLang (langKey) {
+    if (langKey == "es" || langKey == "en") {
+      localStorage.lang = langKey;
+    }
+    $translate.use(localStorage.lang);
+    mixpanel.identify(userEmail);
+    mixpanel.people.set({"$Language": localStorage.lang});
+    location.reload();
+  };
+});
