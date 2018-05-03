997c452490821d7fa82a1353324240f2f8866916
cgomezatfluid <cgomez@fluidattacks.com>
2018-05-03 13:34:54 -0500

    Fix issue #386 part 2

--- app/assets/app/traducciones-2.js
+++ app/assets/app/traducciones-2.js
@@ -71,12 +71,20 @@ const traducciones2 = {
    },
   "tab_description":
   {
+    "actorTooltip": "Persona que podría ejecutar el vector de ataque, que " +
+                    "afecta a los activos especificados, usando el hallazgo",
+    "btsTooltip": "Link a un sistema de seguimiento de errores externo",
     "bts_external": "BTS Externo",
     "cardinality": "Vulnerabilidades Abiertas",
     "category": "Categoria",
     "compromisedAtributes": "Atributos Comprometidos",
+    "compromisedAtributesTooltip": "Atributos que fueron comprometidos al " +
+                                    "explotar la vulnerabilidad",
     "compromisedRecords": "Registros Comprometidos",
+    "compromisedRecordsTooltip": "Número de datos comprometidos al explotar " +
+                                  "la vulnerabilidad",
     "description": "Descripcion",
+    "descriptionTooltip": "Descripción del hallazgo",
     "description_name": "Descripcion",
     "detailed": "Detallado",
     "edit_treatment": "Editar",
@@ -84,28 +92,47 @@ const traducciones2 = {
     "general": "General",
     "probability": "Probabilidad",
     "recommendation": "Recomendacion",
+    "recommendationTooltip": "Recomendación general que se da para evitar " +
+                              "esta falla de seguridad",
     "remediated": "Solicitar Verificación",
     "remediated_finding": "Hallazgo remediado",
     "requirements": "Requisitos",
+    "requirementsTooltip": "Requisito del criterio de seguridad inclumplido " +
+                            "por el cual se está materializando el riesgo",
     "risk": "Valor Riesgo",
     "severity": "Severidad",
     "solution": "Justificación de la Solución Aplicada",
     "stage": "Escenario",
+    "stageTooltip": "Lugar desde donde se explota la vulnerabilidad",
     "system": "Sistemas Comprometidos",
+    "systemTooltip": "Nombre de los sistemas que fueron afectados al " +
+                      "explotar este hallazgo",
     "threat": "Amenaza",
+    "threatTooltip": "Descripción de la amenaza que representa la presencia " +
+                      "de esta falla de seguridad",
     "title": "Titulo",
     "treat_justification": "Justificación del Tratamiento",
+    "treat_justificationTooltip": "Justificación del tratamiento dado al " +
+                                  "hallazgo",
     "treat_manager": "Responsable del Tratamiento",
+    "treat_managerTooltip": "Persona que definió o modificó el tratamiento " +
+                            "que se dará",
     "treatment": "Tratamiento",
+    "treatmentTooltip": "Tratamiento que se le dará al hallazgo",
     "type": "Tipo Hallazgo",
     "update": "Actualizar",
     "update_treatment": "Actualizar",
     "update_treatmodal": "Actualizar Tratamiento",
     "vectors": "Vectores de ataque",
+    "vectorsTooltip": "Descripción del caso de abuso generado por este " +
+                      " hallazgo",
     "verified": "Verificado",
     "verified_finding": "Hallazgo verificado",
     "weakness": "Debilidad",
-    "where": "Donde"
+    "weaknessTooltip": "Link de la vulnerabilidad categorizada en la CWE",
+    "where": "Donde",
+    "whereTooltip": "URL y/o puertos de los recursos que tienen " +
+                    "vulnerabilidades de seguridad"
   },
   "tab_evidence":
   {
