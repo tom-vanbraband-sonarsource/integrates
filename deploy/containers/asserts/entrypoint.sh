78650c6230669a1df46dda9490ee1d1d9bd51aa3
Andres Cuberos <acuberos@fluidattacks.com>
2018-05-09 09:23:27 -0500

    Add build job for Asserts container

---
+++ deploy/containers/asserts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ -z ${REVIEW} ]]; then
+	export FI_ENVIRONMENT=fluidattacks.com/integrates
+else
+	export FI_ENVIRONMENT="$BRANCH.integrates.env.fluidattacks.com"
+fi
+sed -i 's/$FI_ENVIRONMENT/'"$FI_ENVIRONMENT"'/g' /exploit.py
+
+python /exploit.py
+retval=$?
+curl -u ${USER}:${PASS} -T /tmp/fluidasserts.log https://fluid.jfrog.io/fluid/generic/fluidasserts_${ORG}_${APP}_$(date +%Y%m%d%H%M%S).log
+exit $retval
