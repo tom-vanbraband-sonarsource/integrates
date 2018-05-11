39dc18f824f6a82729b12042296d2eb4c2dbe498
cgomezatfluid <cgomez@fluidattacks.com>
2018-05-11 14:58:17 -0500

    Fix issue #440 part 1

--- app/api/drive.py
+++ app/api/drive.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Integrates Google Drive API """
+"""Integrates Google Drive API."""
 
 from __future__ import print_function
 import httplib2
@@ -13,13 +13,14 @@ from oauth2client import tools
 from oauth2client.file import Storage
 from django.conf import settings
 
-from apiclient.http import MediaIoBaseDownload # pylint: disable=import-error
-from apiclient.http import HttpError # pylint: disable=import-error
+from apiclient.http import MediaIoBaseDownload  # pylint: disable=import-error
+from apiclient.http import HttpError  # pylint: disable=import-error
 
 logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
 
+
 class DriveAPI(object):
-    """ Clase para consumir la API de Google Drive """
+    """Clase para consumir la API de Google Drive."""
 
     SCOPES = []
     CLIENT_SECRET_FILE = ""
@@ -27,9 +28,8 @@ class DriveAPI(object):
     APPLICATION_NAME = ""
     FILE = None
 
-    def __init__(self, drive_file_id = ""):
-        """ Constructor de la clase """
-
+    def __init__(self, drive_file_id=""):
+        """Constructor de la clase."""
         self.SCOPES = settings.DRIVE_SCOPES
         self.CLIENT_SECRET_FILE = settings.DRIVE_SECRET_FILE
         self.CLIENT_AUTHOR_FILE = settings.DRIVE_AUTHOR_FILE
@@ -37,8 +37,8 @@ class DriveAPI(object):
         if(drive_file_id != ""):
             self.FILE = self.download(drive_file_id)
 
-    def download(self, drive_file_id = ""):
-        """ Descarga los archivos de google drive en /tmp/ """
+    def download(self, drive_file_id=""):
+        """Descarga los archivos de google drive en /tmp/."""
         filename = "/tmp/:id.tmp".replace(":id", drive_file_id)
         credentials = self.get_credentials()
         http = credentials.authorize(httplib2.Http())
@@ -50,18 +50,23 @@ class DriveAPI(object):
         done = False
         while done is False:
             try:
-                status, done = downloader.next_chunk()# pylint: disable=unused-variable                
+                # pylint: disable=unused-variable
+                status, done = downloader.next_chunk()
             except HttpError:
-                rollbar.report_message('Error: Unable to download the file', 'error')
+                rollbar.report_message(
+                    'Error: Unable to download the file',
+                    'error')
         if done:
             mime = Magic(mime=True)
             mime_type = mime.from_file(filename)
-            if mime_type in ["image/png", "image/jpeg", "image/gif", "text/x-c", "text/x-python", "text/plain", "text/html"]:
+            if mime_type in ["image/png", "image/jpeg", "image/gif",
+                             "text/x-c", "text/x-python", "text/plain",
+                             "text/html"]:
                 return fh
         return None
 
-    def download_images(self, drive_file_id = ""):
-        """ Descarga los archivos de google drive en /tmp/ """
+    def download_images(self, drive_file_id=""):
+        """Descarga los archivos de google drive en /tmp/."""
         hard_path = "/usr/src/app/app/documentator/images/"
         filename = hard_path + ":id.png".replace(":id", drive_file_id)
         credentials = self.get_credentials()
@@ -74,9 +79,12 @@ class DriveAPI(object):
         done = False
         while done is False:
             try:
-                status, done = downloader.next_chunk()# pylint: disable=unused-variable                
+                # pylint: disable=unused-variable
+                status, done = downloader.next_chunk()
             except HttpError:
-                rollbar.report_message('Error: Unable to download the image', 'error')
+                rollbar.report_message(
+                    'Error: Unable to download the image',
+                    'error')
         if done:
             mime = Magic(mime=True)
             mime_type = mime.from_file(filename)
@@ -85,13 +93,12 @@ class DriveAPI(object):
         return None
 
     def get_credentials(self):
-        """ Obtiene las credenciales para autenticar la API """
-        
+        """Obtiene las credenciales para autenticar la API."""
         store = Storage(self.CLIENT_AUTHOR_FILE)
         credentials = store.get()
         if not credentials or credentials.invalid:
             flow = client.flow_from_clientsecrets(
-                self.CLIENT_SECRET_FILE, 
+                self.CLIENT_SECRET_FILE,
                 self.SCOPES
             )
             flow.user_agent = self.APPLICATION_NAME
