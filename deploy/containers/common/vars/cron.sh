4cd4bf929dc0b83f38d485a4ffb1c4c1ba97d3b4
Andres Cuberos <acuberos@fluidattacks.com>
2018-05-28 09:51:08 -0500

    Fix cron deployment

--- deploy/containers/common/vars/cron.sh
+++ deploy/containers/common/vars/cron.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'TORUS_TOKEN_ID='"$TORUS_TOKEN_ID" > /root/mycron
-echo 'TORUS_TOKEN_SECRET='"$TORUS_TOKEN_SECRET" >> /root/mycron
-
 crontab -l >> /root/mycron
 sed -i 's#/usr/bin#torus\ run\ --\ /usr/bin#g' /root/mycron
 
