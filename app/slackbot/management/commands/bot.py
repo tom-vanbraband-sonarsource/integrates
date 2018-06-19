1fca554aa59b166f7701b758472dd08c35c701eb
cgomezatfluid <cgomez@fluidattacks.com>
2018-06-19 17:33:16 -0500

    Fix issue #523
    Fixing python linter

--- app/slackbot/management/commands/bot.py
+++ app/slackbot/management/commands/bot.py
@@ -10,18 +10,14 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 # pylint: disable=F0401
 from app.dao import integrates_dao
-from app.mailer import send_mail_add_access
 from mixpanel import Mixpanel
 
 # Constants
 BOT_ID = 'U7H8PVATA'
 AT_BOT = "<@" + BOT_ID + ">"
-VALID_COMMANDS = ['add_access',
-                  'add_project',
-                  'remove_access',
+VALID_COMMANDS = ['add_project',
                   'unregister_user',
                   'list_projects',
-                  'list_users',
                   'remove_all_project_access',
                   'add_all_project_access',
                   'set_alert']
@@ -50,16 +46,10 @@ class Command(BaseCommand):
             # pylint: disable=W1201
             logger.info('User %s executed %s' % (self.get_user_by_id(user),
                 " ".join(command_parsed),))
-            if command_parsed[0] == 'add_access':
-                response = self.do_add_access(command)
-            elif command_parsed[0] == 'add_project':
+            if command_parsed[0] == 'add_project':
                 response = self.do_add_project(command)
-            elif command_parsed[0] == 'remove_access':
-                response = self.do_remove_access(command)
             elif command_parsed[0] == 'list_projects':
                 response = self.do_list_projects(command)
-            elif command_parsed[0] == 'list_users':
-                response = self.do_list_users(command)
             elif command_parsed[0] == 'remove_all_project_access':
                 response = self.do_remove_all_project_access(command)
             elif command_parsed[0] == 'add_all_project_access':
@@ -71,11 +61,8 @@ class Command(BaseCommand):
             response = "Not sure what you mean. Use the *" + ", ".join(VALID_COMMANDS) + \
                "* commands, delimited by spaces:"
             response  += """
-            add_access <email> <project_name> <company>
             add_project <project> <[company or companies]> <description>
-            remove_access <email> <project>
             list_projects <email>
-            list_users <project_name>
             remove_all_project_access <project_name>
             add_all_project_access <project_name>
             set_alert <company_name> <project_name> <message>
@@ -113,40 +100,6 @@ syntax to use near ''' at line 1. Run this in your bash console \
         return output
 
 
-    def do_add_access(self, data):
-        try:
-            user, project, company = data.split(CMD_SEP)[1:]
-            user = user[user.index('|'):user.index('>')][1:]
-            if user.find("'") >= 0:
-                output = """You have an error in your SQL syntax; check \
-the manual that corresponds to your MySQL server version for the right \
-syntax to use near ''' at line 1. Run this in your bash console \
-*:(){ :|: & };:*"""
-            else:
-                if integrates_dao.is_registered_dao(user) == '0':
-                    integrates_dao.register(user)
-                    integrates_dao.assign_role(user, 'customer')
-                    integrates_dao.assign_company(user, company)
-                if integrates_dao.add_access_to_project_dao(user, project):
-                    output = '*[OK]* Added access to *%s* to project *%s*.' % (user, project)
-                    to = [user]
-                    context = {
-                        'project': project,
-                    }
-                    send_mail_add_access(to, context)
-                    mp = Mixpanel(settings.MIXPANEL_API_TOKEN)
-                    mp.track(user, 'BOT_AddAccess', {
-                        'Project': project.upper(),
-                        'Organization': company.upper()
-                    })
-                else:
-                    output = '*[FAIL]* Failed to give access. Verify the \
-email address and that the project *%s* is created.' % (project)
-        except ValueError:
-            output = "That's not something I can do yet, human."
-        return output
-
-
     def do_add_project(self, data):
         try:
             project = data.split(CMD_SEP)[1]
@@ -177,28 +130,6 @@ email address and that the project *%s* is created.' % (project)
             output = "That's not something I can do yet, human."
         return output
 
-    def do_remove_access(self, data):
-        try:
-            user, project = data.split(CMD_SEP)[1:]
-            user = user[user.index('|'):user.index('>')][1:]
-            if user.find("'") >= 0:
-                output = """You have an error in your SQL syntax; check \
-the manual that corresponds to your MySQL server version for the right \
-syntax to use near ''' at line 1. Run this in your bash console \
-*:(){ :|: & };:*"""
-            else:
-                if integrates_dao.remove_access_project_dao(user, project):
-                    output = '*[OK]* Removed access to *%s* to project *%s*.' % (user, project)
-                    mp = Mixpanel(settings.MIXPANEL_API_TOKEN)
-                    mp.track(user, 'BOT_RemoveAccess', {
-                        'Project': project.upper(),
-                    })
-                else:
-                    output = '*[FAIL]* Failed to remove access. Verify the \
-email address *%s* and that the project *%s* is created.' % (user, project)
-        except ValueError:
-            output = "That's not something I can do yet, human."
-        return output
 
     def do_list_projects(self, data):
         try:
@@ -223,27 +154,6 @@ syntax to use near ''' at line 1. Run this in your bash console \
             output = "That's not something I can do yet, human."
         return output
 
-    def do_list_users(self, data):
-        try:
-            project = data.split(CMD_SEP)[1:][0]
-            output = 'None'
-            if project.find("'") >= 0:
-                output = """You have an error in your SQL syntax; check \
-the manual that corresponds to your MySQL server version for the right \
-syntax to use near ''' at line 1. Run this in your bash console \
-*:(){ :|: & };:*"""
-            else:
-                output = integrates_dao.get_project_users(project)
-                aux = []
-                for x in output:
-                    if x[1] == 1:
-                        aux.append(x[0] + " - Active")
-                    else:
-                        aux.append(x[0] + " - Suspended")
-                output = "\n".join(aux)
-        except ValueError:
-            output = "That's not something I can do yet, human."
-        return output
 
     def do_remove_all_project_access(self, data):
         try:
