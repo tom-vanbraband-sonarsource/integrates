e3a7ebb7fc6a5791ec6f8de554c8206c04c64291
dacevedoatfluid <dacevedo@fluidattacks.com>
2018-07-24 17:00:46 -0500

    Fix issue #723: Improve SocialAuth exception handling

--- app/middleware.py
+++ app/middleware.py
@@ -7,13 +7,18 @@ class SocialAuthException(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
         if hasattr(social_exceptions, exception.__class__.__name__):
             exceptionType = exception.__class__.__name__
+
+            # An already logged in user attempted to access with a different account
             if exceptionType == "AuthAlreadyAssociated":
                 return HttpResponse('<script> \
                     localStorage.setItem("showAlreadyLoggedin","1"); \
                     if (location.origin.indexOf("://fluidattacks.com") === -1) { \
                         location = "/registration"; \
                     }else{ location = "/integrates/registration"; } </script>')
-            elif exceptionType == "AuthCanceled":
+
+            # A user clicked return or stopped the page load after social auth
+            elif exceptionType in ["AuthCanceled", "AuthMissingParameter",
+                                   "AuthStateMissing"]:
                 return redirect("/index")
         else:
             return super(SocialAuthException, self).process_exception(request,
