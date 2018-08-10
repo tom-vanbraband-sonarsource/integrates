0f000943fccdb7fc0d22b9d2382036230cf5317b
cgomezatfluid <cgomez@fluidattacks.com>
2018-08-10 08:58:51 -0500

    Fix issue #751: Changing destinatary in findings to release email

--- app/scheduler.py
+++ app/scheduler.py
@@ -342,7 +342,8 @@ def get_new_releases():
                 'warning')
     if cont > 0:
         context['total'] = cont
-        to = ["projects@fluidattacks.com", "production@fluidattacks.com"]
+        to = ["projects@fluidattacks.com", "production@fluidattacks.com",
+              "jarmas@fluidattacks.com", "jaguirre@fluidattacks.com"]
         send_mail_new_releases(to, context)
     else:
         logger.info('There are no new drafts')
