71adca269fbcf6cba5670bc5de525b22c59dec65
cgomezatfluid <cgomez@fluidattacks.com>
2018-08-16 15:06:26 -0500

    Fix issue #750: Add formstack pagination

--- app/api/formstack.py
+++ app/api/formstack.py
@@ -36,12 +36,26 @@ AppleWebKit/537.36 (KHTML, like Gecko) FLUIDIntegrates/1.0'
         self.AVAILABLE_TOKENS = len(self.TOKEN_LIST)
         self.CURRENT_TOKEN = self.TOKEN_LIST[0]
 
+    def requests_per_page(self, method, url, data=None):
+        """Return the request from Formstack."""
+        formstack_response = self.request(method, url, data)
+        total_submissions = int(formstack_response["total"])
+        while total_submissions > data['per_page']:
+            data["page"] += 1
+            next_page_submissions = self.request(
+                method,
+                url,
+                data)
+            formstack_response["submissions"].extend(
+                next_page_submissions["submissions"])
+            total_submissions -= data['per_page']
+        return formstack_response
 
     def request(self, method, url, data=None):
+        """Validate if a token is available."""
         formstack_request = self.execute_request(method, url, data)
-
         while formstack_request.status_code == 429 and \
-                                                self.AVAILABLE_TOKENS > 0:
+                self.AVAILABLE_TOKENS > 0:
             if self.AVAILABLE_TOKENS < 3:
                 message = 'Warning: There are only {} available formstack \
                 tokens left for today.'.format(self.AVAILABLE_TOKENS)
@@ -49,7 +63,6 @@ AppleWebKit/537.36 (KHTML, like Gecko) FLUIDIntegrates/1.0'
 
             self.refresh_token()
             formstack_request = self.execute_request(method, url, data)
-
         return json.loads(formstack_request.text)
 
     @retry(retry_on_exception=ConnectionError, stop_max_attempt_number=5)
@@ -109,8 +122,9 @@ AppleWebKit/537.36 (KHTML, like Gecko) FLUIDIntegrates/1.0'
         search_field = "29042322"
         data = {'search_field_1': search_field,
                 'search_value_1': project,
-                'per_page': 100}
-        return self.request("GET", self.EV_URL, data=data)
+                'page': 1,
+                'per_page': 50}
+        return self.requests_per_page("GET", self.EV_URL, data=data)
 
     @cache_control(max_age=600)
     def get_submission(self, submission_id):
@@ -124,29 +138,36 @@ AppleWebKit/537.36 (KHTML, like Gecko) FLUIDIntegrates/1.0'
         search_field = "32201732"
         data = {'search_field_1': search_field,
                 'search_value_1': project,
-                'per_page': 100}
-        return self.request("GET", self.FN_URL, data=data)
+                'page': 1,
+                'per_page': 50}
+        return self.requests_per_page("GET", self.FN_URL, data=data)
 
     def get_project_info(self, project):
         """ Get the findings of a project by its name. """
         search_field = "52601266"
         data = {'search_field_1': search_field,
                 'search_value_1': project,
-                'per_page': 100}
-        return self.request("GET", self.IN_URL, data=data)
+                'page': 1,
+                'per_page': 50}
+        return self.requests_per_page("GET", self.IN_URL, data=data)
 
     def get_closings_by_id(self, submission_id):
         """ Get the closures of a finding by its ID """
         search_field = "39596063"
         data = {'search_field_1': search_field,
-                'search_value_1': submission_id}
-        return self.request("GET", self.CL_URL, data=data)
+                'search_value_1': submission_id,
+                'page': 1,
+                'per_page': 50}
+        return self.requests_per_page("GET", self.CL_URL, data=data)
 
     def get_closings_by_project(self, project):
         """ Get the all closures of a project """
         search_field = "39596058"
-        data = {'search_field_1': search_field, 'search_value_1': project}
-        return self.request("GET", self.CL_URL, data=data)
+        data = {'search_field_1': search_field,
+                'search_value_1': project,
+                'page': 1,
+                'per_page': 50}
+        return self.requests_per_page("GET", self.CL_URL, data=data)
 
     def update(self, request_id, data_dto):
         """ Update a record in formstack. """
