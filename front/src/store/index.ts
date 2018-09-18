fc2c0b493eb2f1cdb3ae183fdeb58de97d782403
dacevedoatfluid <dacevedo@fluidattacks.com>
2018-09-18 17:58:26 -0500

    Redux store: Enable Redux DevTools on dev and review envs

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,7 +1,27 @@
 import { createStore, Store } from "redux";
+import { getEnvironment } from "../utils/context";
 import rootReducer from "./rootReducer";
+
+/* tslint:disable:no-any
+ *
+ * NO-ANY: Disabling this rule is necessary because TypeScript doesn't
+ * recognize custom properties attached to the global window object,
+ * which is required to bind redux dev tools browser extension
+ */
+declare var window: any;
+const reduxDevTools: any = (): any =>
+  (
+    window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : undefined
+  );
+/* tslint:enable:no-any */
 
 const store: Store = createStore(
   rootReducer,
+  getEnvironment() === "production"
+  ? undefined
+  : reduxDevTools(),
 );
+
 export = store;
