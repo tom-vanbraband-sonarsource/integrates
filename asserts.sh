8cac06a445ca3aa541da55deb5371fbbd48a06d5
Andres Cuberos <acuberos@fluidattacks.com>
2018-10-23 10:57:08 -0500

    Update Asserts execution in pre-commit

--- asserts.sh
+++ asserts.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-python3 /code/exploit-code.py > /tmp/fluidasserts.log
-retval=$?
-cat /tmp/fluidasserts.log
-
+asserts --show-open /code/exploit-code.py > /tmp/fluidasserts.log
 retval=0
-grep -q "OPEN" /tmp/fluidasserts.log
-if [[ x"$?" = x"0" ]]; then
-    retval=1
-fi
 
-if [[ x"$retval" = x"1" ]]; then
-    echo -e "\033[1;31mThere are open vulnerabilities!"
+if grep -q OPEN /tmp/fluidasserts.log
+then
+  cat /tmp/fluidasserts.log
+  retval=1
 fi
 
 exit $retval
