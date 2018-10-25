d046ef7d6c8d9350efdf4a6fbe6a973f0e54e78b
dacevedoatfluid <dacevedo@fluidattacks.com>
2018-10-25 11:19:13 -0500

    Issue: #880 - Apply single quote string standard to resources entity

--- app/entity/resource.py
+++ app/entity/resource.py
@@ -21,10 +21,10 @@ class Resource(ObjectType):
         self.environments = []
         project_info = integrates_dao.get_project_dynamo(project_name)
         for item in project_info:
-            if "repositories" in item:
+            if 'repositories' in item:
                 self.repositories = item['repositories']
 
-            if "environments" in item:
+            if 'environments' in item:
                 self.environments = item['environments']
 
     def resolve_repositories(self, info):
@@ -55,37 +55,37 @@ class AddRepositories(Mutation):
         email_data = []
 
         for repo in resources_data:
-            if "repository" in repo and "branch" in repo:
-                repository = repo.get("repository")
-                branch = repo.get("branch")
+            if 'repository' in repo and 'branch' in repo:
+                repository = repo.get('repository')
+                branch = repo.get('branch')
                 json_data.append({
                     'urlRepo': repository,
                     'branch': branch
                 })
                 email_text = 'Repository: {repository!s} Branch: {branch!s}' \
                     .format(repository=repository, branch=branch)
-                email_data.append({"urlEnv": email_text})
+                email_data.append({'urlEnv': email_text})
             else:
                 rollbar.report_message('Error: An error occurred adding repository', 'error', info.context)
         add_repo = integrates_dao.add_list_resource_dynamo(
-            "FI_projects",
-            "project_name",
+            'FI_projects',
+            'project_name',
             project_name,
             json_data,
-            "repositories"
+            'repositories'
         )
         if add_repo:
             to = ['continuous@fluidattacks.com', 'projects@fluidattacks.com']
             context = {
                 'project': project_name.upper(),
-                'user_email': info.context.session["username"],
+                'user_email': info.context.session['username'],
                 'action': 'Add repositories',
                 'resources': email_data,
                 'project_url': 'https://fluidattacks.com/integrates/dashboard#!/project/{project!s}/resources'
                 .format(project=project_name)
             }
             email_send_thread = threading.Thread( \
-                                          name="Add repositories email thread", \
+                                          name='Add repositories email thread', \
                                           target=send_mail_repositories, \
                                           args=(to, context,))
             email_send_thread.start()
@@ -109,41 +109,41 @@ class RemoveRepositories(Mutation):
     @require_project_access_gql
     def mutate(self, info, repository_data, project_name):
         success = False
-        repository = repository_data.get("urlRepo")
-        branch = repository_data.get("branch")
-        repo_list = integrates_dao.get_project_dynamo(project_name)[0]["repositories"]
+        repository = repository_data.get('urlRepo')
+        branch = repository_data.get('branch')
+        repo_list = integrates_dao.get_project_dynamo(project_name)[0]['repositories']
         index = -1
         cont = 0
         email_data = []
 
         while index < 0 and len(repo_list) > cont:
-            if repo_list[cont]["urlRepo"] == repository and repo_list[cont]["branch"] == branch:
+            if repo_list[cont]['urlRepo'] == repository and repo_list[cont]['branch'] == branch:
                 email_text = 'Repository: {repository!s} Branch: {branch!s}' \
                     .format(repository=repository, branch=branch)
-                email_data.append({"urlEnv": email_text})
+                email_data.append({'urlEnv': email_text})
                 index = cont
             else:
                 index = -1
             cont += 1
         if index >= 0:
             remove_repo = integrates_dao.remove_list_resource_dynamo(
-                "FI_projects",
-                "project_name",
+                'FI_projects',
+                'project_name',
                 project_name,
-                "repositories",
+                'repositories',
                 index)
             if remove_repo:
                 to = ['continuous@fluidattacks.com', 'projects@fluidattacks.com']
                 context = {
                     'project': project_name.upper(),
-                    'user_email': info.context.session["username"],
+                    'user_email': info.context.session['username'],
                     'action': 'Remove repositories',
                     'resources': email_data,
                     'project_url': 'https://fluidattacks.com/integrates/dashboard#!/project/{project!s}/resources'
                     .format(project=project_name)
                 }
                 threading.Thread( \
-                  name="Remove repositories email thread", \
+                  name='Remove repositories email thread', \
                   target=send_mail_repositories, \
                   args=(to, context,)).start()
                 success = True
@@ -171,32 +171,32 @@ class AddEnvironments(Mutation):
         json_data = []
 
         for envInfo in resources_data:
-            if "environment" in envInfo:
-                environment_url = envInfo.get("environment")
+            if 'environment' in envInfo:
+                environment_url = envInfo.get('environment')
                 json_data.append({
                     'urlEnv': environment_url
                 })
             else:
                 rollbar.report_message('Error: An error occurred adding environments', 'error', info.context)
         add_env = integrates_dao.add_list_resource_dynamo(
-            "FI_projects",
-            "project_name",
+            'FI_projects',
+            'project_name',
             project_name,
             json_data,
-            "environments"
+            'environments'
         )
         if add_env:
             to = ['continuous@fluidattacks.com', 'projects@fluidattacks.com']
             context = {
                 'project': project_name.upper(),
-                'user_email': info.context.session["username"],
+                'user_email': info.context.session['username'],
                 'action': 'Add environments',
                 'resources': json_data,
                 'project_url': 'https://fluidattacks.com/integrates/dashboard#!/project/{project!s}/resources'
                 .format(project=project_name)
             }
             email_send_thread = threading.Thread( \
-                                          name="Add environments email thread", \
+                                          name='Add environments email thread', \
                                           target=send_mail_repositories, \
                                           args=(to, context,))
             email_send_thread.start()
@@ -220,13 +220,13 @@ class RemoveEnvironments(Mutation):
     @require_project_access_gql
     def mutate(self, info, repository_data, project_name):
         success = False
-        environment_url = repository_data.get("urlEnv")
-        env_list = integrates_dao.get_project_dynamo(project_name)[0]["environments"]
+        environment_url = repository_data.get('urlEnv')
+        env_list = integrates_dao.get_project_dynamo(project_name)[0]['environments']
         index = -1
         cont = 0
 
         while index < 0 and len(env_list) > cont:
-            if env_list[cont]["urlEnv"] == environment_url:
+            if env_list[cont]['urlEnv'] == environment_url:
                 json_data = [env_list[cont]]
                 index = cont
             else:
@@ -234,23 +234,23 @@ class RemoveEnvironments(Mutation):
             cont += 1
         if index >= 0:
             remove_env = integrates_dao.remove_list_resource_dynamo(
-                "FI_projects",
-                "project_name",
+                'FI_projects',
+                'project_name',
                 project_name,
-                "environments",
+                'environments',
                 index)
             if remove_env:
                 to = ['continuous@fluidattacks.com', 'projects@fluidattacks.com']
                 context = {
                     'project': project_name.upper(),
-                    'user_email': info.context.session["username"],
+                    'user_email': info.context.session['username'],
                     'action': 'Remove environments',
                     'resources': json_data,
                     'project_url': 'https://fluidattacks.com/integrates/dashboard#!/project/{project!s}/resources'
                     .format(project=project_name)
                 }
                 email_send_thread = threading.Thread( \
-                                              name="Remove environments email thread", \
+                                              name='Remove environments email thread', \
                                               target=send_mail_repositories, \
                                               args=(to, context,))
                 email_send_thread.start()
