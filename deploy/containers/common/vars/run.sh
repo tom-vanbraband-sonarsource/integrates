0f4ceb922970ca94d15479058a6fe753907dca82
Andres Cuberos <acuberos@fluidattacks.com>
2018-11-19 15:34:56 -0500

    Run cron tasks only in bot container

--- deploy/containers/common/vars/run.sh
+++ deploy/containers/common/vars/run.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-env | egrep 'VAULT.*'  >> /etc/environment
-/etc/init.d/td-agent restart 
+env | egrep 'VAULT.*'  >> /etc/environment 
 a2ensite integrates-ssl.conf
 a2ensite 000-default.conf
 /usr/src/app/manage.py makemigrations
 /usr/src/app/manage.py migrate
-/usr/src/app/manage.py crontab add
-if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
-  /root/vars/cron.sh
+if [ -z "${FI_VERSION}" ]; then
+  /usr/src/app/manage.py crontab add
+  /usr/src/app/manage.py bot
+  if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
+    /root/vars/cron.sh
+  fi
+else
+  /etc/init.d/td-agent restart
+  /usr/sbin/apache2ctl -D FOREGROUND
 fi
-/usr/sbin/apache2ctl -D FOREGROUND
