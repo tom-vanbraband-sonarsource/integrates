218b7b94a3ed7076617307b29f8b933a5225ece0
Andres Cuberos <acuberos@fluidattacks.com>
2018-11-19 20:18:16 -0500

    Edit cron before entering bot loop

--- deploy/containers/common/vars/run.sh
+++ deploy/containers/common/vars/run.sh
@@ -8,10 +8,10 @@ a2ensite 000-default.conf
 /usr/src/app/manage.py migrate
 if [ -z "${FI_VERSION}" ]; then
   /usr/src/app/manage.py crontab add
-  /usr/src/app/manage.py bot
   if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
     /root/vars/cron.sh
   fi
+  /usr/src/app/manage.py bot
 else
   /etc/init.d/td-agent restart
   /usr/sbin/apache2ctl -D FOREGROUND
