cf13e16fbbab38c008bb12b6fc28e844cede8567
Mateo Norena <mnorena@fluidattacks.com>
2018-12-19 17:32:47 -0500

    Track deployments in New Relic

---
+++ new_relic_deployment.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Script to save a deployment record in New Relic
+#Reference: https://rpm.newrelic.com/api/explore/application_deployments/create
+
+curl -X POST 'https://api.newrelic.com/v2/applications/'$NEW_RELIC_APP_ID'/deployments.json' \
+-H 'X-Api-Key:'$NEW_RELIC_API_KEY -i \
+-H 'Content-Type:application/json' \
+-d \
+  '{
+    "deployment": {
+      "revision": "'$CI_COMMIT_SHA'",
+      "changelog": "'$CI_COMMIT_MESSAGE'"
+      "description": "'$NEW_RELIC_ENVIRONMENT'",
+      "user": "'$CI_COMMIT_REF_NAME'"
+    }
+  }'
