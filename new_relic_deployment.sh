a6751553890e92ce6bd09bf00969ec56bcb267fd
Mateo Norena <mnorena@fluidattacks.com>
2018-12-19 18:25:19 -0500

    Add double quotes in new_relic_deployment.sh

--- new_relic_deployment.sh
+++ new_relic_deployment.sh
@@ -3,15 +3,15 @@
 #Script to save a deployment record in New Relic
 #Reference: https://rpm.newrelic.com/api/explore/application_deployments/create
 
-curl -X POST 'https://api.newrelic.com/v2/applications/'$NEW_RELIC_APP_ID'/deployments.json' \
--H 'X-Api-Key:'$NEW_RELIC_API_KEY -i \
+curl -X POST 'https://api.newrelic.com/v2/applications/'"$NEW_RELIC_APP_ID"'/deployments.json' \
+-H 'X-Api-Key:'"$NEW_RELIC_API_KEY" -i \
 -H 'Content-Type:application/json' \
 -d \
   '{
     "deployment": {
-      "revision": "'$CI_COMMIT_SHA'",
-      "changelog": "'$CI_COMMIT_MESSAGE'"
-      "description": "'$NEW_RELIC_ENVIRONMENT'",
-      "user": "'$CI_COMMIT_REF_NAME'"
+      "revision": "'"$CI_COMMIT_SHA"'",
+      "changelog": "'"$CI_COMMIT_MESSAGE"'"
+      "description": "'"$NEW_RELIC_ENVIRONMENT"'",
+      "user": "'"$CI_COMMIT_REF_NAME"'"
     }
   }'
