74100cbe05c9465cad59779b9b66e0363be9f302
Andres Roldan <aroldan@fluidattacks.com>
2019-01-16 12:46:41 -0500

    test(front): #1034 calculate total coverage

--- front/runTests.sh
+++ front/runTests.sh
@@ -3,9 +3,10 @@
 testFiles=$(find src/ -name '*.spec.tsx')
 exitCode=0
 
+rm -f coverage_results.txt
 for test in $testFiles
 do
-  nyc mocha -r ignore-styles -r ts-node/register -r ./jsdom.js "$test";
+  nyc mocha -r ignore-styles -r ts-node/register -r ./jsdom.js "$test" | tee -a coverage_results.txt
   exitCode=$?
 
   if [ "$exitCode" -ne 0 ]
@@ -14,4 +15,6 @@ do
   fi
 done
 
+grep ^Statements coverage_results.txt | awk '{print $5}' | awk -F"/" 'BEGIN { max = -inf } { if ($1 > 0+max){ max = $1; c = $1; t = $2 } }  END { print "TOTAL     "t"     "(t - c)"     "(c/t) * 100"%" }'
+rm -f coverage_results.txt
 exit $exitCode
