43e1973eac71750c1033ab74ececb817663384aa
Andres Roldan <aroldan@fluidattacks.com>
2019-01-16 14:38:39 -0500

    test(front): #1034 run mocha recursively

--- front/runTests.sh
+++ front/runTests.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-testFiles=$(find src/ -name '*.spec.tsx')
 exitCode=0
 
 rm -f coverage_results.txt
-for test in $testFiles
-do
-  nyc mocha -r ignore-styles -r ts-node/register -r ./jsdom.js "$test" | tee -a coverage_results.txt
-  exitCode=$?
-
-  if [ "$exitCode" -ne 0 ]
-  then
-    break
-  fi
-done
+nyc mocha --recursive -r ignore-styles -r ts-node/register -r ./jsdom.js "src/**/*.spec.tsx" | tee -a coverage_results.txt
+exitCode=$?
 
 grep ^Statements coverage_results.txt | awk '{print $5}' | awk -F"/" 'BEGIN { max = -inf } { if ($1 > 0+max){ max = $1; c = $1; t = $2 } }  END { print "TOTAL     "t"     "(t - c)"     "(c/t) * 100"%" }'
 rm -f coverage_results.txt
