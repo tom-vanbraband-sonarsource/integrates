a1c14a8d24a8760a687c4c4995b4e7a7a0b96b99
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-01-23 17:12:35 -0500

    feat(front): #1006.34 add form structure

--- front/src/scenes/Dashboard/containers/DescriptionView/formStructure.tsx
+++ front/src/scenes/Dashboard/containers/DescriptionView/formStructure.tsx
@@ -5,6 +5,7 @@ import { dropdownField, textAreaField, textField } from "../../../../utils/forms
 import translate from "../../../../utils/translations/translate";
 import { required } from "../../../../utils/validations";
 import { FormRows } from "../../components/GenericForm/index";
+import { vulnsView as VulnerabilitiesView } from "../../components/Vulnerabilities";
 import { IDescriptionViewProps } from "./index";
 
 export const getFormStructure: ((props: IDescriptionViewProps) => FormRows) =
@@ -140,6 +141,166 @@ export const getFormStructure: ((props: IDescriptionViewProps) => FormRows) =
       label: translate.t("search_findings.tab_description.requirements"),
       renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
       value: props.dataset.requirements,
+      visible: true,
+    }],
+    [{
+      customElement: (
+        <VulnerabilitiesView
+          dataInputs={[]}
+          dataLines={[]}
+          dataPorts={[]}
+          releaseDate={props.dataset.releaseDate}
+          editMode={props.isEditing && _.includes(["admin", "analyst"], props.userRole)}
+          findingId={props.findingId}
+          state="open"
+        />),
+      label: translate.t("search_findings.tab_description.where"),
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "attackVector",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.attack_vectors"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.attackVector,
+      visible: true,
+    },
+     {
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "threat",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.threat"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.threat,
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "recommendation",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.recommendation"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.recommendation,
+      visible: true,
+    },
+     {
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "affectedSystems",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.affected_systems"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.affectedSystems,
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "compromisedAttributes",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.compromised_attrs"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.compromisedAttributes,
+      visible: true,
+    },
+     {
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "compromisedRecords",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.compromised_records"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.compromisedRecords,
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        component: textField,
+        name: "cweUrl",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.weakness"),
+      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
+      value: props.dataset.cweUrl,
+      visible: true,
+    },
+     {
+      componentProps: {
+        component: textField,
+        name: "btsUrl",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.bts"),
+      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
+      value: props.dataset.btsUrl,
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        children: (
+          <React.Fragment>
+            <option value="" selected={true} />
+            <option value="Asumido">
+              {translate.t("search_findings.tab_description.treatment.assumed")}
+            </option>
+            <option value="Nuevo">
+              {translate.t("search_findings.tab_description.treatment.new")}
+            </option>
+            <option value="Remediar">
+              {translate.t("search_findings.tab_description.treatment.in_progress")}
+            </option>
+          </React.Fragment>),
+        component: dropdownField,
+        name: "treatment",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.treatment.title"),
+      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
+      value: props.dataset.treatment,
+      visible: true,
+    },
+     {
+      componentProps: { name: "treatmentManager" },
+      label: translate.t("search_findings.tab_description.treatment_mgr"),
+      renderAsEditable: false,
+      value: props.dataset.treatmentManager,
+      visible: true,
+    }],
+    [{
+      componentProps: {
+        className: globalStyle.noResize,
+        component: textAreaField,
+        name: "treatmentJustification",
+        type: "text",
+        validate: [required],
+      },
+      label: translate.t("search_findings.tab_description.treatment_just"),
+      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
+      value: props.dataset.treatmentJustification,
       visible: true,
     }],
   ];
