f2124b1957df1531c54d26033172527fed2f8587
Andres Roldan <aroldan@fluidattacks.com>
2019-01-24 10:06:04 -0500

    test(back): #1067 remove test keys from cache

---
+++ remove_keys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {1..10}; do
+	for pattern in bwapp unittesting; do
+		for key in $(redis-cli -c -h ${FI_REDIS_SERVER} --scan --pattern *${pattern}*); do
+			echo "Removing key ${key}..."
+			redis-cli -c -h ${FI_REDIS_SERVER} unlink ${key}
+		done
+	done
+done
