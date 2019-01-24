f1200c6ec676fa257d56a8896f71b56a0190c79e
Andres Roldan <aroldan@fluidattacks.com>
2019-01-24 11:12:50 -0500

    test(back): #1067 remove keys from oneshottest

--- remove_keys.sh
+++ remove_keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in {1..10}; do
-	for pattern in bwapp unittesting; do
+	for pattern in bwapp unittesting oneshottest; do
 		for key in $(redis-cli -c -h ${FI_REDIS_SERVER} --scan --pattern *${pattern}*); do
 			echo "Removing key ${key}..."
 			redis-cli -c -h ${FI_REDIS_SERVER} unlink ${key}
