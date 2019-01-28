8616f48a567ab00d79505989acbd0a916ce3d120
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-01-28 09:46:46 -0500

    feat(front): #1006.45 create component base

--- front/src/scenes/Dashboard/containers/DescriptionView/index.tsx
+++ front/src/scenes/Dashboard/containers/DescriptionView/index.tsx
@@ -1,3 +1,26 @@
+/* tslint:disable jsx-no-lambda number-literal-format jsx-no-multiline-js
+ * JSX-NO-LAMBDA: Disabling this rule is necessary for the sake of simplicity and
+ * readability of the code that binds click events
+ *
+ * JSX-NO-MULTILINE-JS: necessary for the sake of readability of the code that dynamically renders fields
+ * as input or <p> depending on their state
+ */
+
+import _ from "lodash";
+import React from "react";
+import { Button, Col, Glyphicon, Row } from "react-bootstrap";
+import { InferableComponentEnhancer, lifecycle } from "recompose";
+import { AnyAction, Reducer } from "redux";
+import { submit } from "redux-form";
+import { ThunkDispatch } from "redux-thunk";
+import { StateType } from "typesafe-actions";
+import { confirmDialog as ConfirmDialog } from "../../../../components/ConfirmDialog";
+import store from "../../../../store/index";
+import reduxWrapper from "../../../../utils/reduxWrapper";
+import translate from "../../../../utils/translations/translate";
+import { remediationModal as RemediationModal } from "../../components/RemediationModal/index";
+import * as actions from "./actions";
+
 export interface IDescriptionViewProps {
   dataset: {
     actor: string;
@@ -30,3 +53,130 @@ export interface IDescriptionViewProps {
   userRole: string;
 }
 
+const enhance: InferableComponentEnhancer<{}> =
+  lifecycle({
+    componentDidMount(): void {
+      const { findingId, projectName } = this.props as IDescriptionViewProps;
+      const thunkDispatch: ThunkDispatch<{}, {}, AnyAction> = (
+        store.dispatch as ThunkDispatch<{}, {}, AnyAction>
+      );
+
+      thunkDispatch(actions.loadDescription(findingId, projectName));
+    },
+  });
+
+const renderMarkVerifiedBtn: (() => JSX.Element) = (): JSX.Element => (
+  <Row style={{ paddingTop: "10px" }}>
+    <Col md={2} mdOffset={10} xs={6} sm={6}>
+      <Button bsStyle="warning" block={true} onClick={(): void => { store.dispatch(actions.openConfirmMdl()); }}>
+        <Glyphicon glyph="ok" /> {translate.t("search_findings.tab_description.mark_verified")}
+      </Button>
+    </Col>
+  </Row>
+);
+
+const renderRequestVerifiyBtn: ((props: IDescriptionViewProps) => JSX.Element) =
+  (props: IDescriptionViewProps): JSX.Element => {
+    const canRequestVerification: boolean =
+      props.dataset.state === "Abierto"
+      && _.includes(["continuous", "Continua", "Concurrente", "Si"], props.dataset.subscription)
+      && props.dataset.treatment !== "Asumido";
+
+    return (
+      <Row style={{ paddingTop: "10px" }}>
+        <Col md={2} mdOffset={10} xs={6} sm={6}>
+          <Button
+            bsStyle="success"
+            block={true}
+            disabled={!canRequestVerification}
+            onClick={(): void => { store.dispatch(actions.openRemediationMdl()); }}
+          >
+            <Glyphicon glyph="ok" /> {translate.t("search_findings.tab_description.request_verify")}
+          </Button>
+        </Col>
+      </Row>
+    );
+  };
+
+const renderUpdateBtn: (() => JSX.Element) = (): JSX.Element => (
+  <Button bsStyle="success" block={true} onClick={(): void => { store.dispatch(submit("editDescription")); }}>
+    <Glyphicon glyph="repeat" /> {translate.t("search_findings.tab_description.update")}
+  </Button>
+);
+
+const renderEditBtn: (() => JSX.Element) = (): JSX.Element => (
+  <Col md={2} xs={6} sm={6}>
+    <Button bsStyle="primary" block={true} onClick={(): void => { store.dispatch(actions.editDescription()); }}>
+      <Glyphicon glyph="edit" /> {translate.t("search_findings.tab_description.editable")}
+    </Button>
+  </Col>
+);
+
+const renderActionButtons: ((props: IDescriptionViewProps) => JSX.Element) =
+  (props: IDescriptionViewProps): JSX.Element => (
+    <React.Fragment>
+      <Col md={12} sm={12} xs={12}>
+        <Row style={{ paddingTop: "10px" }}>
+          <Col md={2} mdOffset={8} xs={6} sm={6}>
+            {_.includes(["admin", "analyst"], props.userRole) && props.isEditing ? renderUpdateBtn() : undefined}
+          </Col>
+          {renderEditBtn()}
+        </Row>
+        {_.includes(["admin", "analyst"], props.userRole) && props.dataset.remediated
+          ? renderMarkVerifiedBtn() : undefined}
+        {_.includes(["admin", "customer"], props.userRole) ? renderRequestVerifiyBtn(props) : undefined}
+      </Col>
+    </React.Fragment>
+  );
+
+const renderForm: ((props: IDescriptionViewProps) => JSX.Element) =
+  (props: IDescriptionViewProps): JSX.Element => (
+    <React.Fragment>
+      <Col md={12} sm={12} xs={12}>
+        <Row>{renderActionButtons(props)}</Row>
+        <Row style={{ paddingTop: "10px" }}>
+          {props.isEditing && _.includes(["customer", "customeradmin"], props.userRole) ? renderUpdateBtn() : undefined}
+        </Row>
+      </Col>
+    </React.Fragment>
+  );
+
+export const component: React.SFC<IDescriptionViewProps> =
+  (props: IDescriptionViewProps): JSX.Element => (
+    <React.StrictMode>
+      <Row>
+        <React.Fragment>
+          {renderForm(props)}
+          <RemediationModal
+            isOpen={props.isRemediationOpen}
+            onClose={(): void => { store.dispatch(actions.closeRemediationMdl()); }}
+            onSubmit={(values: { treatmentJustification: string }): void => {
+              const thunkDispatch: ThunkDispatch<{}, {}, AnyAction> = (
+                store.dispatch as ThunkDispatch<{}, {}, AnyAction>
+              );
+              thunkDispatch(actions.requestVerification(props.findingId, values.treatmentJustification));
+            }}
+          />
+          <ConfirmDialog
+            open={props.isMdlConfirmOpen}
+            title={translate.t("confirmmodal.title_generic")}
+            onProceed={(): void => {
+              const thunkDispatch: ThunkDispatch<{}, {}, AnyAction> = (
+                store.dispatch as ThunkDispatch<{}, {}, AnyAction>
+              );
+              thunkDispatch(actions.verifyFinding(props.findingId));
+            }}
+            onCancel={(): void => { store.dispatch(actions.closeConfirmMdl()); }}
+          />
+        </React.Fragment>
+      </Row>
+    </React.StrictMode>
+  );
+
+export const descriptionView: React.ComponentType<IDescriptionViewProps> = reduxWrapper(
+  enhance(component) as React.SFC<IDescriptionViewProps>,
+  (state: StateType<Reducer>): IDescriptionViewProps => ({
+    ...state.dashboard.description,
+    isMdlConfirmOpen: state.dashboard.isMdlConfirmOpen,
+  }),
+);
