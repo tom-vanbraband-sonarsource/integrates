bc0baf17424f04cf30db2e7f16465c2188d62b3c
Andres Roldan <aroldan@fluidattacks.com>
2019-02-07 16:19:07 -0500

    test(front): #1103 add codecov to react tests

--- front/runTests.sh
+++ front/runTests.sh
@@ -4,8 +4,10 @@ exitCode=0
 
 rm -f coverage_results.txt
 nyc mocha --recursive -r ignore-styles -r ts-node/register -r ./jsdom.js "src/**/*.spec.tsx" | tee -a coverage_results.txt
+nyc report --reporter=text-lcov > coverage.lcov
 exitCode=$?
 
 grep ^Statements coverage_results.txt | awk '{print $5}' | awk -F"/" 'BEGIN { max = -inf } { if ($1 > 0+max){ max = $1; c = $1; t = $2 } }  END { print "TOTAL     "t"     "(t - c)"     "(c/t) * 100"%" }'
 rm -f coverage_results.txt
+codecov --token=${CODECOV_TOKEN}
 exit $exitCode
