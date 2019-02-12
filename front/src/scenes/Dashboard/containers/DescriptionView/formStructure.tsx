62aa210684d30636dfd64e9bdbe0ca0ceef22311
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-12 17:49:10 -0500

    refac(front): #1116 remove unused code

--- front/src/scenes/Dashboard/containers/DescriptionView/formStructure.tsx
+++ front/src/scenes/Dashboard/containers/DescriptionView/formStructure.tsx
@@ -1,80 +1,13 @@
 import _ from "lodash";
 import React from "react";
 import globalStyle from "../../../../styles/global.css";
-import { formatCweUrl, formatDropdownField } from "../../../../utils/formatHelpers";
+import { formatDropdownField } from "../../../../utils/formatHelpers";
 import { dropdownField, textAreaField, textField } from "../../../../utils/forms/fields";
 import translate from "../../../../utils/translations/translate";
-import { numeric, required } from "../../../../utils/validations";
+import { required } from "../../../../utils/validations";
 import { FormRows, IEditableField } from "../../components/GenericForm/index";
 import { vulnsView as VulnerabilitiesView } from "../../components/Vulnerabilities";
 import { IDescriptionViewProps } from "./index";
-
-const renderReportLevelField: ((props: IDescriptionViewProps) => IEditableField) =
-  (props: IDescriptionViewProps): IEditableField => ({
-    componentProps: {
-      children: (
-        <React.Fragment>
-          <option value="" selected={true} />
-          <option value="DETAILED">
-            {translate.t("search_findings.tab_description.reportLevel.detailed")}
-          </option>
-          <option value="GENERAL">
-            {translate.t("search_findings.tab_description.reportLevel.general")}
-          </option>
-        </React.Fragment>),
-      component: dropdownField,
-      name: "reportLevel",
-      validate: [required],
-    },
-    label: translate.t("search_findings.tab_description.reportLevel.title"),
-    renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-    value: props.dataset.reportLevel,
-    visible: props.isEditing,
-  });
-
-const renderFindingTypeField: ((props: IDescriptionViewProps) => IEditableField) =
-  (props: IDescriptionViewProps): IEditableField => ({
-    componentProps: {
-      children: (
-        <React.Fragment>
-          <option value="" selected={true} />
-          <option value="SECURITY">{translate.t("search_findings.tab_description.type.security")}</option>
-          <option value="HYGIENE">{translate.t("search_findings.tab_description.type.hygiene")}</option>
-        </React.Fragment>),
-      component: dropdownField,
-      name: "type",
-      validate: [required],
-    },
-    label: translate.t("search_findings.tab_description.type.title"),
-    renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-    value: translate.t(`search_findings.tab_description.type.${props.dataset.type.toLowerCase()}`),
-    visible: true,
-  });
-
-const renderTreatmentMgrField: ((props: IDescriptionViewProps) => IEditableField) =
-  (props: IDescriptionViewProps): IEditableField => {
-    const options: JSX.Element[] =
-      props.dataset.userEmails.map(({ email }: { email: string }, index: number): JSX.Element =>
-        <option key={index} value={email}>{email}</option>,
-      );
-
-    return {
-      componentProps: {
-        children: (
-          <React.Fragment>
-            <option value="" selected={true} />
-            {options}
-          </React.Fragment>),
-        component: dropdownField,
-        name: "treatmentManager",
-        validate: [...props.formValues.treatment === "Remediar" ? [required] : []],
-      },
-      label: translate.t("search_findings.tab_description.treatment_mgr"),
-      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
-      value: props.dataset.treatmentManager,
-      visible: !props.isEditing || (props.isEditing && props.formValues.treatment === "Remediar"),
-    };
-  };
 
 const renderRiskField: ((props: IDescriptionViewProps) => IEditableField) =
   (props: IDescriptionViewProps): IEditableField => ({
@@ -93,7 +26,6 @@ const renderRiskField: ((props: IDescriptionViewProps) => IEditableField) =
 
 const renderAnalystEditableFields: ((props: IDescriptionViewProps) => FormRows) =
   (props: IDescriptionViewProps): FormRows => [
-    [renderReportLevelField(props), renderFindingTypeField(props)],
     [{
       componentProps: {
         component: textField,
@@ -262,126 +194,10 @@ const renderAnalystEditableFields: ((props: IDescriptionViewProps) => FormRows) 
       visible: true,
     },
      renderRiskField(props)],
-    [{
-      componentProps: {
-        className: globalStyle.noResize,
-        component: textAreaField,
-        name: "recommendation",
-        type: "text",
-        validate: [required],
-      },
-      label: translate.t("search_findings.tab_description.recommendation"),
-      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-      value: props.dataset.recommendation,
-      visible: true,
-    }],
-    [{
-      componentProps: {
-        className: globalStyle.noResize,
-        component: textAreaField,
-        name: "compromisedAttributes",
-        type: "text",
-      },
-      label: translate.t("search_findings.tab_description.compromised_attrs"),
-      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-      value: props.dataset.compromisedAttributes,
-      visible: true,
-    },
-     {
-      componentProps: {
-        className: globalStyle.noResize,
-        component: textAreaField,
-        name: "compromisedRecords",
-        type: "text",
-        validate: [required, numeric],
-      },
-      label: translate.t("search_findings.tab_description.compromised_records"),
-      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-      value: props.dataset.compromisedRecords,
-      visible: true,
-    }],
-    [{
-      componentProps: {
-        component: textField,
-        name: "cweUrl",
-        type: "number",
-        validate: [required, numeric],
-      },
-      label: translate.t("search_findings.tab_description.weakness"),
-      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-      value: formatCweUrl(props.dataset.cweUrl),
-      visible: true,
-    },
-     {
-      componentProps: {
-        component: textField,
-        name: "kbUrl",
-        type: "text",
-      },
-      label: translate.t("search_findings.tab_description.kb"),
-      renderAsEditable: props.isEditing && _.includes(["admin", "analyst"], props.userRole),
-      value: props.dataset.kbUrl,
-      visible: props.dataset.kbUrl !== "" || props.isEditing,
-    }],
-  ];
-
-const renderCustomerEditableFields: ((props: IDescriptionViewProps) => FormRows) =
-  (props: IDescriptionViewProps): FormRows => [
-    [{
-      componentProps: {
-        component: textField,
-        name: "btsUrl",
-        type: "text",
-      },
-      label: translate.t("search_findings.tab_description.bts"),
-      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
-      value: props.dataset.btsUrl,
-      visible: true,
-    }],
-    [{
-      componentProps: {
-        children: (
-          <React.Fragment>
-            <option value="" selected={true} />
-            <option value="Asumido">
-              {translate.t("search_findings.tab_description.treatment.assumed")}
-            </option>
-            <option value="Nuevo">
-              {translate.t("search_findings.tab_description.treatment.new")}
-            </option>
-            <option value="Remediar">
-              {translate.t("search_findings.tab_description.treatment.in_progress")}
-            </option>
-          </React.Fragment>),
-        component: dropdownField,
-        name: "treatment",
-        validate: [required],
-      },
-      label: translate.t("search_findings.tab_description.treatment.title"),
-      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
-      value: props.dataset.treatment,
-      visible: true,
-    },
-     renderTreatmentMgrField(props)],
-    [{
-      componentProps: {
-        className: globalStyle.noResize,
-        component: textAreaField,
-        name: "treatmentJustification",
-        type: "text",
-        validate: [required],
-      },
-      label: translate.t("search_findings.tab_description.treatment_just"),
-      renderAsEditable: props.isEditing && _.includes(["customer", "customeradmin"], props.userRole),
-      value: props.dataset.treatmentJustification,
-      visible: true,
-    }],
   ];
 
 export const getFormStructure: ((props: IDescriptionViewProps) => FormRows) =
   (props: IDescriptionViewProps): FormRows => [
     ...(!props.isEditing || (props.isEditing && _.includes(["admin", "analyst"], props.userRole))
       ? renderAnalystEditableFields(props) : []),
-    ...(!props.isEditing || (props.isEditing && _.includes(["customer", "customeradmin"], props.userRole))
-      ? renderCustomerEditableFields(props) : []),
   ];
