a9f376f3a8eb0b551813258e8bdae1f8d738a733
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-13 15:36:28 -0500

    refac(front): #1116 remove unused code

--- front/src/scenes/Dashboard/containers/DescriptionView/formStructure.tsx
+++
@@ -1,6 +0,0 @@
-import { FormRows, IEditableField } from "../../components/GenericForm/index";
-import { IDescriptionViewProps } from "./index";
-export const getFormStructure: ((props: IDescriptionViewProps) => FormRows) =
-  (props: IDescriptionViewProps): FormRows => [
-    
-  ];
