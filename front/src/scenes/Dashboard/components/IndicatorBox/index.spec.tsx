2a5485c9f99d1428d47fbdf33cbe6a35523b1588
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-15 17:07:43 -0500

    test(front): #1039 apply ts linter rules
    - fix tests for IndicatorBox and SimpleTable
    - 163 incidences left

--- front/src/scenes/Dashboard/components/IndicatorBox/index.spec.tsx
+++ front/src/scenes/Dashboard/components/IndicatorBox/index.spec.tsx
@@ -1,75 +1,62 @@
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'chai';
-import IndicatorBox from './index';
-import 'mocha'
-import React from 'react';
+import { expect } from "chai";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import React from "react";
 import { Col } from "react-bootstrap";
-import style from "./index.css";
+import { default as IndicatorBox } from "./index";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Indicator Box', () => {
+describe("Indicator Box", () => {
 
-  it('should return a function', () => {
-    expect(typeof(IndicatorBox)).to.equal('function');
+  it("should return a function", () => {
+    expect(typeof (IndicatorBox)).to
+      .equal("function");
   });
 
-  it('should have icon', () => {
-    const wrapper = shallow((
-      <IndicatorBox 
-        backgroundColor="#070"
-        color="#700"
-        icon="fa fa-star"
-        name="Unit test"
-        quantity="666"
-        title="Unit title"
-      />
-    ));
-    expect(wrapper.contains(<span className="fa fa-star"/>)).to.equal(true);
-  });
- 
-  it('should be render', () => {
-    const wrapper = shallow(
-      <IndicatorBox 
-        backgroundColor="#070"
-        color="#700"
-        icon="fa fa-star"
-        name="Unit test"
-        quantity="666"
-        title="Unit title"
-      />
-    );
+  const wrapper: ShallowWrapper = shallow((
+    <IndicatorBox
+      backgroundColor="#070"
+      color="#700"
+      icon="fa fa-star"
+      name="Unit test"
+      quantity="666"
+      title="Unit title"
+    />
+  ));
 
+  it("should have icon", () => {
+    expect(wrapper.contains(<span className="fa fa-star" />)).to
+      .equal(true);
+  });
+
+  it("should render", () => {
     expect(
-      wrapper.contains(
-        <Col xs={12} md={3}>
-          <div
-            className={style.widgetbox}
-            data-toggle="tooltip"
-            data-placement="top"
-            title="Unit title"
-            style={{backgroundColor: "#070", color: "#700"}}
-          >
-            <Col xs={4} md={4}>
-              <div className={style.widgeticon}>
-                  <span className="fa fa-star"/>
-              </div>
-            </Col>
-            <Col xs={8} md={8}>
-              <div
-                data-toggle="counter"
-                className={style.widgetvalue}
-              >
-                666
-              </div>
-              <div className={style.widgetdesc}>
-                Unit test
-              </div>
-            </Col>
-          </div>
-        </Col>
-      )
-    ).to.equal(true);
+      wrapper.contains([
+        (
+          <Col xs={4} md={4}>
+            <div>
+              <span className="fa fa-star" />
+            </div>
+          </Col>),
+        (
+          <Col xs={8} md={8}>
+            <div data-toggle="counter">
+              666
+            </div>
+            <div>
+              Unit test
+            </div>
+          </Col>),
+      ]),
+    ).to
+      .equal(true);
+  });
+
+  it("should render specified colors", () => {
+    expect(JSON.stringify(wrapper.find('[title="Unit title"]')
+      .prop<React.CSSProperties>("style"))).to
+      .equal(JSON.stringify({ backgroundColor: "#070", color: "#700" }));
   });
 });
--- front/src/scenes/Dashboard/components/SimpleTable/index.spec.tsx
+++ front/src/scenes/Dashboard/components/SimpleTable/index.spec.tsx
@@ -1,47 +1,51 @@
-import * as React from 'react';
-import { expect } from 'chai';
-import { shallow, configure } from 'enzyme';
-import SimpleTable from './index';
-import { DataAlignType } from 'react-bootstrap-table';
-import 'mocha';
-import Adapter from 'enzyme-adapter-react-16';
+import { expect } from "chai";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import React from "react";
+import { DataAlignType } from "react-bootstrap-table";
+import { IHeader } from "../../../../components/DataTable";
+import { default as SimpleTable } from "./index";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Simple table', () => {
+const functionMock: (() => void) = (): void => undefined;
 
-  it('should return a function', () => {
-    expect(typeof(SimpleTable)).to.equal('function');
+describe("Simple table", () => {
+
+  it("should return a function", () => {
+    expect(typeof (SimpleTable)).to
+      .equal("function");
   });
 
-  it('should render table', () => {
-    const data = [
+  it("should render table", () => {
+    const data: object[] = [
       {
-          "Test header": "value 1",
-          "Test header2": "value 2"
-      }
+        "Test header": "value 1",
+        "Test header2": "value 2",
+      },
     ];
-    const testHeaders = [
+    const testHeaders: IHeader[] = [
       {
         align: "center" as DataAlignType,
+        dataField: "test_header",
+        header: "Prueba 1",
+        isDate: false,
         isStatus: false,
         width: "5%",
         wrapped: false,
-        header: "Prueba 1",
-        dataField: "test_header",
-        isDate: false
       },
       {
         align: "center" as DataAlignType,
+        dataField: "test_header2",
+        header: "Prueba 2",
+        isDate: false,
         isStatus: false,
         width: "5%",
         wrapped: false,
-        header: "Prueba 2",
-        dataField: "test_header2",
-        isDate: false
-      }
+      },
     ];
-    const wrapper = shallow(
+    const wrapper: ShallowWrapper = shallow(
       <SimpleTable
         id="testTable"
         enableRowSelection={true}
@@ -49,12 +53,13 @@ describe('Simple table', () => {
         exportCsv={false}
         search={false}
         headers={testHeaders}
-        onClickRow={()=>{}}
+        onClickRow={functionMock}
         pageSize={25}
         title="Unit test table"
-      />
+      />,
     );
-    let component = wrapper.find(<table/>)
-    expect(component).to.exist;
+
+    expect(wrapper).to.have
+      .lengthOf(1);
   });
 });
