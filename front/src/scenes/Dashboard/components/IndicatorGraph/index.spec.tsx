fee8d0e282155879fb2ce63adc960920097874aa
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-18 09:44:33 -0500

    test(front): #1039 apply ts linter rules
    - fix tests for IndicatorGraph and RemediationModal

--- front/src/scenes/Dashboard/components/IndicatorGraph/index.spec.tsx
+++ front/src/scenes/Dashboard/components/IndicatorGraph/index.spec.tsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import IndicatorGraph from './index';
-import 'mocha'
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { expect } from "chai";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import React from "react";
+import { default as IndicatorGraph } from "./index";
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Indicator Graph', () => {
+describe("Indicator Graph", () => {
 
-  it('should return a function', () => {
-    expect(typeof(IndicatorGraph)).to.equal('function');
+  it("should return a function", () => {
+    expect(typeof (IndicatorGraph)).to
+      .equal("function");
   });
 
-  it('should have header', () => {
-    const data = {
-      labels: [
-        "Open",
-        "Close",
-      ],
+  it("should have header", () => {
+    const data: object = {
       datasets: [{
         backgroundColor: ["#ff1a1a", "#31c0be"],
         data: [6, 4],
         hoverBackgroundColor: ["#e51414", "#258c8a"],
       }],
+      labels: [
+        "Close",
+        "Open",
+      ],
     };
-    const wrapper = shallow((
+    const wrapper: ShallowWrapper = shallow((
       <IndicatorGraph
         data={data}
         name="Unit header"
       />
     ));
-    expect(wrapper.contains(<h3>Unit header</h3>)).to.equal(true);
+    expect(wrapper.contains(<h3>Unit header</h3>)).to
+      .equal(true);
   });
 
 });
--- front/src/scenes/Dashboard/components/RemediationModal/index.spec.tsx
+++ front/src/scenes/Dashboard/components/RemediationModal/index.spec.tsx
@@ -1,33 +1,37 @@
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import { expect } from "chai";
-import { remediationModal as RemediationModal } from "./index";
-import "mocha";
-import * as React from "react";
-import { createStore } from "redux";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import React from "react";
 import { Provider } from "react-redux";
+import { Action, createStore, Store } from "redux";
 import { reduxForm as ReduxForm } from "redux-form";
+import { remediationModal as RemediationModal } from "./index";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Remediation modal', () => {
+const functionMock: (() => void) = (): void => undefined;
 
-  const store = createStore(() => ({}));
-  const wrapper = shallow(
+describe("Remediation modal", () => {
+
+  const store: Store<{}, Action<{}>> = createStore(() => ({}));
+  const wrapper: ShallowWrapper = shallow(
     <Provider store={store}>
       <RemediationModal
         isOpen={true}
-        onClose={(): void => undefined}
-        onSubmit={(): void => undefined}
+        onClose={functionMock}
+        onSubmit={functionMock}
       />
-    </Provider>
+    </Provider>,
   );
 
-  it('should return a function', () => {
-    expect(typeof(RemediationModal)).to.equal('function');
+  it("should return a function", () => {
+    expect(typeof (RemediationModal)).to
+      .equal("function");
   });
 
-  it('should render', () => {
-    expect(wrapper).to.contain(ReduxForm);
+  it("should render", () => {
+    expect(wrapper).to
+      .contain(ReduxForm);
   });
 });
