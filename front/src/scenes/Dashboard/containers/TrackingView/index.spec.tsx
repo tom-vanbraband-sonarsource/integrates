963fe0b83f5fd9962dda598b3d7fe28231b270a4
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-18 14:08:40 -0500

    test(front): #1039 apply ts linter rules
    - fix tests for Tracking and Welcome views

--- front/src/scenes/Dashboard/containers/TrackingView/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/TrackingView/index.spec.tsx
@@ -1,32 +1,33 @@
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import { expect } from "chai";
-import {
-  trackingViewComponent as TrackingView,
-  closing
-} from "./index";
-import "mocha";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
 import React from "react";
-import TextareaAutosize from "react-textarea-autosize";
 import { Col, Row } from "react-bootstrap";
+import TextareaAutosize from "react-textarea-autosize";
+import {
+  closing,
+  trackingViewComponent as TrackingView,
+} from "./index";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Tracking view', () => {
+describe("Tracking view", () => {
 
-  it('should return a function', () => {
-    expect(typeof(TrackingView)).to.equal('function');
+  it("should return a function", () => {
+    expect(typeof (TrackingView)).to
+      .equal("function");
   });
 
-  it('should render vulnerabilities', () => {
+  it("should render vulnerabilities", () => {
     const testClosings: closing[] = [{
       closed: 2,
       cycle: 1,
       date: "2018-10-10",
       effectiveness: 20,
       open: 1,
-    }]
-    const wrapper = shallow(
+    }];
+    const wrapper: ShallowWrapper = shallow(
       <TrackingView
         openFindingsContent="unit/test/index.js line:32"
         closedFindingsContent="unit/main/index.js line:16"
@@ -34,69 +35,74 @@ describe('Tracking view', () => {
         findingId="422286126"
         hasNewVulnerabilities={false}
         userRole="admin"
-      />
+      />,
     );
     expect(
       wrapper.contains(
         <Row>
-        <React.Fragment>
-          <Col
-            md={12}
-            componentClass="div"
-          >
-            <p>search_findings.tab_tracking.open</p>
-            <TextareaAutosize
-              disabled={true}
-              value="unit/test/index.js line:32"
-            />
-          </Col>
-          <Col
-            md={12}
-            componentClass="div"
-          >
-            <p>search_findings.tab_tracking.closed</p>
-            <TextareaAutosize
-              disabled={true}
-              value="unit/main/index.js line:16"
-            />
-          </Col>
+          <React.Fragment>
+            <Col
+              md={12}
+              componentClass="div"
+            >
+              <p>search_findings.tab_tracking.open</p>
+              <TextareaAutosize
+                disabled={true}
+                value="unit/test/index.js line:32"
+              />
+            </Col>
+            <Col
+              md={12}
+              componentClass="div"
+            >
+              <p>search_findings.tab_tracking.closed</p>
+              <TextareaAutosize
+                disabled={true}
+                value="unit/main/index.js line:16"
+              />
+            </Col>
           </React.Fragment>
-        </Row>
-      )
-    ).to.equal(true);
+        </Row>,
+      ),
+    ).to
+      .equal(true);
   });
-  it('should render closings timeline', () => {
+  it("should render closings timeline", () => {
     const testClosings: closing[] = [{
       closed: 0,
       cycle: 0,
       date: "2018-10-10",
       effectiveness: 0,
       open: 4,
-    }]
-    const wrapper = shallow(
+    }];
+    const wrapper: ShallowWrapper = shallow(
       <TrackingView
         closings={testClosings}
         findingId="422286126"
         hasNewVulnerabilities={true}
         userRole="admin"
-      />
+      />,
     );
-    expect(
-      wrapper.find('ul').contains([
+
+    const expectedText: string = "search_findings.tab_tracking.founded,search_findings.tab_tracking.open: 4,"
+      + "search_findings.tab_tracking.closed: 0";
+
+    expect(wrapper.find("ul")
+      .contains([(
         <li className="undefined undefined">
           <div>
             <span>
               2018-10-10
-            </span>
+          </span>
           </div>
           <div>
             <p>
-              search_findings.tab_tracking.founded,search_findings.tab_tracking.open: 4,search_findings.tab_tracking.closed: 0
+              {expectedText}
             </p>
           </div>
         </li>
-      ])
-    ).to.equal(true);
-
- });
+      )]),
+    ).to
+      .equal(true);
+  });
 });
--- front/src/scenes/Registration/containers/WelcomeView/index.spec.tsx
+++ front/src/scenes/Registration/containers/WelcomeView/index.spec.tsx
@@ -1,83 +1,75 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import { component as WelcomeView } from './index';
-import 'mocha';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { expect } from "chai";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import React from "react";
+import { component as WelcomeView } from "./index";
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
 
-describe('Welcome view', () => {
-  it('should return a function', () => {
-    expect(typeof (WelcomeView)).to.equal('function');
+describe("Welcome view", () => {
+  it("should return a function", () => {
+    expect(typeof (WelcomeView)).to
+      .equal("function");
   });
 
-  it('should render', () => {
-    const wrapper = shallow(
+  it("should render", () => {
+    const wrapper: ShallowWrapper = shallow(
       <WelcomeView
         email={"unittesting@fluidattacks.com"}
         isAuthorized={false}
         isRememberEnabled={false}
-        legalNotice={{
-          open: true,
-          rememberDecision: false,
-        }}
+        legalNotice={{ open: true, rememberDecision: false }}
         username={"Test"}
-      />
+      />,
     );
 
-    expect(wrapper).to.have.length(1);
+    expect(wrapper).to.have
+      .length(1);
   });
 
-  it('should render greetings message', () => {
-    const wrapper = shallow(
+  it("should render greetings message", () => {
+    const wrapper: ShallowWrapper = shallow(
       <WelcomeView
         email={"unittesting@fluidattacks.com"}
         isAuthorized={false}
         isRememberEnabled={false}
-        legalNotice={{
-          open: true,
-          rememberDecision: false,
-        }}
+        legalNotice={{ open: true, rememberDecision: false }}
         username={"Test"}
-      />
+      />,
     );
 
-    expect(wrapper.contains(<h1>registration.greeting Test!</h1>)).to.equal(true);
+    expect(wrapper.contains(<h1>registration.greeting Test!</h1>)).to
+      .equal(true);
   });
 
-  it('should render unauthorized message', () => {
-    const wrapper = shallow(
+  it("should render unauthorized message", () => {
+    const wrapper: ShallowWrapper = shallow(
       <WelcomeView
         email={"unittesting@fluidattacks.com"}
         isAuthorized={false}
         isRememberEnabled={false}
-        legalNotice={{
-          open: true,
-          rememberDecision: false,
-        }}
+        legalNotice={{ open: true, rememberDecision: false }}
         username={"Test"}
-      />
+      />,
     );
 
-    expect(wrapper.contains(<p>registration.unauthorized</p>)).to.equal(true);
+    expect(wrapper.contains(<p>registration.unauthorized</p>)).to
+      .equal(true);
   });
 
-  it('should render legal notice', () => {
-    const wrapper = shallow(
+  it("should render legal notice", () => {
+    const wrapper: ShallowWrapper = shallow(
       <WelcomeView
         email={"unittesting@fluidattacks.com"}
         isAuthorized={true}
         isRememberEnabled={false}
-        legalNotice={{
-          open: true,
-          rememberDecision: false,
-        }}
+        legalNotice={{ open: true, rememberDecision: false }}
         username={"Test"}
-      />
+      />,
     );
 
-    expect(wrapper.find({ id: "legalNotice" }).length).to.equal(1);
+    expect(wrapper.find({ id: "legalNotice" }).length).to
+      .equal(1);
   });
 });
