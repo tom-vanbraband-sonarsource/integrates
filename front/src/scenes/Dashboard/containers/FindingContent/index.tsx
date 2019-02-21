4fbb60e5d713aa30444333e4a20938fb37b1fecf
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-21 17:49:58 -0500

    feat(front): #1139.3 add tabs render functions

--- front/src/scenes/Dashboard/containers/FindingContent/index.tsx
+++ front/src/scenes/Dashboard/containers/FindingContent/index.tsx
@@ -1,17 +1,73 @@
+import _ from "lodash";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route } from "react-router-dom";
+import { commentsView as CommentsView } from "../CommentsView/index";
+import { descriptionView as DescriptionView } from "../DescriptionView/index";
+import { evidenceView as EvidenceView } from "../EvidenceView/index";
+import { exploitView as ExploitView } from "../ExploitView/index";
+import { recordsView as RecordsView } from "../RecordsView/index";
+import { severityView as SeverityView } from "../SeverityView/index";
+import { trackingView as TrackingView } from "../TrackingView/index";
 import style from "./index.css";
 
-const renderDescription: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderSeverity: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderEvidence: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderExploit: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderTracking: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderRecords: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderComments: (() => JSX.Element) = (): JSX.Element => <div />;
-const renderObservations: (() => JSX.Element) = (): JSX.Element => <div />;
+interface IContextData { findingId: string; projectName: string; userRole: string; }
+
+/* Hack: get projectName and findingId from url
+ * This values shall be retrieved from the redux state once the dashboard migration finishes
+ */
+const getContextData: (() => IContextData) = (): IContextData => {
+  const locationElements: string[] = location.hash.split("/");
+  const projectName: string = locationElements[2].toLowerCase();
+  const findingId: string = locationElements[3].toLowerCase();
+  const userRole: string = (window as Window & { userRole: string }).userRole;
+
+  return { findingId, projectName, userRole };
+};
+
+// tslint:disable-next-line:no-any Allows to render containers without specifying values for their redux-supplied props
+const reduxProps: any = {};
+const renderDescription: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, projectName, userRole } = getContextData();
+
+  return <DescriptionView findingId={findingId} projectName={projectName} userRole={userRole} {...reduxProps} />;
+};
+const renderSeverity: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, userRole } = getContextData();
+
+  return <SeverityView findingId={findingId} canEdit={_.includes(["admin", "analyst"], userRole)} {...reduxProps} />;
+};
+const renderEvidence: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, userRole } = getContextData();
+
+  return <EvidenceView findingId={findingId} canEdit={_.includes(["admin", "analyst"], userRole)} {...reduxProps} />;
+};
+const renderExploit: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, userRole } = getContextData();
+
+  return <ExploitView findingId={findingId} canEdit={_.includes(["admin", "analyst"], userRole)} {...reduxProps} />;
+};
+const renderTracking: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, userRole } = getContextData();
+
+  return <TrackingView findingId={findingId} hasNewVulnerabilities={true} userRole={userRole} {...reduxProps} />;
+};
+const renderRecords: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId, userRole } = getContextData();
+
+  return <RecordsView findingId={findingId} canEdit={_.includes(["admin", "analyst"], userRole)} {...reduxProps} />;
+};
+const renderComments: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId } = getContextData();
+
+  return <CommentsView type="comment" findingId={findingId} />;
+};
+const renderObservations: (() => JSX.Element) = (): JSX.Element => {
+  const { findingId } = getContextData();
+
+  return <CommentsView type="observation" findingId={findingId} />;
+};
 
 const findingContent: React.SFC = (): JSX.Element => (
   <React.StrictMode>
