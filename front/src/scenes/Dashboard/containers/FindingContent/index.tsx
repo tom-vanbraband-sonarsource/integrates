46cb80d0cb3406ac988b3d828b8a7575cf3e306c
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-02-21 14:59:45 -0500

    feat(front): #1139.2 improve findingcontent base
    - add missing container styles
    - fix relative route paths

--- front/src/scenes/Dashboard/containers/FindingContent/index.tsx
+++ front/src/scenes/Dashboard/containers/FindingContent/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import ReactDOM from "react-dom";
-import { BrowserRouter, NavLink, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import style from "./index.css";
 
 const renderDescription: (() => JSX.Element) = (): JSX.Element => <div />;
@@ -12,70 +13,72 @@ const renderRecords: (() => JSX.Element) = (): JSX.Element => <div />;
 const renderComments: (() => JSX.Element) = (): JSX.Element => <div />;
 const renderObservations: (() => JSX.Element) = (): JSX.Element => <div />;
 
-const findingContent: React.SFC = (): JSX.Element => {
-  const locationElements: string[] = location.hash.split("/");
-  const projectName: string = locationElements[2].toLowerCase();
-  const findingId: string = locationElements[3].toLowerCase();
+const findingContent: React.SFC = (): JSX.Element => (
+  <React.StrictMode>
+    <div className={style.mainContainer}>
+      <Row>
+        <Col md={12} sm={12}>
+          <BrowserRouter basename="/dashboard#!/project">
+            <React.Fragment>
+              <ul className={`${style.tabsContainer} pills-tabs`}>
+                <li id="infoItem" className={style.tab}>
+                  <Link to="description" aria-expanded="false">
+                    <i className="icon s7-note2" />&nbsp;Description
+                    </Link>
+                </li>
+                <li id="cssv2Item" className={style.tab}>
+                  <Link to="severity" aria-expanded="false">
+                    <i className="icon s7-calculator" />&nbsp;Severity
+                    </Link>
+                </li>
+                <li id="evidenceItem" className={style.tab}>
+                  <Link to="evidence">
+                    <i className="icon s7-photo" />&nbsp;Evidence
+                    </Link>
+                </li>
+                <li id="exploitItem" className={style.tab}>
+                  <Link to="exploit">
+                    <i className="icon s7-file" />&nbsp;Exploit
+                    </Link>
+                </li>
+                <li id="trackingItem" className={style.tab}>
+                  <Link to="tracking" aria-expanded="true">
+                    <i className="icon s7-graph1" />&nbsp;Tracking
+                    </Link>
+                </li>
+                <li id="recordsItem" className={style.tab}>
+                  <Link to="records" aria-expanded="true">
+                    <i className="icon s7-notebook" />&nbsp;Records
+                    </Link>
+                </li>
+                <li id="commentItem" className={style.tab}>
+                  <Link to="comments">
+                    <i className="icon s7-comment" />&nbsp;Comments
+                    </Link>
+                </li>
+                <li id="observationsItem" className={style.tab}>
+                  <Link to="observations">
+                    <i className="icon s7-note" />&nbsp;Observations
+                    </Link>
+                </li>
+              </ul>
 
-  return (
-    <React.StrictMode>
-      <BrowserRouter basename={`dashboard#!/project/${projectName}/${findingId}`}>
-        <React.Fragment>
-          <ul className={style.tabsContainer}>
-            <li className={style.tab}>
-              <NavLink to="/description" activeClassName={style.activeTab} aria-expanded="false">
-                <i className="icon s7-note2" />&nbsp;Description
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/severity" activeClassName={style.activeTab} aria-expanded="false">
-                <i className="icon s7-calculator" />&nbsp;Severity
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/evidence" activeClassName={style.activeTab}>
-                <i className="icon s7-photo" />&nbsp;Evidence
-                </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/exploit" activeClassName={style.activeTab}>
-                <i className="icon s7-file" />&nbsp;Exploit
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/tracking" activeClassName={style.activeTab} aria-expanded="true">
-                <i className="icon s7-graph1" />&nbsp;Tracking
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/records" activeClassName={style.activeTab} aria-expanded="true">
-                <i className="icon s7-notebook" />&nbsp;Records
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/comments" activeClassName={style.activeTab}>
-                <i className="icon s7-comment" />&nbsp;Comments
-              </NavLink>
-            </li>
-            <li className={style.tab}>
-              <NavLink to="/observations" activeClassName={style.activeTab}>
-                <i className="icon s7-note" />&nbsp;Observations
-              </NavLink>
-            </li>
-          </ul>
-
-          <Route path="/description" component={renderDescription} />
-          <Route path="/severity" component={renderSeverity} />
-          <Route path="/evidence" component={renderEvidence} />
-          <Route path="/exploit" component={renderExploit} />
-          <Route path="/tracking" component={renderTracking} />
-          <Route path="/records" component={renderRecords} />
-          <Route path="/comments" component={renderComments} />
-          <Route path="/observations" component={renderObservations} />
-        </React.Fragment>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-};
+              <div className={style.tabContent}>
+                <Route path="/:proj/:fin/description" render={renderDescription} />
+                <Route path="/:proj/:fin/severity" render={renderSeverity} />
+                <Route path="/:proj/:fin/evidence" render={renderEvidence} />
+                <Route path="/:proj/:fin/exploit" render={renderExploit} />
+                <Route path="/:proj/:fin/tracking" render={renderTracking} />
+                <Route path="/:proj/:fin/records" render={renderRecords} />
+                <Route path="/:proj/:fin/comments" render={renderComments} />
+                <Route path="/:proj/:fin/observations" render={renderObservations} />
+              </div>
+            </React.Fragment>
+          </BrowserRouter>
+        </Col>
+      </Row>
+    </div>
+  </React.StrictMode>
+);
 
 ReactDOM.render(React.createElement(findingContent), document.getElementById("root"));
