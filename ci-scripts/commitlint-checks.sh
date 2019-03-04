e70cc0f110edd3a101f1ef01d77753b107d38aa6
Andres Cuberos <acuberos@fluidattacks.com>
2019-03-04 18:03:52 -0500

    perf(build): #1154 improve build time
    - Remove unused dependencies from deps container and install
      Fluent in this stage, as well as npm packages.
    - Now Integrates and Bot only download the source code and
      configure it. NPM packages are brought from the deps container
      and are updated, if applies.
    - Remove 200 deltas check from commitlint so it is managed by
      Danger and it is possible to bypass if necessary.
    - Use the output of gulp-compiler during the Integrates container
      build, instead of running gulp again.

--- ci-scripts/commitlint-checks.sh
+++ ci-scripts/commitlint-checks.sh
@@ -12,15 +12,3 @@ for ((i = 0 ; i < N_COMMITS ; i++)); do
     EXIT_FAIL=1
   fi
 done
-
-DELTAS=$(git log --stat . | egrep "files? changed" | head -n 1 | cut -d, -f 2- | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g' | { c=0; while read x; do c=$(($c+$x)); done; echo $c; })
-
-echo "Commit has $DELTAS deltas."
-
-if [[ $DELTAS -gt 200 ]]; then
-  EXIT_FAIL=1
-fi
-
-if [[ x"$EXIT_FAIL" = x"1" ]]; then
-    exit 1
-fi
