4b56e97c8339d83090dc57761d82306bd86ffc75
Andres Cuberos <acuberos@fluidattacks.com>
2019-03-12 18:59:20 -0500

    feat(build): #1096.1 separate build deps
    - Build a container that has all the development dependencies
      necessary to run the pipeline.
    - Refactor New-Version mail so it takes into account if the MR
      has multiple commits.

--- integrates_version.py
+++ integrates_version.py
@@ -3,41 +3,33 @@
 
 from git import Repo
 from app import mailer
-import time
 import os
-import sys
 import fileinput
 from __init__ import FI_MAIL_ENGINEERING
 
-def create_integrates_version():
-    cur_time = time.localtime()
-    min_month = (cur_time.tm_mday - 1) * 1440 + cur_time.tm_hour * 60 + \
-        cur_time.tm_min
-    version = time.strftime('%y.%m.{}').format(min_month)
-    with open('/usr/local/share/FI_version.txt', 'w') as version_file:
-        version_file.write(version)
-
-def send_mail_version():
+def send_mail_version(commit_sha, commit_before_sha):
     version = get_integrates_version()
     repo = Repo(os.getcwd())
-    changelog = repo.git.log('-1', '--pretty=<b>%s</b>\n%b')
-    commit_message = changelog.replace("\n", "<br />\n")
+    message = repo.git.log(commit_before_sha + '...' + commit_sha,
+        '--pretty=format:<b>%s</b>%n%bCommitted by: %an%n')
     to = [FI_MAIL_ENGINEERING]
     context = {
         'version': version,
-        'message': commit_message,
-        }
+        'message': message.replace('\n', '<br/>\n')
+    }
     mailer.send_mail_new_version(to, context)
 
 def get_integrates_version():
     file = fileinput.FileInput('/usr/src/app/app/templates/dashboard.html')
-    version = ""
+    version = ''
     for line in file:
-        if ">v." in line:
-            version = line.split(">v. ")[1].split("<")[0]
+        if '>v.' in line:
+            version = line.split('>v. ')[1].split('<')[0]
             return version
     file.close()
     return version
 
 if __name__ == '__main__':
-    send_mail_version()
+    commit_sha = os.environ['CI_COMMIT_SHA']
+    commit_before_sha = os.environ['CI_COMMIT_BEFORE_SHA']
+    send_mail_version(commit_sha, commit_before_sha)
