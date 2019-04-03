e5ea1e4ada3a45ff882b1a561db60c262bd8b4f7
Mateo Norena <mnorena@fluidattacks.com>
2019-04-03 15:36:03 -0500

    fix(build): #1196 return commitlint exit code

--- ci-scripts/commitlint-checks.sh
+++ ci-scripts/commitlint-checks.sh
@@ -2,13 +2,11 @@
 
 # shellcheck disable=SC2086
 
-EXIT_FAIL=0
-
 git fetch --prune &> /dev/null
 N_COMMITS=$(git log --oneline origin/master..origin/$CI_COMMIT_REF_NAME | wc -l)
 for ((i = 0 ; i < N_COMMITS ; i++)); do
   if ! git rev-parse HEAD~$i | xargs git log -1 --pretty=%B | npx commitlint
   then
-    EXIT_FAIL=1
+    exit 1
   fi
 done
