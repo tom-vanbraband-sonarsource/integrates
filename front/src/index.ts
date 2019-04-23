7b50238ae6f5136b5e5af4b56d7ac6fdf5e38058
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-04-23 11:52:15 -0500

    refac(front): #872 remove migrated code

--- front/src/index.ts
+++ front/src/index.ts
@@ -5,11 +5,7 @@
  */
 import angular from "angular";
 import { react2angular } from "react2angular";
-import { dataTable } from "./components/DataTable/index";
-import frame from "./components/Frame/index";
-import button from "./components/RButton/index";
 import { fileInput } from "./scenes/Dashboard/components/FileInput/index";
-import { Sidebar } from "./scenes/Dashboard/components/Sidebar";
 import Access from "./scenes/Login/components/Access/index";
 import { welcomeView } from "./scenes/Registration/containers/WelcomeView";
 
@@ -27,71 +23,6 @@ angular
       Access,
       [],
     ),
-  );
-
-/**
- * @url: #/project/:name/indicators
- * @page: indicators.html
- * @controllers: ["projectIndicatorsCtrl"]
- * @tag: <frame/>
- */
-angular
-  .module("FluidIntegrates")
-  .component(
-    "formFrame",
-    react2angular(
-      frame,
-      [
-        "id",
-        "src",
-        "height",
-      ],
-    ),
-  );
-
-/**
- * @url: /dashboard
- * @page: dashboard.html
- * @controllers: dashboardCtrl
- * @tag: <d-table/>
- */
-angular
-  .module("FluidIntegrates")
-  .component(
-   "dTable",
-   react2angular(
-     dataTable, [
-       "dataset",
-       "enableRowSelection",
-       "exportCsv",
-       "headers",
-       "id",
-       "onClickRow",
-       "pageSize",
-       "search",
-       "title",
-     ],
-   ),
-  );
-
-/**
- * @url: /dashboard
- * @page: dashboard.html
- * @controllers: dashboardCtrl
- * @tag: <r-button/>
- */
-angular
-  .module("FluidIntegrates")
-  .component(
-   "rButton",
-   react2angular(
-     button, [
-       "bstyle",
-       "btitle",
-       "bicon",
-       "onClickButton",
-     ],
-   ),
   );
 
 /**
@@ -130,10 +61,3 @@ angular
       ],
     ),
 );
-
-/**
- * @tag: <sidebar/>
- */
-angular
-  .module("FluidIntegrates")
-  .component("sidebar", react2angular(Sidebar, ["onLogoutClick"]));
