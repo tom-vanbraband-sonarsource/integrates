fd2ef848261814cc9d6e9137ce0ce576814f1111
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-04-29 17:03:37 -0500

    feat(front): #1192 add google login base

--- mobile/src/containers/LoginView/index.tsx
+++ mobile/src/containers/LoginView/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Image, View } from "react-native";
+import { Button, Image, View, ToastAndroid } from "react-native";
 import { RouteComponentProps } from "react-router-native";
+import { Google } from "expo";
 
 // tslint:disable-next-line: no-default-import
 import { default as FluidLogo } from "../../../assets/logo.png";
@@ -10,13 +11,25 @@ import { styles } from "./styles";
 type ILoginProps = RouteComponentProps;
 
 const loginView: React.FunctionComponent<ILoginProps> = (props: ILoginProps): JSX.Element => {
-  const handleGoogleButtonClick: (() => void) = (): void => { props.history.push("/Menu"); };
+  const handleGoogleButtonClick: (() => void) = (): void => {
+    Google.logInAsync({
+      androidClientId: "335718398321-vv3cfdee0ng40tuhgm5g2mp42c2d2o9j.apps.googleusercontent.com",
+      scopes: ["profile", "email"],
+    }).then((result: Google.LogInResult) => {
+      if (result.type === "success") {
+        props.history.push("/Menu", { userInfo: result.user });
+      }
+    }).catch((e: Error) => {
+      ToastAndroid.show('Oops! There is an error.', ToastAndroid.SHORT);
+      throw e;
+    });
+  };
 
   return (
     <View style={styles.container}>
       <Image source={FluidLogo} style={styles.logo} />
       <View style={styles.buttonsContainer}>
-        <Button title="Log in with Google" onPress={handleGoogleButtonClick} />
+        <Button title="Log in with Google" color="#cc0000" onPress={handleGoogleButtonClick} />
       </View>
     </View>
   );
--- mobile/src/containers/MenuView/index.tsx
+++ mobile/src/containers/MenuView/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { RouteComponentProps } from "react-router-native";
+
 import { styles } from "./styles";
 
-const menuView: React.FunctionComponent = (): JSX.Element => (
-  <View style={styles.container}>
-    <Text>Hello world</Text>
-  </View>
-);
+interface IUser {
+  email?: string;
+  familyName: string;
+  givenName: string;
+  id: string;
+  name: string;
+  photoUrl?: string;
+}
+
+type IMenuProps = RouteComponentProps<{}, {}, { userInfo: IUser }>;
+
+const menuView: React.FunctionComponent<IMenuProps> = (props: IMenuProps): JSX.Element => {
+  const { userInfo } = props.location.state;
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome {userInfo.givenName}!</Text>
+    </View>
+  );
+};
 
 export { menuView as MenuView };
