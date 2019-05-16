31e59125250c4b9f09cf081728bd1c1b74e23ff7
cgomezatfluid <cgomez@fluidattacks.com>
2019-05-16 13:39:09 -0500

    refac(front): #867.16 remove unnused code

--- front/src/scenes/Dashboard/containers/EventsView/actionTypes.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable:no-inferrable-types
- * Disabling this rule is necessary due to a tslint 'type'
- * conflict that would force to reassign each variable,
- * compromising constants readability
- */
-export const LOAD_EVENTS: string = "dashboard/resources/events";
-export const CLEAR_EVENTS_STATE: string = "dashboard/resources/events/clear";
