0e635ddb0da037815afb3c7a06ba09685a3fde8c
Carolina Gómez <cgomez@fluidattacks.com>
2019-05-29 09:54:46 -0500

    refac(front): #867.21 add files modal

---
+++ front/src/scenes/Dashboard/components/AddFilesModal/index.spec.tsx
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { describe, it } from "mocha";
+import * as React from "react";
+import { Provider } from "react-redux";
+import { Action, createStore, Store } from "redux";
+import { reduxForm as ReduxForm } from "redux-form";
+import { addFilesModal as AddFilesModal } from "./index";
+
+configure({ adapter: new ReactSixteenAdapter() });
+
+const functionMock: (() => void) = (): void => undefined;
+
+describe("Add Files modal", () => {
+
+  const store: Store<{}, Action<{}>> = createStore(() => ({}));
+  const wrapper: ShallowWrapper = shallow(
+    <Provider store={store}>
+      <AddFilesModal
+        isOpen={true}
+        onClose={functionMock}
+        onSubmit={functionMock}
+        showUploadProgress={true}
+        uploadProgress={10}
+      />
+    </Provider>,
+  );
+
+  it("should return a function", () => {
+    expect(typeof (AddFilesModal)).to
+      .equal("function");
+  });
+
+  it("should render", () => {
+    expect(wrapper).to
+      .contain(ReduxForm);
+  });
+});
---
+++ front/src/scenes/Dashboard/components/AddFilesModal/index.tsx
@@ -0,0 +1,136 @@
+/* tslint:disable jsx-no-multiline-js jsx-no-lambda
+ * JSX-NO-MULTILINE-JS: Disabling this rule is necessary for the sake of
+ * readability of the code that dynamically renders the fields
+ * JSX-NO-LAMBDA: Disabling this rule is necessary for the sake of simplicity and
+ * readability of the code that binds click events
+ */
+
+import React from "react";
+import { ButtonToolbar, Col, ProgressBar, Row } from "react-bootstrap";
+import { Provider } from "react-redux";
+import {
+  ConfigProps, DecoratedComponentClass, Field,
+  FieldArray, GenericFieldArray, InjectedFormProps,
+  reduxForm,
+  WrappedFieldArrayProps,
+} from "redux-form";
+import { Button } from "../../../../components/Button/index";
+import { default as Modal } from "../../../../components/Modal/index";
+import store from "../../../../store/index";
+import { focusError } from "../../../../utils/forms/events";
+import { textAreaField } from "../../../../utils/forms/fields";
+import translate from "../../../../utils/translations/translate";
+import { required } from "../../../../utils/validations";
+import { fileInput as FileInput } from "../../components/FileInput/index";
+
+export interface IAddFilesModalProps {
+  isOpen: boolean;
+  showUploadProgress?: boolean;
+  uploadProgress?: number;
+  onClose(): void;
+  onSubmit(values: {}): void;
+}
+
+/* This is necessary because there is a problem with the type of FieldArray in redux-form library,
+* this is the issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/32858#issuecomment-461441222
+*/
+/* tslint:disable-next-line:variable-name
+* Disabling here is necessary due a conflict
+* between lowerCamelCase var naming rule from tslint
+* and PascalCase rule for naming JSX elements
+*/
+const FieldArrayCustom: new () => GenericFieldArray<Field<React.InputHTMLAttributes<HTMLInputElement> |
+  React.SelectHTMLAttributes<HTMLSelectElement> | React.TextareaHTMLAttributes<HTMLTextAreaElement>>,
+  WrappedFieldArrayProps<undefined>> = FieldArray as new () =>
+  GenericFieldArray<Field, WrappedFieldArrayProps<undefined>>;
+
+type formProps = IAddFilesModalProps & InjectedFormProps<{}, IAddFilesModalProps>;
+
+const renderFilesFields: ((props: WrappedFieldArrayProps<undefined>) => JSX.Element) =
+    (props: WrappedFieldArrayProps<undefined>): JSX.Element => (
+      <React.Fragment>
+        {props.fields.map((fieldName: string) => (
+          <Row>
+            <Col md={12}>
+              <div>
+                <FileInput fileName="" fileSize={100} icon="search" id="file" type="" visible={true}/>
+              </div>
+            </Col>
+            <Col md={12}>
+              <label>
+                <label style={{ color: "#f22" }}>* </label>
+                {translate.t("search_findings.tab_resources.description")}
+              </label>
+              <Field name={`${fieldName}.description`} component={textAreaField} type="text" validate={[required]} />
+            </Col>
+          </Row>
+        ))}
+      </React.Fragment>
+    );
+
+const renderFooter: ((props: formProps) => JSX.Element) =
+  (props: formProps): JSX.Element => (
+    <React.Fragment>
+      <ButtonToolbar className="pull-right">
+        <Button bsStyle="default" onClick={(): void => { props.onClose(); }}>
+          {translate.t("confirmmodal.cancel")}
+        </Button>
+        <Button bsStyle="primary" type="submit" disabled={props.pristine || props.submitting}>
+          {translate.t("confirmmodal.proceed")}
+        </Button>
+      </ButtonToolbar>
+    </React.Fragment>
+  );
+
+const renderFilesForm: ((props: formProps) => JSX.Element) =
+    (props: formProps): JSX.Element => (
+      <React.Fragment>
+        <form onSubmit={props.handleSubmit}>
+          <FieldArrayCustom name="resources" component={renderFilesFields} />
+          { props.showUploadProgress === true ?
+            <React.Fragment>
+              <br />
+              {translate.t("search_findings.tab_resources.uploading_progress")}
+              <ProgressBar active={true} now={props.uploadProgress} label={`${props.uploadProgress}%`} />
+            </React.Fragment>
+            : undefined
+          }
+          <br />
+          {renderFooter(props)}
+        </form>
+        <br />
+      </React.Fragment>
+    );
+
+type resourcesForm =
+  DecoratedComponentClass<{}, IAddFilesModalProps & Partial<ConfigProps<{}, IAddFilesModalProps>>, string>;
+
+/* tslint:disable:variable-name
+ * Disabling here is necessary due a conflict
+ * between lowerCamelCase var naming rule from tslint
+ * and PascalCase rule for naming JSX elements
+ */
+
+const FilesForm: resourcesForm = reduxForm<{}, IAddFilesModalProps>({
+  enableReinitialize: true,
+  form: "addFiles",
+  initialValues: {
+    resources: [{ fileName: "", description: ""}],
+  },
+  onSubmitFail: focusError,
+})(renderFilesForm);
+// tslint:enable:variable-name
+
+export const addFilesModal: React.FC<IAddFilesModalProps> =
+  (props: IAddFilesModalProps): JSX.Element => (
+    <React.StrictMode>
+      <Provider store={store}>
+        <Modal
+          open={props.isOpen}
+          headerTitle={translate.t("search_findings.tab_resources.modal_file_title")}
+          content={<FilesForm {...props} />}
+          footer={<div />}
+        />
+      </Provider>
+    </React.StrictMode>
+  );
