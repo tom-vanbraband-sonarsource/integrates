a2164a44f2e8c7202e4fdde110fef490d2537c23
Carolina Gómez <cgomez@fluidattacks.com>
2019-06-05 17:31:11 -0500

    refac(front): #867.31 format users

--- front/src/utils/formatHelpers.ts
+++ front/src/utils/formatHelpers.ts
@@ -13,36 +13,35 @@ import translate from "./translations/translate";
 type IUserList = IDashboardState["users"]["userList"];
 
 export const formatUserlist:
-((userList: IUserList) => IUserList) = (userList: IUserList): IUserList => {
-  for (const user of userList) {
-    user.role = translate.t(`search_findings.tab_users.${user.role}`);
-    const lastLoginDate: number[] = JSON.parse(user.lastLogin);
-    let DAYS_IN_MONTH: number;
-    DAYS_IN_MONTH = 30;
-
-    if (lastLoginDate[0] >= DAYS_IN_MONTH) {
-      const ROUNDED_MONTH: number = Math.round(lastLoginDate[0] / DAYS_IN_MONTH);
-      user.lastLogin = translate.t("search_findings.tab_users.months_ago", {count: ROUNDED_MONTH});
-    } else if (lastLoginDate[0] > 0 && lastLoginDate[0] < DAYS_IN_MONTH) {
-      user.lastLogin = translate.t("search_findings.tab_users.days_ago", {count: lastLoginDate[0]});
-    } else if (lastLoginDate[0] === -1) {
-      user.lastLogin = "-";
-      user.firstLogin = "-";
-    } else {
-      let SECONDS_IN_HOUR: number;
-      SECONDS_IN_HOUR = 3600;
-      const ROUNDED_HOUR: number = Math.round(lastLoginDate[1] / SECONDS_IN_HOUR);
-      let SECONDS_IN_MINUTES: number;
-      SECONDS_IN_MINUTES = 60;
-      const ROUNDED_MINUTES: number = Math.round(lastLoginDate[1] / SECONDS_IN_MINUTES);
-      user.lastLogin = ROUNDED_HOUR >= 1 && ROUNDED_MINUTES >= SECONDS_IN_MINUTES
-      ? translate.t("search_findings.tab_users.hours_ago", {count: ROUNDED_HOUR})
-      : translate.t("search_findings.tab_users.minutes_ago", {count: ROUNDED_MINUTES});
-    }
+((userList: IUserList) => IUserList) = (userList: IUserList): IUserList => userList.map((user: IUserList[0]) => {
+  const role: string = translate.t(`search_findings.tab_users.${user.role}`);
+  const lastLoginDate: number[] = JSON.parse(user.lastLogin);
+  let DAYS_IN_MONTH: number;
+  DAYS_IN_MONTH = 30;
+  let lastLogin: string; lastLogin = "";
+  let firstLogin: string; firstLogin = "";
+  if (lastLoginDate[0] >= DAYS_IN_MONTH) {
+    const ROUNDED_MONTH: number = Math.round(lastLoginDate[0] / DAYS_IN_MONTH);
+    lastLogin = translate.t("search_findings.tab_users.months_ago", {count: ROUNDED_MONTH});
+  } else if (lastLoginDate[0] > 0 && lastLoginDate[0] < DAYS_IN_MONTH) {
+    lastLogin = translate.t("search_findings.tab_users.days_ago", {count: lastLoginDate[0]});
+  } else if (lastLoginDate[0] === -1) {
+    lastLogin = "-";
+    firstLogin = "-";
+  } else {
+    let SECONDS_IN_HOUR: number;
+    SECONDS_IN_HOUR = 3600;
+    const ROUNDED_HOUR: number = Math.round(lastLoginDate[1] / SECONDS_IN_HOUR);
+    let SECONDS_IN_MINUTES: number;
+    SECONDS_IN_MINUTES = 60;
+    const ROUNDED_MINUTES: number = Math.round(lastLoginDate[1] / SECONDS_IN_MINUTES);
+    lastLogin = ROUNDED_HOUR >= 1 && ROUNDED_MINUTES >= SECONDS_IN_MINUTES
+    ? translate.t("search_findings.tab_users.hours_ago", {count: ROUNDED_HOUR})
+    : translate.t("search_findings.tab_users.minutes_ago", {count: ROUNDED_MINUTES});
   }
 
-  return userList;
-};
+  return { ...user, role, lastLogin, firstLogin };
+});
 
 export const castFieldsCVSS2: ((dataset: ISeverityViewProps["dataset"]) => ISeverityField[]) =
   (dataset: ISeverityViewProps["dataset"]): ISeverityField[] => {
