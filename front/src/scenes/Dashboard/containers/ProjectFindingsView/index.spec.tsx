4f78a4f0d00f07864f9dfcca9b3e583f2d2ee028
Carolina Gómez <cgomez@fluidattacks.com>
2019-06-27 15:55:52 -0500

    test(front): #1253.19 jest tests
    - ProjectFindingsView Component

---
+++ front/src/scenes/Dashboard/containers/ProjectFindingsView/index.spec.tsx
@@ -0,0 +1,113 @@
+import { configure, mount, ReactWrapper } from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { GraphQLError } from "graphql";
+// tslint:disable-next-line: no-import-side-effect
+import "isomorphic-fetch";
+import * as React from "react";
+// tslint:disable-next-line: no-submodule-imports
+import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
+import { Provider } from "react-redux";
+import wait from "waait";
+import store from "../../../../store/index";
+import ProjectFindingsView from "./index";
+import { GET_FINDINGS } from "./queries";
+import { IProjectFindingsProps } from "./types";
+
+configure({ adapter: new ReactSixteenAdapter() });
+
+const functionMock: (() => void) = (): void => undefined;
+
+describe("ProjectFindingsView", () => {
+
+  const mockProps: IProjectFindingsProps = {
+    match: {
+      isExact: true,
+      params: {projectName: "TEST"},
+      path: "/",
+      url: "",
+    },
+    onCloseReportsModal: functionMock,
+    onOpenReportsModal: functionMock,
+    reportsModal: {
+      isOpen: false,
+    },
+  };
+
+  const mocks: ReadonlyArray<MockedResponse> = [
+    {
+      request: {
+        query: GET_FINDINGS,
+        variables: {
+          projectName: "TEST",
+        },
+      },
+      result: {
+        data: {
+          project: {
+            __typename: "Project",
+            findings: [{
+              __typename: "Finding",
+              age: 252,
+              description: "This is a test description",
+              id: "438679960",
+              isExploitable: true,
+              lastVulnerability: 33,
+              openVulnerabilities: 6,
+              remediated: false,
+              severityScore: 2.9,
+              state: "Abierto",
+              title: "FIN.S.0038. Fuga de informaci\u00f3n de negocio",
+              treatment: "IN PROGRESS",
+              type: "SECURITY",
+            }],
+          },
+        },
+      },
+  }];
+
+  const mockError: ReadonlyArray<MockedResponse> = [
+    {
+      request: {
+        query: GET_FINDINGS,
+        variables: {
+          projectName: "TEST",
+        },
+      },
+      result: {
+        errors: [new GraphQLError("Access denied")],
+      },
+    },
+  ];
+
+  it("should return a object", () => {
+    expect(typeof (ProjectFindingsView))
+      .toEqual("object");
+  });
+
+  it("should render a component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocks} addTypename={true}>
+          <ProjectFindingsView {...mockProps} />
+        </MockedProvider>
+      </Provider>,
+    );
+    await wait(0);
+    expect(wrapper)
+      .toHaveLength(1);
+  });
+
+  it("should render an error in component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mockError} addTypename={true}>
+          <ProjectFindingsView {...mockProps} />
+        </MockedProvider>
+      </Provider>,
+    );
+    await wait(0);
+    expect(wrapper)
+      .toHaveLength(1);
+  });
+
+});
