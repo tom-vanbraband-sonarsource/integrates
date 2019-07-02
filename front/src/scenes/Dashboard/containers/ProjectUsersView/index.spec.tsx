6a6da292f5a26fc8c8419579fafaa87676404f63
Carolina Gómez <cgomez@fluidattacks.com>
2019-07-02 09:57:58 -0500

    test(front): #1253.21 jest tests
    - ProjectUsersView Component

--- front/src/scenes/Dashboard/containers/ProjectUsersView/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/ProjectUsersView/index.spec.tsx
@@ -1,13 +1,17 @@
-import { expect } from "chai";
-import { configure, shallow, ShallowWrapper } from "enzyme";
+import { configure, mount, ReactWrapper } from "enzyme";
 import ReactSixteenAdapter from "enzyme-adapter-react-16";
-import React from "react";
-import { Glyphicon } from "react-bootstrap";
-import { reduxForm as ReduxForm } from "redux-form";
-import { Button } from "../../../../components/Button/index";
-import { dataTable as DataTable } from "../../../../components/DataTable/index";
-import { FluidIcon } from "../../../../components/FluidIcon";
+import { GraphQLError } from "graphql";
+// tslint:disable-next-line: no-import-side-effect
+import "isomorphic-fetch";
+import * as React from "react";
+// tslint:disable-next-line: no-submodule-imports
+import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
+import { Provider } from "react-redux";
+import wait from "waait";
+import store from "../../../../store/index";
 import ProjectUsersView from "./index";
+import { GET_USERS } from "./queries";
+import { IProjectUsersViewProps } from "./types";
 
 configure({ adapter: new ReactSixteenAdapter() });
 
@@ -15,86 +19,122 @@ const functionMock: (() => void) = (): void => undefined;
 
 describe("Project users view", () => {
 
-  const wrapper: ShallowWrapper = shallow(
-    <ProjectUsersView
-      match={{ params: { projectName: "unittesting" }, isExact: false, path: "", url: "" }}
-    />,
-  );
+  const mockPropsAdd: IProjectUsersViewProps = {
+    addModal: {
+      initialValues: {},
+      open: false,
+      type: "add",
+    },
+    match: {
+      isExact: true,
+      params: {projectName: "TEST"},
+      path: "/",
+      url: "",
+    },
+    onCloseUsersModal: functionMock,
+    onOpenModal: functionMock,
+    userRole: "manager",
+  };
 
-  it("should return a function", () => {
-    expect(typeof (ProjectUsersView)).to
-      .equal("function");
+  const mockPropsEdit: IProjectUsersViewProps = {
+    addModal: {
+      initialValues: {},
+      open: true,
+      type: "edit",
+    },
+    match: {
+      isExact: true,
+      params: {projectName: "TEST"},
+      path: "/",
+      url: "",
+    },
+    onCloseUsersModal: functionMock,
+    onOpenModal: functionMock,
+    userRole: "manager",
+  };
+
+  const mocks: ReadonlyArray<MockedResponse> = [
+    {
+      request: {
+        query: GET_USERS,
+        variables: {
+          projectName: "TEST",
+        },
+      },
+      result: {
+        data: {
+          project: {
+            __typename: "Project",
+            users: [{
+              __typename: "User",
+              email: "user@gmail.com",
+              firstLogin: "2017-09-05 15:00:00",
+              lastLogin: "[3, 81411]",
+              organization: "TEST",
+              phoneNumber: "-",
+              responsibility: "-",
+              role: "customer",
+            }],
+          },
+        },
+      },
+  }];
+
+  const mockError: ReadonlyArray<MockedResponse> = [
+    {
+      request: {
+        query: GET_USERS,
+        variables: {
+          projectName: "TEST",
+        },
+      },
+      result: {
+        errors: [new GraphQLError("Access denied")],
+      },
+    },
+  ];
+
+  it("should return a object", () => {
+    expect(typeof (ProjectUsersView))
+      .toEqual("object");
   });
 
-  it("should render action buttons", () => {
-    const buttons: ShallowWrapper = wrapper.find(Button);
-    expect(buttons.length).to
-      .equal(3);
-
-    const editBtn: string = buttons.at(0)
-      .html();
-    const expectedEditBtn: string = shallow(
-      <Button
-        id="editUser"
-        block={true}
-        bsStyle="primary"
-        onClick={functionMock}
-      >
-        <FluidIcon icon="edit" />
-        &nbsp;search_findings.tab_users.edit
-      </Button>,
-    )
-      .html();
-    expect(editBtn).to
-      .equal(expectedEditBtn);
-
-    const addBtn: string = buttons.at(1)
-      .html();
-    const expectedAddBtn: string = shallow(
-      <Button
-        id="addUser"
-        block={true}
-        bsStyle="primary"
-        onClick={functionMock}
-      >
-        <Glyphicon glyph="plus" />
-        &nbsp;search_findings.tab_users.add_button
-      </Button>,
-    )
-      .html();
-
-    expect(addBtn).to
-      .equal(expectedAddBtn);
-
-    const removeBtn: string = buttons.at(2)
-      .html();
-    const expectedRemoveBtn: string = shallow(
-      <Button
-        id="removeUser"
-        block={true}
-        bsStyle="primary"
-        onClick={functionMock}
-      >
-        <Glyphicon glyph="minus" />
-        &nbsp;search_findings.tab_users.remove_user
-      </Button>,
-    )
-      .html();
-
-    expect(removeBtn).to
-      .equal(expectedRemoveBtn);
+  it("should render an error in component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mockError} addTypename={true}>
+          <ProjectUsersView {...mockPropsAdd} />
+        </MockedProvider>
+      </Provider>,
+    );
+    await wait(0);
+    expect(wrapper)
+      .toHaveLength(1);
   });
 
-  it("should render users table", () => {
-    expect(
-      wrapper.find(DataTable)
-        .html(),
-    ).to
-      .contain('<div id="tblUsers">');
+  it("should render an add user component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocks} addTypename={true}>
+          <ProjectUsersView {...mockPropsAdd} />
+        </MockedProvider>
+      </Provider>,
+    );
+    await wait(0);
+    expect(wrapper)
+      .toHaveLength(1);
   });
 
-  it("should render add user form", () => {
-    expect(wrapper).to
-      .contain(ReduxForm);
+  it("should render an edit user component", async () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocks} addTypename={true}>
+          <ProjectUsersView {...mockPropsEdit} />
+        </MockedProvider>
+      </Provider>,
+    );
+    await wait(0);
+    expect(wrapper)
+      .toHaveLength(1);
   });
 });
