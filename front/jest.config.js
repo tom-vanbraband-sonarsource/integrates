231a5d27e6f7552ba41c53b178ae2bbc2466adac
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-07-11 13:49:13 -0500

    test(front): #1253 add missing tests
    - add tests for exploit view
    - removed unnecessary jest config

--- front/jest.config.js
+++ front/jest.config.js
@@ -2,22 +2,10 @@
 
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx,js,jsx}",
-    "!**/node_modules/**",
-    "!**/*.d.ts",
-    "!src/**/*.spec.tsx"
-  ],
   coverageDirectory: "coverage",
   coverageReporters: [
     "text",
     "lcov"
-  ],
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx"
   ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
@@ -35,17 +23,6 @@ module.exports = {
   },
   transformIgnorePatterns: [
     "node_modules/*"
-  ],
-  testRegex: [
-    "(src/components/.*/index\\.spec\\.tsx)$",
-    "(src/scenes/Dashboard/.*/.*/index\\.spec\\.tsx)$",
-    "src/scenes/Dashboard/containers/ProjectUsersView/AddUserModal/index.spec.tsx",
-    "src/scenes/Login/containers/Access/index.spec.tsx",
-    "src/scenes/Registration/components/CompulsoryNotice/index.spec.tsx",
-    "src/scenes/Registration/containers/WelcomeView/index.spec.tsx",
-    "src/scenes/Dashboard/index.spec.tsx",
-    "src/utils/forms/index.spec.tsx",
-    "src/utils/index.spec.tsx"
   ],
   verbose: true
 };
