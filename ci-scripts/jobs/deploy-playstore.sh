109e80adb867ebea48a598c78be7c6d97ef6f875
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-07-30 13:04:07 -0500

    feat(build): #1315.21 deploy functional
    - Remove vault_login from gitlab-ci
    - Add deploy-playstore function

---
+++ ci-scripts/jobs/deploy-playstore.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+deploy_playstore() {
+
+  # Deploys Integrates apk to Google Playstore
+
+  # import functions
+  . ci-scripts/helpers/others.sh
+
+  # Logs in to vault in order to read variables
+  vault_login
+
+  cd mobile/
+  echo "$(
+    vault read -field=playstore_credentials secret/integrates/production
+  )" > playstore-credentials.json
+  bundle install
+  bundle exec fastlane supply \
+    --aab ./output/integrates.aab \
+    --json_key ./playstore-credentials.json \
+    --package_name "com.fluidattacks.integrates" \
+    --track "$MOBILE_RELEASE_CHANNEL"
+
+  rm mobile/playstore-credentials.json
+}
+
+deploy_playstore
