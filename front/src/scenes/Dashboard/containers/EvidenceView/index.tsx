809020d483249f8dc822eadafefc9589d69095fc
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-08-01 16:02:22 -0500

    perf(front): #1343 avoid lambdas in jsx
    - should decrease memory consumption

--- front/src/scenes/Dashboard/containers/EvidenceView/index.tsx
+++ front/src/scenes/Dashboard/containers/EvidenceView/index.tsx
@@ -1,8 +1,6 @@
-/* tslint:disable jsx-no-multiline-js jsx-no-lambda
+/* tslint:disable jsx-no-multiline-js
  * JSX-NO-MULTILINE-JS: Disabling this rule is necessary for the sake of
  * readability of the code that dynamically renders the images
- * JSX-NO-LAMBDA: Disabling this rule is necessary for the sake of simplicity and
- * readability of the code that binds click events
  */
 
 import _ from "lodash";
@@ -39,25 +37,22 @@ export interface IEvidenceViewProps {
   isImageOpen: boolean;
 }
 
-const enhance: InferableComponentEnhancer<{}> =
-  lifecycle({
-    componentWillUnmount(): void { store.dispatch(actions.clearEvidence()); },
-    componentDidMount(): void {
-      mixpanel.track(
-        "FindingEvidence",
-        {
-          Organization: (window as Window & { userOrganization: string }).userOrganization,
-          User: (window as Window & { userName: string }).userName,
-        });
+const enhance: InferableComponentEnhancer<{}> = lifecycle<IEvidenceViewProps, {}>({
+  componentWillUnmount(): void { store.dispatch(actions.clearEvidence()); },
+  componentDidMount(): void {
+    mixpanel.track("FindingEvidence", {
+      Organization: (window as Window & { userOrganization: string }).userOrganization,
+      User: (window as Window & { userName: string }).userName,
+    });
 
-      const { findingId } = this.props as IEvidenceViewProps;
-      const thunkDispatch: ThunkDispatch<{}, {}, AnyAction> = (
-        store.dispatch as ThunkDispatch<{}, {}, AnyAction>
-      );
+    const { findingId } = this.props;
+    const thunkDispatch: ThunkDispatch<{}, {}, AnyAction> = (
+      store.dispatch as ThunkDispatch<{}, {}, AnyAction>
+    );
 
-      thunkDispatch(actions.loadEvidence(findingId));
-    },
-  });
+    thunkDispatch(actions.loadEvidence(findingId));
+  },
+});
 
 const updateEvidence: ((values: {}, evidenceId: number, props: IEvidenceViewProps) => void) =
   (values: { [key: string]: string }, evidenceId: number, props: IEvidenceViewProps): void => {
@@ -66,12 +61,10 @@ const updateEvidence: ((values: {}, evidenceId: number, props: IEvidenceViewProp
     );
     let fileId: string; fileId = `#evidence${evidenceId}`;
     let descriptionField: string; descriptionField = `evidence${evidenceId}_description`;
-    mixpanel.track(
-      "UpdateEvidence",
-      {
-        Organization: (window as Window & { userOrganization: string }).userOrganization,
-        User: (window as Window & { userName: string }).userName,
-      });
+    mixpanel.track("UpdateEvidence", {
+      Organization: (window as Window & { userOrganization: string }).userOrganization,
+      User: (window as Window & { userName: string }).userName,
+    });
     if (isFileSelected(fileId)) {
       if (isValidEvidenceFile(fileId)) {
         thunkDispatch(actions.updateEvidence(props.findingId, evidenceId, values[descriptionField], descriptionField));
@@ -85,19 +78,19 @@ const updateEvidence: ((values: {}, evidenceId: number, props: IEvidenceViewProp
     }
   };
 
-const renderEditPanel: ((props: IEvidenceViewProps) => JSX.Element) = (props: IEvidenceViewProps): JSX.Element => (
-  <Row>
-    <Col md={2} mdOffset={10} xs={12} sm={12}>
-      <Button
-        bsStyle="primary"
-        block={true}
-        onClick={(): void => { store.dispatch(actions.editEvidence(!props.isEditing)); }}
-      >
-        <FluidIcon icon="edit" /> {translate.t("search_findings.tab_severity.editable")}
-      </Button>
-    </Col>
-  </Row>
-);
+const renderEditPanel: ((props: IEvidenceViewProps) => JSX.Element) = (props: IEvidenceViewProps): JSX.Element => {
+  const handleClick: (() => void) = (): void => { store.dispatch(actions.editEvidence(!props.isEditing)); };
+
+  return (
+    <Row>
+      <Col md={2} mdOffset={10} xs={12} sm={12}>
+        <Button bsStyle="primary" block={true} onClick={handleClick}>
+          <FluidIcon icon="edit" /> {translate.t("search_findings.tab_severity.editable")}
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 const renderImages: ((props: IEvidenceViewProps) => JSX.Element) =
   (props: IEvidenceViewProps): JSX.Element => {
@@ -105,13 +98,21 @@ const renderImages: ((props: IEvidenceViewProps) => JSX.Element) =
     findingBaseUrl = `${window.location.href.split("dashboard#!/")[0]}${window.location.href.split("dashboard#!/")[1]}`;
     let emptyImage: string;
     emptyImage = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+    const evidenceImages: IEvidenceViewProps["images"] = props.images.filter(
+      (image: IEvidenceViewProps["images"][0]) => props.isEditing ? true : !_.isEmpty(image.url),
+    );
 
     return (
       <div>
-        {(props.isEditing
-          ? props.images
-          : props.images.filter((image: IEvidenceViewProps["images"][0]) => !_.isEmpty(image.url)))
-          .map((image: IEvidenceViewProps["images"][0], index: number) =>
+        {evidenceImages.map((image: IEvidenceViewProps["images"][0], index: number): JSX.Element => {
+          const handleClick: (() => void) = (): void => {
+            if (!props.isEditing) {
+              store.dispatch(actions.openEvidence(index));
+            }
+          };
+          const handleUpdate: ((values: {}) => void) = (values: {}): void => { updateEvidence(values, index, props); };
+
+          return (
             <EvidenceImage
               key={index}
               name={`evidence${index}`}
@@ -119,14 +120,11 @@ const renderImages: ((props: IEvidenceViewProps) => JSX.Element) =
               isDescriptionEditable={index > 1}
               isEditing={props.isEditing}
               url={_.isEmpty(image.url) ? `data:image/png;base64,${emptyImage}` : `${findingBaseUrl}/${image.url}`}
-              onClick={
-                props.isEditing
-                  ? (): void => undefined
-                  : (): void => { store.dispatch(actions.openEvidence(index)); }
-              }
-              onUpdate={(values: {}): void => { updateEvidence(values, index, props); }}
-            />,
-        )}
+              onClick={handleClick}
+              onUpdate={handleUpdate}
+            />
+          );
+        })}
       </div>
     );
   };
@@ -138,34 +136,38 @@ const renderLightBox: ((props: IEvidenceViewProps) => JSX.Element) = (props: IEv
     props.images.filter((image: IEvidenceViewProps["images"][0]) => !_.isEmpty(image.url));
   const nextIndex: number = (props.currentIndex + 1) % evidenceImages.length;
   const previousIndex: number = (props.currentIndex + evidenceImages.length - 1) % evidenceImages.length;
+  const adjustZoom: (() => void) = (): void => {
+    /**
+     * Hack:
+     * Due to a bug in this component, we need to set an initial img scale so it doesn't look too small
+     */
+    setTimeout((): void => {
+      const currentImg: HTMLImageElement =
+        (document.getElementsByClassName("ril-image-current")[0] as HTMLImageElement);
+      const lightboxContainer: HTMLElement = (document.getElementsByClassName("ril-inner")[0] as HTMLElement);
+      const scaleFactor: number = (lightboxContainer.offsetWidth / currentImg.width) * 0.65;
+      currentImg.style.transform = `scale(${scaleFactor})`;
+    },         100);
+  };
+  const handleClose: (() => void) = (): void => { store.dispatch(actions.closeEvidence()); };
+  const handleMovePrevious: (() => void) = (): void => {
+    store.dispatch(actions.moveEvidenceIndex(props.currentIndex, evidenceImages.length, "previous"));
+  };
+  const handleMoveNext: (() => void) = (): void => {
+    store.dispatch(actions.moveEvidenceIndex(props.currentIndex, evidenceImages.length, "next"));
+  };
 
   return (
     <Lightbox
       imageTitle={evidenceImages[props.currentIndex].description}
       imagePadding={50}
-      onAfterOpen={(): void => {
-        /**
-         * Hack:
-         * Due to a bug in this component, we need to set an initial img scale so it doesn't look too small
-         */
-        setTimeout((): void => {
-          const currentImg: HTMLImageElement =
-            (document.getElementsByClassName("ril-image-current")[0] as HTMLImageElement);
-          const lightboxContainer: HTMLElement = (document.getElementsByClassName("ril-inner")[0] as HTMLElement);
-          const scaleFactor: number = (lightboxContainer.offsetWidth / currentImg.width) * 0.65;
-          currentImg.style.transform = `scale(${scaleFactor})`;
-        },         100);
-      }}
+      onAfterOpen={adjustZoom}
       mainSrc={`${findingBaseUrl}/${evidenceImages[props.currentIndex].url}`}
       nextSrc={`${findingBaseUrl}/${evidenceImages[nextIndex].url}`}
       prevSrc={`${findingBaseUrl}/${evidenceImages[previousIndex].url}`}
-      onCloseRequest={(): void => { store.dispatch(actions.closeEvidence()); }}
-      onMovePrevRequest={(): void => {
-        store.dispatch(actions.moveEvidenceIndex(props.currentIndex, evidenceImages.length, "previous"));
-      }}
-      onMoveNextRequest={(): void => {
-        store.dispatch(actions.moveEvidenceIndex(props.currentIndex, evidenceImages.length, "next"));
-      }}
+      onCloseRequest={handleClose}
+      onMovePrevRequest={handleMovePrevious}
+      onMoveNextRequest={handleMoveNext}
       reactModalStyle={{ overlay: { zIndex: "1200" } }}
     />
   );
