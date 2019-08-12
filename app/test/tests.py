4bcb9a920c2c45bed2a010871fbea40f2c08841b
Crhistian D. Guatuzmal <cguatuzmal@fluidattacks.com>
2019-08-12 11:55:43 -0500

    test(back): #1388 moved event entity test
    moved event entity testing to entity_test folder

--- app/test/tests.py
+++ app/test/tests.py
@@ -72,64 +72,6 @@ class FormstackAPITests(TestCase):
 
 class GraphQLTests(TestCase):
 
-    def test_get_event(self):
-        """Check for eventuality"""
-        query = '''{
-            event(identifier:"418900971"){
-                detail
-            }
-        }'''
-        request = RequestFactory().get('/')
-        middleware = SessionMiddleware()
-        middleware.process_request(request)
-        request.session.save()
-        request.session['username'] = "unittest"
-        request.session['company'] = "unittest"
-        request.session['role'] = "admin"
-        request.COOKIES[settings.JWT_COOKIE_NAME] = jwt.encode(
-            {
-                'user_email': 'unittest',
-                'user_role': 'admin',
-                'company': 'unittest'
-            },
-            algorithm='HS512',
-            key=settings.JWT_SECRET,
-        )
-        result = dict(SCHEMA.execute(query, context_value=request).data)
-        if 'event' in result.keys():
-            detail = dict(result['event'])['detail']
-            assert detail == 'Integrates unit test'
-        assert 'event' in result
-
-    def test_get_events(self):
-        """Check for eventualities"""
-        query = '''{
-            events(projectName:"unittesting"){
-                detail
-            }
-        }'''
-        request = RequestFactory().get('/')
-        middleware = SessionMiddleware()
-        middleware.process_request(request)
-        request.session.save()
-        request.session['username'] = "unittest"
-        request.session['company'] = "unittest"
-        request.session['role'] = "admin"
-        request.COOKIES[settings.JWT_COOKIE_NAME] = jwt.encode(
-            {
-                'user_email': 'unittest',
-                'user_role': 'admin',
-                'company': 'unittest'
-            },
-            algorithm='HS512',
-            key=settings.JWT_SECRET,
-        )
-        result = dict(SCHEMA.execute(query, context_value=request).data)
-        if 'events' in result:
-            detail = dict(result['events'][0])['detail']
-            assert len(detail) >= 1
-        assert 'events' in result
-
     def test_get_finding(self):
         """ Check for finding """
         query = '''{
