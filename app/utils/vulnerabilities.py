de4253ea57ec60b6487c78a19661ead241e73661
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-08-13 15:45:32 -0500

    perf(back): #1343.24 create vulns dataloader

---
+++ app/utils/vulnerabilities.py
@@ -0,0 +1,6 @@
+def format_data(vuln):
+    vuln['current_state'] = vuln.get('historic_state', [{}])[-1].get('state')
+    vuln['treatment'] = ('closed' if vuln['current_state'] == 'closed'
+                         else vuln.get('treatment')).lower().capitalize()
+
+    return vuln
