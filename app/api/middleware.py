849b53b1c306049d0cfed9e3a4f2b8e84d5b3036
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-08-15 18:33:00 -0500

    refac(back): #1343.29 add missing comments

--- app/api/middleware.py
+++ app/api/middleware.py
@@ -6,6 +6,13 @@ from app.exceptions import ForbiddenField, QueryDepthExceeded
 
 
 def _get_depth(selection_set, fragments, depth_level=0):
+    """
+    Recursively calculates the query depth
+
+    :param selection_set: requested attributes from the entity
+    :param fragments: array of fragments definitions
+    :param depth_level: current depth
+    """
     max_depth = depth_level
     for field in selection_set.selections:
         if isinstance(field, FragmentSpread):
@@ -22,6 +29,12 @@ class ExecutorBackend(GraphQLCoreBackend):
     # pylint: disable=too-few-public-methods
     # Disabled because we only need to override a single method
     def document_from_string(self, schema, document_string):
+        """
+        Validates the query depth
+
+        :param schema: GraphQL schema instance
+        :param document_string: query string sent by the client
+        """
         document = super(ExecutorBackend, self).document_from_string(
             schema, document_string)
         document_tree = document.document_ast
@@ -40,6 +53,14 @@ class ExecutorBackend(GraphQLCoreBackend):
 
 
 def blacklist_middleware(next_middleware, root, info, **kwargs):
+    """
+    Bans certain fields in production environment
+
+    :param next_middleware: reference to call other configured middlewares
+    :param root: root node of the query
+    :param info: object containing resolver info and request context
+    :param kwargs: dict of arguments passed to the field
+    """
     blacklisted_fields = ['__schema', '__type']
     if info.field_name.lower() in blacklisted_fields and not settings.DEBUG:
         raise ForbiddenField()
