2d5caf24acfe73508200541d92be4cf80312e906
Crhistian D. Guatuzmal <cguatuzmal@fluidattacks.com>
2019-08-20 08:59:55 -0500

    test(back): #843 me test
    added test for entity me

---
+++ app/test/entity_test/test_me.py
@@ -0,0 +1,49 @@
+import pytest
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.conf import settings
+from graphene.test import Client
+from jose import jwt
+
+from app.api.schema import SCHEMA
+
+
+@pytest.mark.usefixtures(
+    'create_users_table',
+    'create_projects_table',
+    'create_project_access_table')
+class MeTests(TestCase):
+
+    def test_get_me(self):
+        query = '''
+            query {
+              me {
+                projects {
+                          name
+                          description
+                }
+              }
+            }
+        '''
+        testing_client = Client(SCHEMA)
+        request = RequestFactory().get('/')
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+        request.session['username'] = 'unittest'
+        request.session['company'] = 'unittest'
+        request.session['role'] = 'admin'
+        request.COOKIES[settings.JWT_COOKIE_NAME] = jwt.encode(
+            {
+                'user_email': 'unittest',
+                'user_role': 'admin',
+                'company': 'unittest'
+            },
+            algorithm='HS512',
+            key=settings.JWT_SECRET,
+        )
+        result = testing_client.execute(query, context=request)
+        assert 'errors' not in result
+        assert 'projects' in result['data']['me']
