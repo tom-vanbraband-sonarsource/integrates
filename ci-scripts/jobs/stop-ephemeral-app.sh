c2e5af0f40698fe0616fdd4d7751c4c079589c2f
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-09-02 12:01:01 -0500

    refac(build): #1315.42 stop-ephemeral-app
    - Make stop-ephemeral-app functional
    - Remove retries from deps

---
+++ ci-scripts/jobs/stop-ephemeral-app.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+stop_ephemeral_app() {
+
+  # Stop ephemeral application
+  # of a specific branch
+
+  set -e
+
+  # Set integrates namespace
+  kubectl config set-context \
+    "$(kubectl config current-context)" \
+    --namespace="${CI_PROJECT_NAME}"
+
+  # Delete deployment, service and ingress.
+  kubectl delete deployment "review-${CI_COMMIT_REF_SLUG}"
+  kubectl delete service "service-${CI_COMMIT_REF_SLUG}"
+  kubectl delete ingress "review-${CI_COMMIT_REF_SLUG}"
+}
+
+stop_ephemeral_app
