c6d0fb0725cae99b9d612b1dc0b1af8fce579161
Crhistian D. Guatuzmal <cguatuzmal@fluidattacks.com>
2019-09-04 10:41:02 -0500

    test(back): #1372 add domain tests
    - added some more tests for domain

--- app/test/domain_test/test_project.py
+++ app/test/domain_test/test_project.py
@@ -1,9 +1,12 @@
 import pytest
-
-from app.domain.project import (get_email_recipients, validate_tags,
-    validate_project, get_vulnerabilities)
+from datetime import datetime
 
 from django.test import TestCase
+
+from app.domain.project import (
+    get_email_recipients, validate_tags, validate_project, get_vulnerabilities,
+    get_pending_closing_check, get_last_closing_vuln, get_last_closing_date)
+from app.dal.integrates_dal import DYNAMODB_RESOURCE
 
 
 @pytest.mark.usefixtures(
@@ -25,3 +28,39 @@ class ProjectTest(TestCase):
     def test_validate_project(self):
         assert validate_project('unittesting')
         assert not validate_project('unexisting_project')
+
+    def test_get_vulnerabilities(self):
+        findings_to_get = ['463558592', '422286126']
+        findings = [
+            DYNAMODB_RESOURCE.Table('FI_findings').get_item(
+                TableName='FI_findings',
+                Key={'finding_id': finding_id}
+            )['Item']
+            for finding_id in findings_to_get]
+
+        test_data = get_vulnerabilities(findings, 'openVulnerabilities')
+        expected_output = 6
+        assert test_data == expected_output
+
+        test_data = get_vulnerabilities(findings, 'closedVulnerabilities')
+        expected_output = 3
+        assert test_data == expected_output
+
+    def test_get_pending_closing_checks(self):
+        test_data = get_pending_closing_check('unittesting')
+        expected_output = 4
+        assert test_data == expected_output
+
+    def test_get_last_closing_vuln(self):
+        findings_to_get = ['463558592', '422286126']
+        findings = [
+            DYNAMODB_RESOURCE.Table('FI_findings').get_item(
+                TableName='FI_findings',
+                Key={'finding_id': finding_id}
+            )['Item']
+            for finding_id in findings_to_get]
+        test_data = get_last_closing_vuln(findings)
+        actual_date = datetime.now().date()
+        initial_date = datetime(2019, 01, 15).date()
+        expected_output = actual_date - initial_date
+        assert test_data == expected_output.days
