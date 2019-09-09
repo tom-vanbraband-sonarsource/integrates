2dc12b6dab58e562a136882d5b9ea4f049c0817e
Mateo Norena <mnorena@fluidattacks.com>
2019-09-09 17:28:50 -0500

    feat(build): #1095.2 install dynamodb_local

--- ci-scripts/jobs/test-app.sh
+++ ci-scripts/jobs/test-app.sh
@@ -15,6 +15,8 @@ test_app() {
   cp -a "$PWD" /usr/src/app_src
   cd /usr/src/app_src || return 1
   service redis-server start
+  java -Djava.library.path=/usr/local/lib/dynamodb-local/DynamoDBLocal_lib -jar /usr/local/lib/dynamodb-local/DynamoDBLocal.jar -sharedDb -port 8022 &
+  DYNAMODB_PROCESS=$!
   pytest \
     --ds=fluidintegrates.settings \
     --verbose \
@@ -29,7 +31,7 @@ test_app() {
     --junitxml=build/test/results.xml \
     app/test/
   cp -a build/coverage/results.xml "$CI_PROJECT_DIR/coverage.xml"
-
+  kill $DYNAMODB_PROCESS
   cd "$CI_PROJECT_DIR" || return 1
 }
 
--- deploy/containers/common/vars/run.sh
+++ deploy/containers/common/vars/run.sh
@@ -9,6 +9,7 @@ if [ "$1" = 'app' ]; then
   a2ensite 000-default.conf
   /etc/init.d/td-agent restart
   service redis-server restart
+  java -Djava.library.path=/usr/local/lib/dynamodb-local/DynamoDBLocal_lib -jar /usr/local/lib/dynamodb-local/DynamoDBLocal.jar -sharedDb -port 8022 &
   /usr/sbin/apache2ctl -D FOREGROUND
 elif [ "$1" = 'bot' ]; then
   if [ "$CI_COMMIT_REF_NAME" = 'master' ]; then
