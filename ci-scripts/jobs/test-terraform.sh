f3f04b3ab2b73dea1f692a3f6c0a00c95deabf5a
dsalazaratfluid <dsalazar@fluidattacks.com>
2019-09-26 19:32:28 -0500

    fix(build): #1442.3 test-terraform
    - Use env var for deciding which
    credentials use for aws secrets

--- ci-scripts/jobs/test-terraform.sh
+++ ci-scripts/jobs/test-terraform.sh
@@ -17,23 +17,23 @@ test_terraform() {
   export TF_VAR_aws_s3_resources_bucket
 
   AWS_ACCESS_KEY_ID="$(
-    vault read -field=aws_terraform_access_key secret/integrates/development
+    vault read -field=aws_terraform_access_key secret/integrates/$ENV_NAME
   )"
   AWS_SECRET_ACCESS_KEY="$(
-    vault read -field=aws_terraform_secret_key secret/integrates/development
+    vault read -field=aws_terraform_secret_key secret/integrates/$ENV_NAME
   )"
   TF_VAR_aws_s3_resources_bucket="$(
-    vault read -field=aws_s3_resources_bucket secret/integrates/development
+    vault read -field=aws_s3_resources_bucket secret/integrates/$ENV_NAME
   )"
 
   cd deploy/terraform || return 1
-  terraform init --backend-config="bucket=${FS_S3_BUCKET}"
+  terraform init --backend-config="bucket=$FS_S3_BUCKET"
   terraform validate
   terraform plan -refresh=true -out=plan
   terraform show -no-color plan > plan.txt
-  mv plan.txt "${CI_PROJECT_DIR}"
+  mv plan.txt "$CI_PROJECT_DIR"
   rm plan
-  cd "${CI_PROJECT_DIR}" || return 1
+  cd "$CI_PROJECT_DIR" || return 1
 }
 
 test_terraform
