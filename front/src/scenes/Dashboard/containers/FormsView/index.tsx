4f8f3eab364a3530bafd9e02374ac71aec7bfb67
dacevedoatfluid <dacevedo@fluidattacks.com>
2019-09-27 12:28:31 -0500

    refac(front): #1283.18 disable fs finding form

--- front/src/scenes/Dashboard/containers/FormsView/index.tsx
+++ front/src/scenes/Dashboard/containers/FormsView/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import { Redirect, Route, Switch } from "react-router";
 import { NavLink } from "react-router-dom";
 import { default as Frame } from "../../../../components/Frame";
@@ -11,7 +12,22 @@ const formsView: React.FC = (): JSX.Element => {
   );
 
   const findingForm: (() => JSX.Element) = (): JSX.Element => (
-    <Frame src="https://fluidattacks.com/forms/hallazgos" height={5500} id="ifrmFindings" />
+    <Row>
+      <Col xs={12} md={12} sm={12} style={{ textAlign: "center" }}>
+        <h3>There is a new way to report findings!</h3>
+        <br />
+        <p>
+          Read more details in&nbsp;
+          <a
+            href="https://community.fluidattacks.com/t/finding-report-from-integrates/454"
+            rel="noopener"
+            target="_blank"
+          >
+            Fluid Attacks's community
+          </a>
+        </p>
+      </Col>
+    </Row>
   );
 
   const closingForm: (() => JSX.Element) = (): JSX.Element => (
