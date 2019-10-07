a31558717e1291398bcc63eebe62165310043dcf
Sebastian Lizcano <slizcano@fluidattacks.com>
2019-10-07 17:11:00 -0500

    test(front): #1372 add form fields test

--- front/src/utils/forms/index.spec.tsx
+++ front/src/utils/forms/index.spec.tsx
@@ -1,13 +1,28 @@
-import { configure, shallow, ShallowWrapper } from "enzyme";
+import { configure, mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 import ReactSixteenAdapter from "enzyme-adapter-react-16";
 import React from "react";
+import { Provider } from "react-redux";
 import { Field } from "redux-form";
+import { GenericForm } from "../../scenes/Dashboard/components/GenericForm";
+import store from "../../store";
 import { required } from "../validations";
-import { dropdownField, fileInputField, phoneNumberField, textAreaField, textField } from "./fields";
+import { autocompleteTextField, dateField, dropdownField, fileInputField, phoneNumberField, textAreaField,
+  textField } from "./fields";
 
 configure({ adapter: new ReactSixteenAdapter() });
 
 describe("Form fields", () => {
+
+  it("should return a autocompleteTextField function", () => {
+    expect(typeof (autocompleteTextField))
+      .toEqual("function");
+  });
+
+  it("should return a dateField function", () => {
+    expect(typeof (dateField))
+      .toEqual("function");
+  });
+
   it("should return a textField function", () => {
     expect(typeof (textField))
       .toEqual("function");
@@ -97,6 +112,47 @@ describe("Form fields", () => {
       />,
     );
     expect(wrapper.find("textAreaTest"))
+      .toBeTruthy();
+  });
+
+  it("should render autocompleteTextField component", () => {
+    const titleSuggestions: string[] = [""];
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <GenericForm
+          name=""
+          onSubmit={jest.fn()}
+        >
+          <Field
+            component={autocompleteTextField}
+            name="autocompleteTest"
+            suggestions={titleSuggestions}
+            type="text"
+            validate={[required]}
+          />
+        </GenericForm>
+      </Provider>,
+    );
+    expect(wrapper.find("autocompleteTest"))
+      .toBeTruthy();
+  });
+
+  it("should render dateField component", () => {
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <GenericForm
+          name=""
+          onSubmit={jest.fn()}
+        >
+          <Field
+            name="dateTest"
+            component={dateField}
+            validate={[required]}
+          />
+        </GenericForm>
+      </Provider>,
+    );
+    expect(wrapper.find("dateTest"))
       .toBeTruthy();
   });
 });
