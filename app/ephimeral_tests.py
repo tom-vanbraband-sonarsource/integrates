9dd4a53777d28e377f8bee195f47206741f114cc
Mateo Norena <mnorena@fluidattacks.com>
2019-10-15 16:23:56 -0500

    fix(build): #1474 selenium tests
    - save screenshots before every click to follow all steps

--- app/ephimeral_tests.py
+++ app/ephimeral_tests.py
@@ -78,13 +78,14 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '03-01-indicators.png')
         proj_elem.click()
         WebDriverWait(selenium,
                       self.delay).until(
                             EC.presence_of_element_located(
                                 (By.XPATH,
                                 "//*[contains(text(), 'Max severity found')]")))
-        selenium.save_screenshot(SCR_PATH + '03-indicators.png')
+        selenium.save_screenshot(SCR_PATH + '03-02-indicators.png')
         assert 'Max severity found' in selenium.page_source
 
     def test_04_findings(self):
@@ -94,6 +95,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '04-01-findings.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -102,7 +104,7 @@ class ViewTestCase(unittest.TestCase):
                             EC.presence_of_element_located(
                                 (By.XPATH,
                                 "//*[contains(text(), 'Reflected Cross Site')]")))
-        selenium.save_screenshot(SCR_PATH + '04-findings.png')
+        selenium.save_screenshot(SCR_PATH + '04-02-findings.png')
         assert 'Reflected Cross Site' in selenium.page_source
 
     def test_05_finding(self):
@@ -112,6 +114,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '05-01-finding.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -120,6 +123,7 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '05-02-finding.png')
         find_ele.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -128,7 +132,7 @@ class ViewTestCase(unittest.TestCase):
                                 "//*[contains(text(), 'http://localhost/bWAPP/htmli_post.php')]")))
 
         time.sleep(5)
-        selenium.save_screenshot(SCR_PATH + '05-finding.png')
+        selenium.save_screenshot(SCR_PATH + '05-03-finding.png')
         assert 'The forms in the application allow the injection of code' in selenium.page_source
         assert 'REQ.0173. The system must discard all' in selenium.page_source
         assert 'http://localhost/bWAPP/htmli_post.php' in selenium.page_source
@@ -140,6 +144,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '06-01-severity.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -148,11 +153,13 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '06-02-severity.png')
         find_ele.click()
         sev_elem = WebDriverWait(selenium,
                                 self.delay).until(
                                     EC.presence_of_element_located(
                                         (By.ID, 'cssv2Item')))
+        selenium.save_screenshot(SCR_PATH + '06-03-severity.png')
         sev_elem.click()
 
         WebDriverWait(selenium,
@@ -162,7 +169,7 @@ class ViewTestCase(unittest.TestCase):
                             "//*[contains(text(), 'CVSS v3 Temporal')]")))
 
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '06-severity.png')
+        selenium.save_screenshot(SCR_PATH + '06-04-severity.png')
         assert 'Proof of Concept' in selenium.page_source
 
     def test_07_evidence(self):
@@ -172,6 +179,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '07-01-evidence.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -180,11 +188,13 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '07-02-evidence.png')
         find_ele.click()
         sev_elem = WebDriverWait(selenium,
                                 self.delay).until(
                                     EC.presence_of_element_located(
                                         (By.ID, 'evidenceItem')))
+        selenium.save_screenshot(SCR_PATH + '07-03-evidence.png')
         sev_elem.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -192,7 +202,7 @@ class ViewTestCase(unittest.TestCase):
                             (By.XPATH,
                             "//*[contains(text(), 'Pop-up message')]")))
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '07-evidence.png')
+        selenium.save_screenshot(SCR_PATH + '07-04-evidence.png')
         assert 'Pop-up message' in selenium.page_source
 
     def test_08_exploit(self):
@@ -202,6 +212,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '08-01-exploit.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -210,11 +221,13 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '08-02-exploit.png')
         find_ele.click()
         sev_elem = WebDriverWait(selenium,
                                 self.delay).until(
                                     EC.presence_of_element_located(
                                         (By.ID, 'exploitItem')))
+        selenium.save_screenshot(SCR_PATH + '08-03-exploit.png')
         sev_elem.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -222,7 +235,7 @@ class ViewTestCase(unittest.TestCase):
                             (By.XPATH,
                             "//*[contains(text(), 'http.has_xss')]")))
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '08-exploit.png')
+        selenium.save_screenshot(SCR_PATH + '08-04-exploit.png')
         assert 'http.has_xss' in selenium.page_source
 
     def test_09_tracking(self):
@@ -232,6 +245,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '09-01-tracking.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -240,11 +254,13 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '09-02-tracking.png')
         find_ele.click()
         sev_elem = WebDriverWait(selenium,
                                 self.delay).until(
                                     EC.presence_of_element_located(
                                         (By.ID, 'trackingItem')))
+        selenium.save_screenshot(SCR_PATH + '09-03-tracking.png')
         sev_elem.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -252,7 +268,7 @@ class ViewTestCase(unittest.TestCase):
                             (By.XPATH,
                             "//*[contains(text(), '2019-01-21')]")))
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '09-tracking.png')
+        selenium.save_screenshot(SCR_PATH + '09-04-tracking.png')
         assert '2019-01-21' in selenium.page_source
 
     def test_10_comments(self):
@@ -262,6 +278,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '10-01-comments.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -270,11 +287,13 @@ class ViewTestCase(unittest.TestCase):
                                     EC.presence_of_element_located(
                                         (By.XPATH,
                                          "//*[contains(text(), 'Reflected Cross Site')]")))
+        selenium.save_screenshot(SCR_PATH + '10-02-comments.png')
         find_ele.click()
         sev_elem = WebDriverWait(selenium,
                                 self.delay).until(
                                     EC.presence_of_element_located(
                                         (By.ID, 'commentItem')))
+        selenium.save_screenshot(SCR_PATH + '10-03-comments.png')
         sev_elem.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -282,7 +301,7 @@ class ViewTestCase(unittest.TestCase):
                             (By.XPATH,
                             "//*[contains(text(), 'Validations were run against 5 of the 10 reported fields')]")))
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '10-comments.png')
+        selenium.save_screenshot(SCR_PATH + '10-04-comments.png')
         assert 'Validations were run against 5 of the 10 reported fields' in selenium.page_source
 
     def test_11_techpdf(self):
@@ -292,6 +311,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '11-01-techpdf.png')
         proj_elem.click()
 
         selenium.get(self.url + '/dashboard#!/project/BWAPP/findings')
@@ -306,6 +326,7 @@ class ViewTestCase(unittest.TestCase):
                                         EC.presence_of_element_located(
                                             (By.XPATH,
                                              "//button[contains(text(),'Reports')]")))
+        selenium.save_screenshot(SCR_PATH + '11-02-techpdf.png')
         rep_modal.click()
         tech_pdf_report = WebDriverWait(selenium,
                                         self.delay).until(
@@ -313,13 +334,14 @@ class ViewTestCase(unittest.TestCase):
                                                 (By.XPATH,
                                                  "//div[@id='techReport']//button[contains(text(), 'PDF')]")))
 
+        selenium.save_screenshot(SCR_PATH + '11-03-techpdf.png')
         tech_pdf_report.click()
         WebDriverWait(selenium,
                       self.delay).until(
                         EC.presence_of_element_located(
                             (By.XPATH,
                              "//*[contains(text(), 'Reflected Cross')]")))
-        selenium.save_screenshot(SCR_PATH + '11-techpdf.png')
+        selenium.save_screenshot(SCR_PATH + '11-04-techpdf.png')
         assert 'Reflected Cross Site' in selenium.page_source
 
     def test_13_events(self):
@@ -329,6 +351,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '13-01-events.png')
         proj_elem.click()
         selenium.get(self.url + '/dashboard#!/project/BWAPP/events')
 
@@ -337,7 +360,7 @@ class ViewTestCase(unittest.TestCase):
                                         EC.presence_of_element_located(
                                             (By.XPATH,
                                              "//*[contains(text(), 'not possible to access the repo')]")))
-        selenium.save_screenshot(SCR_PATH + '131-events.png')
+        selenium.save_screenshot(SCR_PATH + '13-02-events.png')
         event_tab.click()
         WebDriverWait(selenium,
                       self.delay).until(
@@ -346,7 +369,7 @@ class ViewTestCase(unittest.TestCase):
                              "//*[contains(text(), 'Affected components')]")))
 
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '132-events.png')
+        selenium.save_screenshot(SCR_PATH + '13-03-events.png')
         assert 'Test Event' in selenium.page_source
 
     def test_14_resources(self):
@@ -356,6 +379,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '14-01-resources.png')
         proj_elem.click()
         selenium.get(self.url + '/dashboard#!/project/BWAPP/resources')
 
@@ -364,7 +388,7 @@ class ViewTestCase(unittest.TestCase):
                 (By.XPATH, "//*[contains(text(), 'Environments')]")))
 
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '14-resources.png')
+        selenium.save_screenshot(SCR_PATH + '14-02-resources.png')
         assert 'https://fluidattacks.com' in selenium.page_source
 
     def test_15_project_comments(self):
@@ -374,6 +398,7 @@ class ViewTestCase(unittest.TestCase):
                                       EC.presence_of_element_located(
                                           (By.XPATH,
                                            "//*[contains(text(), 'BWAPP Sample')]")))
+        selenium.save_screenshot(SCR_PATH + '15-01-proj_comments.png')
         proj_elem.click()
         selenium.get(self.url + '/dashboard#!/project/BWAPP/comments')
 
@@ -382,5 +407,5 @@ class ViewTestCase(unittest.TestCase):
                 (By.XPATH, "//*[contains(text(), 'Now we can post comments on projects')]")))
 
         time.sleep(3)
-        selenium.save_screenshot(SCR_PATH + '15-proj_comments.png')
+        selenium.save_screenshot(SCR_PATH + '15-02-proj_comments.png')
         assert 'Now we can post comments on projects' in selenium.page_source
