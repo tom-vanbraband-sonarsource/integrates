fcfe6ba5a15fde1ac56c12c26ee85ee47bd5ddcd
Andres Roldan <aroldan@fluidattacks.com>
2019-11-08 12:03:51 -0500

    perf(build): #1530 paralellize prospector
    - Run prospectors commands in parallel
    - Reduce layers in deps-development container

--- ci-scripts/jobs/test-app.sh
+++ ci-scripts/jobs/test-app.sh
@@ -5,8 +5,8 @@
 # Linters
 run_lint () {
     RETVAL=0
-    prospector -F -s high -u django -i node_modules app/ || RETVAL=1
-    prospector -F -s veryhigh -u django -i node_modules fluidintegrates/ || RETVAL=1
+    prospector -F -s high -u django -i node_modules app/ || RETVAL=1 &
+    prospector -F -s veryhigh -u django -i node_modules fluidintegrates/ || RETVAL=1 &
     return $RETVAL
 }
 
