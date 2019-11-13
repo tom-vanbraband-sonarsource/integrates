b493f018702bd656bb1c998f9f50298b9747ea78
Andres Roldan <aroldan@fluidattacks.com>
2019-11-13 11:34:21 -0500

    test(back): #1513 fix test logic
    - Will calculate mean time dynamically

--- app/test/domain_test/test_project.py
+++ app/test/domain_test/test_project.py
@@ -188,7 +188,11 @@ class ProjectTest(TestCase):
 
         test_data = get_mean_remediate(open_finding)
         current_day = datetime.now(tz=timezone('America/Bogota')).date()
-        expected_output = Decimal('201.0').quantize(Decimal('0.1'))
+        expected_output = Decimal(round(int(
+            int((current_day - datetime(2019, 2, 16).date()).days) * 2
+            + int((current_day - datetime(2019, 2, 17).date()).days) * 2
+            + int((current_day - datetime(2018, 12, 17).date()).days))
+            / 7.0)).quantize(Decimal('0.1'))
         assert test_data == expected_output
 
         closed_vuln_finding = ['457497316']
