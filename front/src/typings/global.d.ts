2d5a646c2a0c8f6646016e354a6fd05db738ef83
John Perez <jperez@fluidattacks.com>
2019-11-15 09:54:43 -0500

    feat(front): #1535.1 prepare new datatable
    - Orginize global types and set specific types
    - Add react-bootstrap-table-next component

--- front/src/typings/global.d.ts
+++ front/src/typings/global.d.ts
@@ -2,10 +2,10 @@ declare module '*.css';
 
 //Typings for img files
 declare module "*.png" {
-  const value: any;
-  export = value;
+    const value: any;
+    export = value;
 }
 
 declare module "*.svg" { const value: any; export = value; }
 
-type dict = { [value: string]: string | dict};
+type dict = { [value: string]: string | dict };
---
+++ front/src/typings/react-bootstrap-table-2/index.d.ts
@@ -0,0 +1,1 @@
+// This will be the react-bootstrap-table-2 types
