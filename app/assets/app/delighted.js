795854ad341634030f7a620a48bef98e5afc33d0
Neil Sanchez <nsanchez@fluidattacks.com>
2019-11-20 08:42:40 -0500

    refac(front): #1351 fix unexpected expressions
    - Added function to execute delighted
    - Changed comma operator by semicolon
    - Changed if conditional by short circuit statement

--- app/assets/app/delighted.js
+++ app/assets/app/delighted.js
@@ -10,7 +10,10 @@ function pushElement() {
   }
 }
 
-!function(e,t,r,n,a){if(!e[a]){for(i=e[a]=[],s=0;s<r.length;s++){var c=r[s];if(i[c]){i[c]=i[c]}else{i[c]=pushElement()(c)}}i.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+n+"/"+a+".js";var p=t.getElementsByTagName("script")[0];p.parentNode.insertBefore(o,p)}}(window,document,["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],"C2IiXJX4CW06goZ8","delighted");
+function delightedExecute(e,t,r,n,a){if(!e[a]){for(i=e[a]=[],s=0;s<r.length;s++){var c=r[s];i[c]=i[c]||pushElement()(c)}i.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript";o.async=!0;o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+n+"/"+a+".js";var p=t.getElementsByTagName("script")[0];p.parentNode.insertBefore(o,p)}}
+
+delightedExecute(window,document,["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],"C2IiXJX4CW06goZ8","delighted");
+
 if(!userEmail.includes('fluidattacks.com')) {
 	delighted.survey({
       email: userEmail,
