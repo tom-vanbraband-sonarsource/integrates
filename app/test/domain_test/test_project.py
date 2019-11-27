ba893ba777e67189b9156007a8a7c8749aac1dcd
Andres Roldan <aroldan@fluidattacks.com>
2019-11-27 08:24:39 -0500

    test(back): #1576 fix test

--- app/test/domain_test/test_project.py
+++ app/test/domain_test/test_project.py
@@ -189,7 +189,7 @@ class ProjectTest(TestCase):
         test_data = get_mean_remediate(open_finding)
         current_day = datetime.now(tz=timezone('America/Bogota')).date()
         expected_output = Decimal(round(int(
-            int((current_day - datetime(2019, 2, 17).date()).days) * 2
+            int((current_day - datetime(2019, 2, 19).date()).days) * 2
             + int((current_day - datetime(2019, 2, 18).date()).days) * 2
             + int((current_day - datetime(2018, 12, 17).date()).days))
             / 7.0)).quantize(Decimal('0.1'))
