485d3124ca161c13a03f6696a14f10a50ed8c042
Andres Roldan <aroldan@fluidattacks.com>
2019-11-28 16:54:34 -0500

    fix(back): #1577 rename lambda function

--- deploy/terraform/lambda/send_mail_new_vulnerabilities.tf
+++ deploy/terraform/lambda/send_mail_new_vulnerabilities.tf
@@ -1,6 +1,6 @@
-resource "aws_lambda_function" "send_mail_new_vulnerabilities" {
+resource "aws_lambda_function" "integrates-send-mail-new-vulnerabilities" {
   filename      = "../../lambda/packages/send_mail_new_vulnerabilities.zip"
-  function_name = "send_mail_new_vulnerabilities"
+  function_name = "integrates-send-mail-new-vulnerabilities"
   role          = aws_iam_role.integrates-lambdas.arn
   handler       = "lambda_send_mail_new_vulnerabilities.send_mail_new_vulnerabilities"
   publish       = true
