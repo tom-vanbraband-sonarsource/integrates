858459f9dc6f922f3c0675745ed4ca94d44005c9
Andres Roldan <aroldan@fluidattacks.com>
2019-12-03 13:53:10 -0500

    fix(back): #1577 gracefully death
    - If the lambda raises an exception,
    die gracefully

--- lambda/send_mail_new_vulnerabilities/lambda_send_mail_new_vulnerabilities.py
+++ lambda/send_mail_new_vulnerabilities/lambda_send_mail_new_vulnerabilities.py
@@ -1,21 +1,31 @@
 """Send new vulnerabilities mail lambda function."""
-
 import json
-
 import mandrill
 
 
 # pylint: disable=unused-argument
 def send_mail_new_vulnerabilities(event, context):
     """Lambda code."""
-    record = event['Records'][0]
-    template_name = record['attributes']['MessageGroupId']
-    body = json.loads(record['body'])
+    try:
+        record = event['Records'][0]
+        template_name = record['attributes']['MessageGroupId']
+        body = json.loads(record['body'])
 
-    message = body['message']
-    api_key = body['api_key']
-    mandrill_client = mandrill.Mandrill(api_key)
-    mandrill_client.messages.send_template(template_name, [], message)
+        message = body['message']
+        api_key = body['api_key']
+    except KeyError:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('An invalid message was given.')
+        }
+    try:
+        mandrill_client = mandrill.Mandrill(api_key)
+        mandrill_client.messages.send_template(template_name, [], message)
+    except mandrill.InvalidKeyError:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('An invalid key was given.')
+        }
     return {
         'statusCode': 200,
         'body': json.dumps('Done.')
