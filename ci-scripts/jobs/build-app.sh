ac7fe89ffb7088ebafd9a5ed805f97ff7252ecf7
Daniel Salazar <dsalazar@fluidattacks.com>
2019-12-17 15:41:03 -0500

    feat(build): #1613.4 app move app
    - Move app to docker build
    - Move docker build jobs to autoscaling as machines are now bigger

--- ci-scripts/jobs/build-app.sh
+++ ci-scripts/jobs/build-app.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 build_app() {
 
@@ -7,16 +7,19 @@ build_app() {
   set -e
 
   # import functions
+  . <(curl -s https://gitlab.com/fluidattacks/public/raw/master/shared-scripts/build-container.sh)
   . ci-scripts/helpers/others.sh
 
-  vault_install /busybox
+  vault_install /usr/bin/
   vault_login
 
   # Set necessary envars
+  local NAME
   export FI_DRIVE_AUTHORIZATION
   export FI_DRIVE_AUTHORIZATION_CLIENT
   export FI_SSL_KEY
   export FI_SSL_CERT
+  NAME='app'
   FI_SSL_KEY=$(vault read -field=ssl_key secret/integrates/$ENV_NAME)
   FI_SSL_CERT=$(vault read -field=ssl_cert secret/integrates/$ENV_NAME)
 
@@ -24,16 +27,11 @@ build_app() {
   FI_VERSION=$(app_version)
   echo -n "$FI_VERSION" > version.txt
 
-  kaniko_login
-
-  NAME='app'
-
   # Build container using kaniko
-  kaniko_build \
-    "$NAME" \
-    eph=true \
-    cache=false \
-    --single-snapshot \
+  build_container \
+    "registry.gitlab.com/fluidattacks/integrates/$NAME:$CI_COMMIT_REF_NAME" \
+    "$CI_PROJECT_DIR" \
+    -f "deploy/containers/$NAME/Dockerfile" \
     --build-arg CI_API_V4_URL="$CI_API_V4_URL" \
     --build-arg CI_COMMIT_REF_NAME="$CI_COMMIT_REF_NAME" \
     --build-arg CI_PROJECT_ID="$CI_PROJECT_ID" \
