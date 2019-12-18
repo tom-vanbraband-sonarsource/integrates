e3f359f0a91af8f1eaaeb13098295d778cdd6a57
Daniel Salazar <dsalazar@fluidattacks.com>
2019-12-18 16:15:40 -0500

    feat(build): #1613.6 always build mobile
    - Always build mobile with cache

--- ci-scripts/jobs/deps-mobile.sh
+++ ci-scripts/jobs/deps-mobile.sh
@@ -9,30 +9,17 @@ deps_mobile() {
 
   # import functions
   . <(curl -s https://gitlab.com/fluidattacks/public/raw/master/shared-scripts/build-container.sh)
-  . ci-scripts/helpers/check-changed.sh
 
   local NAME
-  local FILES
 
   NAME='deps-mobile'
-  FILES=(
-    'mobile/package.json'
-    'mobile/Gemfile'
-    'deploy/containers/deps-mobile/Dockerfile'
-    'ci-scripts/jobs/deps-mobile.sh'
-  )
 
-  if check_file_changed "${FILES[@]}" \
-     || [ $SCHEDULE ]; then
-    echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf \
-      && sysctl -p
-    build_container \
-      "registry.gitlab.com/fluidattacks/integrates/$NAME:$CI_COMMIT_REF_NAME" \
-      "$CI_PROJECT_DIR" \
-      -f "deploy/containers/$NAME/Dockerfile"
-  else
-    echo "No relevant files for $NAME were modified. Skipping build."
-  fi
+  echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf \
+    && sysctl -p
+  build_container \
+    "registry.gitlab.com/fluidattacks/integrates/$NAME:$CI_COMMIT_REF_NAME" \
+    "$CI_PROJECT_DIR" \
+    -f "deploy/containers/$NAME/Dockerfile"
 }
 
 deps_mobile
