a2c8a3f6d2ed3ab08ffd1d4a9100eddcf62651ee
Andres Roldan <aroldan@fluidattacks.com>
2019-12-23 09:09:23 -0500

    feat(infra): #1623 bye-bye apache
    - Use uvicorn as new webserver

--- app/urls.py
+++ app/urls.py
@@ -24,26 +24,26 @@ handler500 = 'app.views.error500'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Principal process.
-    url(r'^index/?$', views.index, name='index'),
-    url(r'^error500/?$', views.error500, name='error500'),
-    url(r'^error401/?$', views.error401, name='error401'),
-    url(r'^login/?$', services.login, name='login'),
-    url(r'^logout/?$', views.logout, name='logout'),
-    url(r'^dashboard/?$', views.app, name='dashboard'),
-    url(r'^registration/?$', views.app, name='registration'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
-    url(r'^api/?\.*$', csrf_exempt(verify_csrf(APIView.as_view()))),
+    url(r'^/?index/?$', views.index, name='index'),
+    url(r'^/?error500/?$', views.error500, name='error500'),
+    url(r'^/?error401/?$', views.error401, name='error401'),
+    url(r'^/?login/?$', services.login, name='login'),
+    url(r'^/?logout/?$', views.logout, name='logout'),
+    url(r'^/?dashboard/?$', views.app, name='dashboard'),
+    url(r'^/?registration/?$', views.app, name='registration'),
+    url(r'^/?oauth/', include('social_django.urls', namespace='social')),
+    url(r'^/?api/?\.*$', csrf_exempt(verify_csrf(APIView.as_view()))),
     # Use of Formstack services.
-    url(r'^project/(?P<project>[A-Za-z0-9]+)/(?P<evidence_type>[A-Za-z0-9]+)/'
+    url(r'^/?project/(?P<project>[A-Za-z0-9]+)/(?P<evidence_type>[A-Za-z0-9]+)/'
         r'(?P<findingid>[0-9]+)/([A-Za-z.=]+)/(?P<fileid>[\w\.-]+)?$',
         views.get_evidence),
-    url(r'^(?P<findingid>[0-9]+)/download_vulnerabilities?$',
+    url(r'^/?(?P<findingid>[0-9]+)/download_vulnerabilities?$',
         views.download_vulnerabilities),
     # Documentation.
-    url(r'^pdf/(?P<lang>[a-z]{2})/project/(?P<project>[A-Za-z0-9]+)/(?P<doctype>[a-z]+)/?$',
+    url(r'^/?pdf/(?P<lang>[a-z]{2})/project/(?P<project>[A-Za-z0-9]+)/(?P<doctype>[a-z]+)/?$',
         views.project_to_pdf),
-    url(r'^xls/(?P<lang>[a-z]{2})/project/(?P<project>[A-Za-z0-9]+)/?$',
+    url(r'^/?xls/(?P<lang>[a-z]{2})/project/(?P<project>[A-Za-z0-9]+)/?$',
         views.project_to_xls),
-    url(r'^complete_report/?$', views.generate_complete_report),
-    url(r'^export_all_vulnerabilities/?$', views.export_all_vulnerabilities)
+    url(r'^/?complete_report/?$', views.generate_complete_report),
+    url(r'^/?export_all_vulnerabilities/?$', views.export_all_vulnerabilities)
 ]
--- deploy/containers/common/vars/run.sh
+++ deploy/containers/common/vars/run.sh
@@ -20,22 +20,24 @@ fi
 sops_vars "$ENV_NAME"
 
 if [ "$CI_COMMIT_REF_NAME" = 'master' ]; then
-  ./manage.py crontab add
-  crontab -l >> /tmp/mycron
-  crontab /tmp/mycron
-  service cron restart
+    ./manage.py crontab add
+    crontab -l >> /tmp/mycron
+    crontab /tmp/mycron
+    service cron restart
 fi
 
 python3 deploy/containers/common/vars/render.py "$ENV_NAME"
 ./manage.py collectstatic --no-input
-a2ensite integrates-ssl.conf
-a2ensite 000-default.conf
 /etc/init.d/td-agent restart
 
 if [[ "$ENV_NAME" = "development" ]]; then
-  service redis-server restart
-  java -Djava.library.path=/usr/local/lib/dynamodb-local/DynamoDBLocal_lib -jar /usr/local/lib/dynamodb-local/DynamoDBLocal.jar -sharedDb -port 8022 &
-  . deploy/containers/common/vars/provision_local_db.sh
+    service redis-server restart
+    java -Djava.library.path=/usr/local/lib/dynamodb-local/DynamoDBLocal_lib -jar /usr/local/lib/dynamodb-local/DynamoDBLocal.jar -sharedDb -port 8022 &
+    . deploy/containers/common/vars/provision_local_db.sh
 fi
 
-/usr/sbin/apache2ctl -D FOREGROUND
+uvicorn --workers=4 \
+    --host=0.0.0.0 \
+    --port=80 \
+    --root-path=/integrates \
+    fluidintegrates.asgi:application
