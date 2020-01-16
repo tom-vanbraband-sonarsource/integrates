fb43181a31da50e82509409b98d519638944ab67
David Acevedo <dacevedo@fluidattacks.com>
2020-01-16 18:27:18 -0500

    style(front): #1665.5 properly indent code

--- front/src/scenes/Dashboard/containers/TrackingView/index.tsx
+++ front/src/scenes/Dashboard/containers/TrackingView/index.tsx
@@ -45,75 +45,75 @@ export const trackingView: React.FC<TrackingViewProps> = (props: TrackingViewPro
           if (_.isUndefined(data) || loading) { return <React.Fragment />; }
 
           return (
-        <React.Fragment>
-          <Row>
-            <Col md={12}>
-              {_.includes(["admin", "analyst"], userRole) ?
+            <React.Fragment>
               <Row>
-                <Col md={2} className={style.text_right}>
-                  <label className={style.track_title}>
-                    {translate.t("search_findings.tab_tracking.pending")}
-                  </label>
-                </Col>
-                <Col md={10}>
-                  <VulnerabilitiesView
-                    analyst={_.includes(["admin", "analyst"], userRole)}
-                    editMode={false}
-                    editModePending={true}
-                    state="PENDING"
-                    findingId={findingId}
-                    userRole={userRole}
-                  />
+                <Col md={12}>
+                  {_.includes(["admin", "analyst"], userRole) ?
+                    <Row>
+                      <Col md={2} className={style.text_right}>
+                        <label className={style.track_title}>
+                          {translate.t("search_findings.tab_tracking.pending")}
+                        </label>
+                      </Col>
+                      <Col md={10}>
+                        <VulnerabilitiesView
+                          analyst={_.includes(["admin", "analyst"], userRole)}
+                          editMode={false}
+                          editModePending={true}
+                          state="PENDING"
+                          findingId={findingId}
+                          userRole={userRole}
+                        />
+                      </Col>
+                    </Row>
+                    : undefined}
+                  <Row>
+                    <Col md={2} className={style.text_right}>
+                      <label className={style.track_title}>{translate.t("search_findings.tab_tracking.open")}</label>
+                    </Col>
+                    <Col md={10}>
+                      <VulnerabilitiesView
+                        analyst={_.includes(["admin", "analyst"], userRole)}
+                        editMode={false}
+                        findingId={findingId}
+                        state="open"
+                        userRole={userRole}
+                      />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={2} className={style.text_right}>
+                      <label className={style.track_title}>{translate.t("search_findings.tab_tracking.closed")}</label>
+                    </Col>
+                    <Col md={10}>
+                      <VulnerabilitiesView
+                        analyst={_.includes(["admin", "analyst"], userRole)}
+                        editMode={false}
+                        findingId={findingId}
+                        state="closed"
+                        userRole={userRole}
+                      />
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-                : undefined}
               <Row>
-                <Col md={2} className={style.text_right}>
-                  <label className={style.track_title}>{translate.t("search_findings.tab_tracking.open")}</label>
-                </Col>
-                <Col md={10}>
-                  <VulnerabilitiesView
-                    analyst={_.includes(["admin", "analyst"], userRole)}
-                    editMode={false}
-                    findingId={findingId}
-                    state="open"
-                    userRole={userRole}
-                  />
+                <Col mdOffset={3} md={9} sm={12}>
+                  <ul className={style.timelineContainer}>
+                    {data.finding.tracking.map((closing: IClosing, index: number): JSX.Element => (
+                      <TrackingItem
+                        closed={closing.closed}
+                        cycle={closing.cycle}
+                        date={closing.date}
+                        effectiveness={closing.effectiveness}
+                        key={index}
+                        open={closing.open}
+                      />
+                    ))}
+                  </ul>
                 </Col>
               </Row>
-              <Row>
-                <Col md={2} className={style.text_right}>
-                  <label className={style.track_title}>{translate.t("search_findings.tab_tracking.closed")}</label>
-                </Col>
-                <Col md={10}>
-                  <VulnerabilitiesView
-                    analyst={_.includes(["admin", "analyst"], userRole)}
-                    editMode={false}
-                    findingId={findingId}
-                    state="closed"
-                    userRole={userRole}
-                  />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        <Row>
-          <Col mdOffset={3} md={9} sm={12}>
-            <ul className={style.timelineContainer}>
-              {data.finding.tracking.map((closing: IClosing, index: number): JSX.Element => (
-                <TrackingItem
-                  closed={closing.closed}
-                  cycle={closing.cycle}
-                  date={closing.date}
-                  effectiveness={closing.effectiveness}
-                  key={index}
-                  open={closing.open}
-                />
-              ))}
-            </ul>
-          </Col>
-        </Row>
-        </React.Fragment>
+            </React.Fragment>
           );
         }}
       </Query>
