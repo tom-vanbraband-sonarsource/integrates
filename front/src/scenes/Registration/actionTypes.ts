a696382dec9ff9ce56becfc52d7cbe4d0ec87448
David Acevedo <dacevedo@fluidattacks.com>
2020-01-24 10:08:15 -0500

    refac(front): #1688.5 remove unused code

--- front/src/scenes/Registration/actionTypes.ts
+++
@@ -1,6 +0,0 @@
-let CHECK_REMEMBER: string;
-CHECK_REMEMBER = "registration/legal_notice/remember";
-
-export {
-  CHECK_REMEMBER,
-};
--- front/src/store/rootReducer.ts
+++ front/src/store/rootReducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers, Reducer } from "redux";
 import { reducer as formReducer } from "redux-form";
 import { dashboard } from "../scenes/Dashboard/reducer";
-import { registration } from "../scenes/Registration/reducer";
 
 const rootReducer: Reducer = combineReducers({
   dashboard,
   form: formReducer,
-  registration,
 });
 
 export = rootReducer;
