d4d53cedfbad38ad8618e4f385fbe313e642a7f0
Andres Roldan <aroldan@fluidattacks.com>
2020-01-24 13:53:11 -0500

    test(back): #1593 improve abac tests
    - More thorough ABAC tests

--- test/util_test/test_abac.py
+++ test/util_test/test_abac.py
@@ -9,41 +9,131 @@ def get_enforcer():
 
 class AbacTest(TestCase):
 
-    def test_enforcer_basic(self):
+    def test_enforcer_anonymous(self):
+        """Tests for an anonymous user."""
         enfor = get_enforcer()
 
         class TestItem:
             pass
 
-        sub1 = TestItem()
-        sub1.username = ''
-
-        sub2 = TestItem()
-        sub2.username = 'someone'
-
-        obj = '/integrates/registration'
-
-        self.assertFalse(enfor.enforce(sub1, obj))
-        self.assertTrue(enfor.enforce(sub2, obj))
-
-        sub3 = TestItem()
-        sub3.username = 'someone'
-        sub3.role = 'customer'
+        sub = TestItem()
+        sub.username = ''
 
         should_deny = (
             '/integrates/export_all_vulnerabilities',
-            '/integrates/12312313/download_vulnerabilities'
+            '/integrates/12312313/download_vulnerabilities',
+            '/integrates/registration',
+            '/integrates/dashboard',
+            '/integrates/xls/',
+            '/integrates/pdf/',
+            '/integrates/logout'
         )
+
+        for url in should_deny:
+            self.assertFalse(enfor.enforce(sub, url))
+
+    def test_enforcer_user_wrong_role(self):
+        """Tests for an user with a wrong role."""
+        enfor = get_enforcer()
+
+        class TestItem:
+            pass
+
+        sub = TestItem()
+        sub.username = 'someone'
+        sub.role = 'guest'
+
+        should_deny = (
+            '/integrates/export_all_vulnerabilities',
+            '/integrates/12312313/download_vulnerabilities',
+            '/integrates/xls/',
+            '/integrates/pdf/',
+        )
+
+        for url in should_deny:
+            self.assertFalse(enfor.enforce(sub, url))
+
+
+    def test_enforcer_customer(self):
+        """Tests for an customer user."""
+        enfor = get_enforcer()
+
+        class TestItem:
+            pass
+
+        sub = TestItem()
+        sub.username = 'someone@customer.com'
+        sub.role = 'customer'
+
+        should_deny = (
+            '/integrates/export_all_vulnerabilities',
+            '/integrates/12312313/download_vulnerabilities',
+        )
+
+        for url in should_deny:
+            self.assertFalse(enfor.enforce(sub, url))
 
         should_allow = (
             '/integrates/registration',
             '/integrates/dashboard',
             '/integrates/xls/',
             '/integrates/pdf/',
+            '/integrates/logout'
+        )
+
+        for url in should_allow:
+            self.assertTrue(enfor.enforce(sub, url))
+
+    def test_enforcer_analyst(self):
+        """Tests for an analyst user."""
+        enfor = get_enforcer()
+
+        class TestItem:
+            pass
+
+        sub = TestItem()
+        sub.username = 'someone@fluidattacks.com'
+        sub.role = 'analyst'
+
+        should_deny = (
+            '/integrates/export_all_vulnerabilities',
         )
 
         for url in should_deny:
-            self.assertFalse(enfor.enforce(sub3, url))
+            self.assertFalse(enfor.enforce(sub, url))
+
+        should_allow = (
+            '/integrates/12312313/download_vulnerabilities',
+            '/integrates/registration',
+            '/integrates/dashboard',
+            '/integrates/xls/',
+            '/integrates/pdf/',
+            '/integrates/logout'
+        )
 
         for url in should_allow:
-            self.assertTrue(enfor.enforce(sub3, url))
+            self.assertTrue(enfor.enforce(sub, url))
+
+    def test_enforcer_admin(self):
+        """Tests for an admin user."""
+        enfor = get_enforcer()
+
+        class TestItem:
+            pass
+
+        sub = TestItem()
+        sub.username = 'admin@fluidattacks.com'
+        sub.role = 'admin'
+
+        should_allow = (
+            '/integrates/export_all_vulnerabilities',
+            '/integrates/12312313/download_vulnerabilities',
+            '/integrates/registration',
+            '/integrates/dashboard',
+            '/integrates/xls/',
+            '/integrates/pdf/',
+            '/integrates/logout'
+        )
+
+        for url in should_allow:
+            self.assertTrue(enfor.enforce(sub, url))
