78c577313b29758dbeacf6a90b5bcb4008362352
Andres Roldan <aroldan@fluidattacks.com>
2020-01-24 17:27:56 -0500

    test(back): #1593 refactored abac tests
    - Now a global list is defined with sensible URLs
    - Each test will define allowed URLs
    - Each test will subtract denied URLs

--- test/util_test/test_abac.py
+++ test/util_test/test_abac.py
@@ -2,6 +2,17 @@ import casbin
 from django.conf import settings
 from django.test import TestCase
 
+GLOBAL_URL_LIST = {
+    '/integrates/export_all_vulnerabilities',
+    '/integrates/12312313/download_vulnerabilities',
+    '/integrates/registration',
+    '/integrates/dashboard',
+    '/integrates/project/BWAPP/12345/evidence/file1.png',
+    '/integrates/xls/',
+    '/integrates/pdf/',
+    '/integrates/logout'
+}
+
 
 def get_enforcer():
     return casbin.Enforcer(settings.CASBIN_POLICY_MODEL_FILE)
@@ -19,15 +30,7 @@ class AbacTest(TestCase):
         sub = TestItem()
         sub.username = ''
 
-        should_deny = (
-            '/integrates/export_all_vulnerabilities',
-            '/integrates/12312313/download_vulnerabilities',
-            '/integrates/registration',
-            '/integrates/dashboard',
-            '/integrates/xls/',
-            '/integrates/pdf/',
-            '/integrates/logout'
-        )
+        should_deny = GLOBAL_URL_LIST
 
         for url in should_deny:
             self.assertFalse(enfor.enforce(sub, url))
@@ -43,16 +46,18 @@ class AbacTest(TestCase):
         sub.username = 'someone'
         sub.role = 'guest'
 
-        should_deny = (
-            '/integrates/export_all_vulnerabilities',
-            '/integrates/12312313/download_vulnerabilities',
-            '/integrates/xls/',
-            '/integrates/pdf/',
-        )
+        should_allow = {
+            '/integrates/registration',
+            '/integrates/dashboard',
+            '/integrates/logout'
+        }
+        should_deny = GLOBAL_URL_LIST - should_allow
 
         for url in should_deny:
             self.assertFalse(enfor.enforce(sub, url))
 
+        for url in should_allow:
+            self.assertTrue(enfor.enforce(sub, url))
 
     def test_enforcer_customer(self):
         """Tests for an customer user."""
@@ -65,24 +70,22 @@ class AbacTest(TestCase):
         sub.username = 'someone@customer.com'
         sub.role = 'customer'
 
-        should_deny = (
-            '/integrates/export_all_vulnerabilities',
-            '/integrates/12312313/download_vulnerabilities',
-        )
-
-        for url in should_deny:
-            self.assertFalse(enfor.enforce(sub, url))
-
-        should_allow = (
+        should_allow = {
             '/integrates/registration',
             '/integrates/dashboard',
             '/integrates/xls/',
             '/integrates/pdf/',
+            '/integrates/project/BWAPP/12345/evidence/file1.png',
             '/integrates/logout'
-        )
+        }
 
         for url in should_allow:
             self.assertTrue(enfor.enforce(sub, url))
+
+        should_deny = GLOBAL_URL_LIST - should_allow
+
+        for url in should_deny:
+            self.assertFalse(enfor.enforce(sub, url))
 
     def test_enforcer_analyst(self):
         """Tests for an analyst user."""
@@ -95,24 +98,23 @@ class AbacTest(TestCase):
         sub.username = 'someone@fluidattacks.com'
         sub.role = 'analyst'
 
-        should_deny = (
-            '/integrates/export_all_vulnerabilities',
-        )
-
-        for url in should_deny:
-            self.assertFalse(enfor.enforce(sub, url))
-
-        should_allow = (
+        should_allow = {
             '/integrates/12312313/download_vulnerabilities',
             '/integrates/registration',
             '/integrates/dashboard',
+            '/integrates/project/BWAPP/12345/evidence/file1.png',
             '/integrates/xls/',
             '/integrates/pdf/',
             '/integrates/logout'
-        )
+        }
 
         for url in should_allow:
             self.assertTrue(enfor.enforce(sub, url))
+
+        should_deny = GLOBAL_URL_LIST - should_allow
+
+        for url in should_deny:
+            self.assertFalse(enfor.enforce(sub, url))
 
     def test_enforcer_admin(self):
         """Tests for an admin user."""
@@ -125,15 +127,7 @@ class AbacTest(TestCase):
         sub.username = 'admin@fluidattacks.com'
         sub.role = 'admin'
 
-        should_allow = (
-            '/integrates/export_all_vulnerabilities',
-            '/integrates/12312313/download_vulnerabilities',
-            '/integrates/registration',
-            '/integrates/dashboard',
-            '/integrates/xls/',
-            '/integrates/pdf/',
-            '/integrates/logout'
-        )
+        should_allow = GLOBAL_URL_LIST
 
         for url in should_allow:
             self.assertTrue(enfor.enforce(sub, url))
