ad68aaad34f81cd0f0b2c458d35b2a465f010ec5
Mario Varas <mvaras@fluidattacks.com>
2020-02-04 13:37:34 -0500

    refac(back): #1723 refac alert domain
    - added alert dal

---
+++ django-apps/integrates-back/backend/dal/alert.py
@@ -0,0 +1,106 @@
+"""DAL functions for alerts."""
+
+import rollbar
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+
+from backend.dal import integrates_dal
+from backend.dal.helpers import dynamodb
+
+DYNAMODB_RESOURCE = dynamodb.DYNAMODB_RESOURCE
+TABLE = DYNAMODB_RESOURCE.Table('FI_alerts_by_company')
+
+
+def get(company_name, project_name):
+    """ Get alerts of a company. """
+    company_name = company_name.lower()
+    project_name = project_name.lower()
+    filter_key = 'company_name'
+    filter_sort = 'project_name'
+    if project_name == 'all':
+        filtering_exp = Key(filter_key).eq(company_name)
+        response = TABLE.query(
+            KeyConditionExpression=filtering_exp)
+    else:
+        filtering_exp = Key(filter_key).eq(company_name) & \
+            Key(filter_sort).eq(project_name)
+        response = TABLE.query(KeyConditionExpression=filtering_exp)
+    items = response['Items']
+    while response.get('LastEvaluatedKey'):
+        response = TABLE.query(
+            KeyConditionExpression=filtering_exp,
+            ExclusiveStartKey=response['LastEvaluatedKey'])
+        items += response['Items']
+    return items
+
+
+def change_status(message, company_name, project_name):
+    """ Activate or deactivate the alert of a company. """
+    message = message.lower()
+    company_name = company_name.lower()
+    project_name = project_name.lower()
+    if ((project_name == 'all' and message == 'deactivate') or
+            (project_name != 'all' and message == 'deactivate')):
+        status = '0'
+    else:
+        status = '1'
+    item = get(company_name, project_name)
+    for _item in item:
+        payload = {'company_name': _item['company_name'],
+                   'project_name': _item['project_name'], }
+        try:
+            TABLE.update_item(
+                Key=payload,
+                UpdateExpression='SET status_act = :val1',
+                ExpressionAttributeValues={
+                    ':val1': status,
+                }
+            )
+        except ClientError:
+            rollbar.report_exc_info()
+            return False
+    return True
+
+
+def put(message, company_name, project_name):
+    """ Create, update or activate an alert for a company. """
+    project_name = project_name.lower()
+    company_name = company_name.lower()
+    primary_key = {'project_name': project_name}
+    project_exists = integrates_dal.attribute_exists(
+        'FI_projects', primary_key, ['project_name'])
+    resp = False
+    if project_name == 'all' or project_exists:
+        item = get(company_name, project_name)
+        if item == []:
+            try:
+                response = TABLE.put_item(
+                    Item={
+                        'company_name': company_name,
+                        'project_name': project_name,
+                        'message': message,
+                        'status_act': '1',
+                    }
+                )
+                resp = response['ResponseMetadata']['HTTPStatusCode'] == 200
+            except ClientError:
+                rollbar.report_exc_info()
+        else:
+            for _item in item:
+                try:
+                    response = TABLE.update_item(
+                        Key={
+                            'company_name': _item['company_name'],
+                            'project_name': _item['project_name'],
+                        },
+                        UpdateExpression='SET message = :val1,\
+                            status_act = :val2',
+                        ExpressionAttributeValues={
+                            ':val1': str(message),
+                            ':val2': '1',
+                        }
+                    )
+                except ClientError:
+                    rollbar.report_exc_info()
+            resp = response['ResponseMetadata']['HTTPStatusCode'] == 200
+    return resp
--- django-apps/integrates-back/backend/domain/alert.py
+++ django-apps/integrates-back/backend/domain/alert.py
@@ -1,15 +1,13 @@
-from backend.dal import integrates_dal
+from backend.dal import alert as alert_dal
 
 
 def get_company_alert(company, project_name):
-    return integrates_dal.get_company_alert_dynamo(company, project_name)
+    return alert_dal.get(company, project_name)
 
 
 def set_company_alert(company, message, project_name):
     if message in ('ACTIVATE', 'DEACTIVATE'):
-        resp = integrates_dal.change_status_comalert_dynamo(
-            message, company, project_name)
+        resp = alert_dal.change_status(message, company, project_name)
     else:
-        resp = integrates_dal.set_company_alert_dynamo(
-            message, company, project_name)
+        resp = alert_dal.put(message, company, project_name)
     return resp
