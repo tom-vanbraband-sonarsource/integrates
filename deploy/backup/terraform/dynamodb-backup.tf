d206c81404463346ff429bd45c7cbc9c4c92b1a9
Mateo Norena <mnorena@fluidattacks.com>
2020-02-10 20:45:35 -0500

    fix(infra): #1631 use variable

--- deploy/backup/terraform/dynamodb-backup.tf
+++ deploy/backup/terraform/dynamodb-backup.tf
@@ -1,14 +1,14 @@
+resource "aws_backup_vault" "backup_vault" {
+  name        = "integrates-backup-vault"
+  kms_key_arn = "arn:aws:kms:us-east-1:205810638802:key/d33073aa-19f8-4390-afa1-abcda2be27d7"
+}
+
 resource "aws_backup_plan" "integrates_backup_plan" {
   name = "integrates-backup-plan"
 
   rule {
     rule_name         = "integrates-backup-rule"
-    target_vault_name = "aws_backup_vault.backup_vault.name"
+    target_vault_name = aws_backup_vault.backup_vault.name
     schedule          = "cron(0 12 * * ? *)"
   }
-}
-
-resource "aws_backup_vault" "backup_vault" {
-  name        = "integrates-backup-vault"
-  kms_key_arn = "arn:aws:kms:us-east-1:205810638802:key/d33073aa-19f8-4390-afa1-abcda2be27d7"
 }
