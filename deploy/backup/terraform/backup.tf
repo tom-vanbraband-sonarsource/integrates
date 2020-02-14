711568ef19bb3633cbf1d39b72ddf8d102ac6f4a
Mateo Norena <mnorena@fluidattacks.com>
2020-02-13 20:04:54 -0500

    feat(infra): #1631 create daily backup plan
    - reschedule to execute at midnight in -5 tz
    - edit name to daily backup plan
    - set deletion after seven days

--- deploy/backup/terraform/backup.tf
+++ deploy/backup/terraform/backup.tf
@@ -3,12 +3,15 @@ resource "aws_backup_vault" "backup_vault" {
   kms_key_arn = "arn:aws:kms:us-east-1:205810638802:key/d33073aa-19f8-4390-afa1-abcda2be27d7"
 }
 
-resource "aws_backup_plan" "integrates_backup_plan" {
-  name = "integrates-backup-plan"
+resource "aws_backup_plan" "integrates_daily_backup_plan" {
+  name = "integrates-daily-backup-plan"
 
   rule {
-    rule_name         = "integrates-backup-rule"
+    rule_name         = "integrates-backup-daily-rule"
     target_vault_name = aws_backup_vault.backup_vault.name
-    schedule          = "cron(0 0 * * ? *)"
+    schedule          = "cron(0 5 * * ? *)"
+    lifecycle {
+      delete_after = 7
+    }
   }
 }
--- deploy/backup/terraform/dynamodb.tf
+++ deploy/backup/terraform/dynamodb.tf
@@ -1,7 +1,7 @@
-resource "aws_backup_selection" "dynamodb_backup_selection" {
+resource "aws_backup_selection" "dynamodb_daily_backup_selection" {
   iam_role_arn = aws_iam_role.integrates-backup-role.arn
-  name         = "integrates_dynamodb_backup_selection"
-  plan_id      = aws_backup_plan.integrates_backup_plan.id
+  name         = "integrates_dynamodb_daily_backup_selection"
+  plan_id      = aws_backup_plan.integrates_daily_backup_plan.id
 
   resources    = [
     for table in var.dynamodb-tables : "arn:aws:dynamodb:us-east-1:205810638802:table/${table}"
