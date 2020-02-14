fb8c5b0b25596702c489e4d76d1af03382e8d7b7
David Acevedo <dacevedo@fluidattacks.com>
2020-02-14 15:04:19 -0500

    refac(front): #1726.9 modernize event evidence
    - use apollo hooks for better readability
    - with nested mutations

--- front/src/scenes/Dashboard/containers/EventEvidenceView/index.tsx
+++ front/src/scenes/Dashboard/containers/EventEvidenceView/index.tsx
@@ -3,8 +3,7 @@
  * Disabling this rule is necessary for accessing render props from
  * apollo components
  */
-import { MutationFunction, QueryResult } from "@apollo/react-common";
-import { Mutation, Query } from "@apollo/react-components";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import { ApolloError } from "apollo-client";
 import { GraphQLError } from "graphql";
 import _ from "lodash";
@@ -28,25 +27,30 @@ type EventEvidenceProps = RouteComponentProps<{ eventId: string }>;
 
 const eventEvidenceView: React.FC<EventEvidenceProps> = (props: EventEvidenceProps): JSX.Element => {
   const { eventId } = props.match.params;
+  const { userRole } = window as typeof window & Dictionary<string>;
+  const baseUrl: string = window.location.href.replace("dashboard#!/", "");
 
+  // State management
   const [isEditing, setEditing] = React.useState(false);
   const handleEditClick: (() => void) = (): void => { setEditing(!isEditing); };
 
   const [lightboxIndex, setLightboxIndex] = React.useState(-1);
+  const openImage: (() => void) = (): void => {
+    if (!isEditing) { setLightboxIndex(0); }
+  };
 
-  const baseUrl: string = window.location.href.replace("dashboard#!/", "");
-
-  return (
-    <React.StrictMode>
-      <Query query={GET_EVENT_EVIDENCES} variables={{ eventId }}>
-        {({ data, refetch }: QueryResult): JSX.Element => {
-          if (_.isUndefined(data) || _.isEmpty(data)) { return <React.Fragment />; }
-
-          const handleUpdateResult: (() => void) = (): void => {
-            refetch()
-              .catch();
-          };
-          const handleUpdateError: ((updateError: ApolloError) => void) = (updateError: ApolloError): void => {
+  // GraphQL operations
+  const { data, refetch } = useQuery(GET_EVENT_EVIDENCES, { variables: { eventId } });
+  const [downloadEvidence] = useMutation(DOWNLOAD_FILE_MUTATION, {
+    onCompleted: (downloadData: { downloadEventFile: { url: string } }): void => {
+      const newTab: Window | null = window.open(downloadData.downloadEventFile.url);
+      (newTab as Window).opener = undefined;
+    },
+  });
+  const [removeEvidence] = useMutation(REMOVE_EVIDENCE_MUTATION, { onCompleted: refetch });
+  const [updateEvidence] = useMutation(UPDATE_EVIDENCE_MUTATION, {
+    onCompleted: refetch,
+    onError: (updateError: ApolloError): void => {
             updateError.graphQLErrors.forEach(({ message }: GraphQLError): void => {
               switch (message) {
                 case "Exception - Invalid File Size":
@@ -63,13 +67,52 @@ const eventEvidenceView: React.FC<EventEvidenceProps> = (props: EventEvidencePro
                   rollbar.error("An error occurred updating event evidence", updateError);
               }
             });
-          };
+    },
+  });
 
-          const { userRole } = (window as typeof window & { userRole: string });
-          const canEdit: boolean = _.includes(["admin", "analyst"], userRole)
-            && data.event.eventStatus !== "CLOSED";
+  if (_.isUndefined(data) || _.isEmpty(data)) { return <React.Fragment />; }
 
-          return (
+  const handleDownload: (() => void) = (): void => {
+    if (!isEditing) {
+      downloadEvidence({ variables: { eventId, fileName: data.event.evidenceFile } })
+        .catch();
+    }
+  };
+
+  const removeImage: (() => void) = (): void => {
+    setEditing(false);
+    removeEvidence({ variables: { eventId, evidenceType: "IMAGE" } })
+      .catch();
+  };
+
+  const removeFile: (() => void) = (): void => {
+    setEditing(false);
+    removeEvidence({ variables: { eventId, evidenceType: "FILE" } })
+      .catch();
+  };
+
+  const updateImage: ((values: { filename: FileList }) => void) = (values: { filename: FileList }): void => {
+    setEditing(false);
+
+    updateEvidence({
+      variables: { eventId, evidenceType: "IMAGE", file: values.filename[0] },
+    })
+      .catch();
+  };
+
+  const updateFile: ((values: { filename: FileList }) => void) = (values: { filename: FileList }): void => {
+    setEditing(false);
+
+    updateEvidence({
+      variables: { eventId, evidenceType: "FILE", file: values.filename[0] },
+    })
+      .catch();
+  };
+
+  const canEdit: boolean = _.includes(["admin", "analyst"], userRole) && data.event.eventStatus !== "CLOSED";
+
+  return (
+    <React.StrictMode>
             <React.Fragment>
               <Row>
                 <Col md={2} mdOffset={10} xs={12} sm={12}>
@@ -88,37 +131,7 @@ const eventEvidenceView: React.FC<EventEvidenceProps> = (props: EventEvidencePro
                   <p>{translate.t("project.events.evidence.no_data")}</p>
                 </div>
               ) : undefined}
-              {!_.isEmpty(data.event.evidence) || isEditing
-                ? <Mutation
-                  mutation={UPDATE_EVIDENCE_MUTATION}
-                  onCompleted={handleUpdateResult}
-                  onError={handleUpdateError}
-                >
-                  {(updateEvidence: MutationFunction): JSX.Element => {
-                    const handleUpdate: ((values: { filename: FileList }) => void) = (
-                      values: { filename: FileList },
-                    ): void => {
-                      updateEvidence({
-                        variables: { eventId, evidenceType: "IMAGE", file: values.filename[0] },
-                      })
-                        .catch();
-                      setEditing(false);
-                    };
-
-                    return (
-                      <Mutation mutation={REMOVE_EVIDENCE_MUTATION} onCompleted={handleUpdateResult}>
-                        {(removeImage: MutationFunction): JSX.Element => {
-                          const handleRemove: (() => void) = (): void => {
-                            removeImage({ variables: { eventId, evidenceType: "IMAGE" } })
-                              .catch();
-                            setEditing(false);
-                          };
-
-                          const openImage: (() => void) = (): void => {
-                            if (!isEditing) { setLightboxIndex(0); }
-                          };
-
-                          return (
+              {!_.isEmpty(data.event.evidence) || isEditing ? (
                             <EvidenceImage
                               acceptedMimes="image/jpeg,image/gif,image/png"
                               content={_.isEmpty(data.event.evidence) ? <div /> : `${baseUrl}/${data.event.evidence}`}
@@ -126,63 +139,14 @@ const eventEvidenceView: React.FC<EventEvidenceProps> = (props: EventEvidencePro
                               isDescriptionEditable={false}
                               isEditing={isEditing}
                               isRemovable={!_.isEmpty(data.event.evidence)}
-                              name="evidence"
+                              name="image"
                               onClick={openImage}
-                              onDelete={handleRemove}
-                              onUpdate={handleUpdate}
+                              onDelete={removeImage}
+                              onUpdate={updateImage}
                               validate={validEvidenceImage}
                             />
-                          );
-                        }}
-                      </Mutation>
-                    );
-                  }}
-                </Mutation>
-                : undefined}
-              {!_.isEmpty(data.event.evidenceFile) || isEditing
-                ? <Mutation
-                  mutation={UPDATE_EVIDENCE_MUTATION}
-                  onCompleted={handleUpdateResult}
-                  onError={handleUpdateError}
-                >
-                  {(updateEvidence: MutationFunction): JSX.Element => {
-                    const handleUpdate: ((values: { filename: FileList }) => void) = (
-                      values: { filename: FileList },
-                    ): void => {
-                      updateEvidence({
-                        variables: { eventId, evidenceType: "FILE", file: values.filename[0] },
-                      })
-                        .catch();
-                      setEditing(false);
-                    };
-
-                    const handleDownload: ((downloadData: { downloadEventFile: { url: string } }) => void) = (
-                      downloadData: { downloadEventFile: { url: string } },
-                    ): void => {
-                      const newTab: Window | null = window.open(downloadData.downloadEventFile.url);
-                      (newTab as Window).opener = undefined;
-                    };
-
-                    return (
-                      <Mutation mutation={DOWNLOAD_FILE_MUTATION} onCompleted={handleDownload}>
-                        {(downloadEvidence: MutationFunction): JSX.Element => {
-                          const handleClick: (() => void) = (): void => {
-                            if (!isEditing) {
-                              downloadEvidence({ variables: { eventId, fileName: data.event.evidenceFile } })
-                                .catch();
-                            }
-                          };
-
-                          return (
-                            <Mutation mutation={REMOVE_EVIDENCE_MUTATION} onCompleted={handleUpdateResult}>
-                              {(removeFile: MutationFunction): JSX.Element => {
-                                const handleRemove: (() => void) = (): void => {
-                                  removeFile({ variables: { eventId, evidenceType: "FILE" } })
-                                    .catch();
-                                  setEditing(false);
-                                };
-
-                                return (
+              ) : undefined}
+              {!_.isEmpty(data.event.evidenceFile) || isEditing ? (
                                   <EvidenceImage
                                     acceptedMimes="application/pdf,application/zip,text/csv,text/plain"
                                     content={<Glyphicon glyph="file" />}
@@ -191,30 +155,18 @@ const eventEvidenceView: React.FC<EventEvidenceProps> = (props: EventEvidencePro
                                     isEditing={isEditing}
                                     isRemovable={!_.isEmpty(data.event.evidenceFile)}
                                     name="file"
-                                    onClick={handleClick}
-                                    onDelete={handleRemove}
-                                    onUpdate={handleUpdate}
+                                    onClick={handleDownload}
+                                    onDelete={removeFile}
+                                    onUpdate={updateFile}
                                     validate={validEventFile}
                                   />
-                                );
-                              }}
-                            </Mutation>
-                          );
-                        }}
-                      </Mutation>
-                    );
-                  }}
-                </Mutation>
-                : undefined}
+              ) : undefined}
               <EvidenceLightbox
                 evidenceImages={[{ url: data.event.evidence }]}
                 index={lightboxIndex}
                 onChange={setLightboxIndex}
               />
             </React.Fragment>
-          );
-        }}
-      </Query>
     </React.StrictMode>
   );
 };
