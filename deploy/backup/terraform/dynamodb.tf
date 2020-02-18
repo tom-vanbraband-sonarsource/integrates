7b9083880162e16038a341f230ad2b6124683e47
Mateo Norena <mnorena@fluidattacks.com>
2020-02-18 09:53:58 -0500

    feat(infra): #1631 organize backup rules
    - fix yearly cron backup plan
       - execute on the first sunday of every year
       - execute at 1:30am (GTM-5)
       - set deletion after fifteen years (5475 days)
    - fix weekly cron day-of-week value
    - create only a backup plan with multiple rules
    - delete backup terraform file since is not needed

--- deploy/backup/terraform/dynamodb.tf
+++ deploy/backup/terraform/dynamodb.tf
@@ -1,27 +1,48 @@
-resource "aws_backup_selection" "dynamodb_daily_backup_selection" {
-  iam_role_arn = aws_iam_role.integrates-backup-role.arn
-  name         = "integrates_dynamodb_daily_backup_selection"
-  plan_id      = aws_backup_plan.integrates_daily_backup_plan.id
+resource "aws_backup_plan" "integrates_backup_plan" {
+  name = "integrates-backup-plan"
 
-  resources    = [
-    for table in var.dynamodb-tables : "arn:aws:dynamodb:us-east-1:205810638802:table/${table}"
-  ]
+  rule {
+    rule_name         = "integrates-backup-daily-rule"
+    target_vault_name = aws_backup_vault.backup_vault.name
+    schedule          = "cron(0 5 * * ? *)"
+    lifecycle {
+      delete_after = 7
+    }
+  }
+
+  rule {
+    rule_name         = "integrates-backup-weekly-rule"
+    target_vault_name = aws_backup_vault.backup_vault.name
+    schedule          = "cron(30 5 ? * SUN *)"
+    lifecycle {
+      delete_after = 84
+    }
+  }
+
+  rule {
+    rule_name         = "integrates-backup-monthly-rule"
+    target_vault_name = aws_backup_vault.backup_vault.name
+    schedule          = "cron(0 6 ? * 1#1 *)"
+    lifecycle {
+      delete_after = 1095
+    }
+  }
+
+  rule {
+    rule_name         = "integrates-backup-yearly-rule"
+    target_vault_name = aws_backup_vault.backup_vault.name
+    schedule          = "cron(30 6 ? JAN 1#1 *)"
+    lifecycle {
+      delete_after = 5475
+    }
+  }
+
 }
 
-resource "aws_backup_selection" "dynamodb_weekly_backup_selection" {
+resource "aws_backup_selection" "dynamodb_backup_selection" {
   iam_role_arn = aws_iam_role.integrates-backup-role.arn
-  name         = "integrates_dynamodb_weekly_backup_selection"
-  plan_id      = aws_backup_plan.integrates_weekly_backup_plan.id
-
-  resources    = [
-    for table in var.dynamodb-tables : "arn:aws:dynamodb:us-east-1:205810638802:table/${table}"
-  ]
-}
-
-resource "aws_backup_selection" "dynamodb_monthly_backup_selection" {
-  iam_role_arn = aws_iam_role.integrates-backup-role.arn
-  name         = "integrates_dynamodb_monthly_backup_selection"
-  plan_id      = aws_backup_plan.integrates_monthly_backup_plan.id
+  name         = "integrates_dynamodb_backup_selection"
+  plan_id      = aws_backup_plan.integrates_backup_plan.id
 
   resources    = [
     for table in var.dynamodb-tables : "arn:aws:dynamodb:us-east-1:205810638802:table/${table}"
