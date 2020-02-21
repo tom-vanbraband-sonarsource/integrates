e3829d8f71ab14c7d5898c60ec303521207ed081
David Acevedo <dacevedo@fluidattacks.com>
2020-02-21 15:57:58 -0500

    test(front): #1752.10 increase coverage
    - fully cover submit mutation

--- front/src/scenes/Dashboard/containers/FindingContent/index.spec.tsx
+++ front/src/scenes/Dashboard/containers/FindingContent/index.spec.tsx
@@ -1,6 +1,7 @@
 import { MockedProvider, MockedResponse } from "@apollo/react-testing";
 import { configure, mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 import ReactSixteenAdapter from "enzyme-adapter-react-16";
+import { GraphQLError } from "graphql";
 // tslint:disable-next-line: no-import-side-effect
 import "isomorphic-fetch";
 import * as React from "react";
@@ -10,12 +11,20 @@ import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import wait from "waait";
 import store from "../../../../store";
+import { msgError } from "../../../../utils/notifications";
 import { GET_PROJECT_ALERT } from "../ProjectContent/queries";
 import { FindingContent } from "./index";
-import { GET_FINDING_HEADER } from "./queries";
+import { GET_FINDING_HEADER, SUBMIT_DRAFT_MUTATION } from "./queries";
 import { IFindingContentProps } from "./types";
 
 configure({ adapter: new ReactSixteenAdapter() });
+
+jest.mock("../../../../utils/notifications", () => {
+  const mockedNotifications: Dictionary = jest.requireActual("../../../../utils/notifications");
+  mockedNotifications.msgError = jest.fn();
+
+  return mockedNotifications;
+});
 
 describe("FindingContent", () => {
 
@@ -129,6 +138,22 @@ describe("FindingContent", () => {
     },
   };
 
+  const submitMutationMock: Readonly<MockedResponse> = {
+    request: {
+      query: SUBMIT_DRAFT_MUTATION,
+      variables: {
+        findingId: "438679960",
+      },
+    },
+    result: {
+      data: {
+        submitDraft: {
+          success: true,
+        },
+      },
+    },
+  };
+
   it("should return a object", () => {
     expect(typeof (FindingContent))
       .toEqual("function");
@@ -215,6 +240,96 @@ describe("FindingContent", () => {
       .at(0)
       .prop("show"))
       .toEqual(false);
+  });
+
+  it("should submit draft", async () => {
+    type resultType = Dictionary<{ finding: { historicState: Dictionary[] } }>;
+    const result: resultType = draftMock.result as resultType;
+    const submittedDraftMock: Readonly<MockedResponse> = {
+      ...draftMock,
+      result: {
+        ...draftMock.result,
+        data: {
+          ...result.data,
+          finding: {
+            ...result.data.finding,
+            historicState: [
+              ...result.data.finding.historicState,
+              {
+                analyst: "someone@fluidattacks.com",
+                date: "2019-10-31 11:30:00",
+                state: "SUBMITTED",
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    (window as typeof window & { userRole: string }).userRole = "analyst";
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/project/TEST/findings/438679960/description"]}>
+        <Provider store={store}>
+          <MockedProvider mocks={[draftMock, submitMutationMock, submittedDraftMock]} addTypename={false}>
+            <FindingContent {...mockProps} />
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>,
+    );
+    await act(async () => { await wait(0); wrapper.update(); });
+    let submitButton: ReactWrapper = wrapper.find("ButtonToolbar")
+      .at(0)
+      .find("Button")
+      .filterWhere((element: ReactWrapper) => element.text()
+        .includes("Submit"));
+    submitButton.simulate("click");
+    await act(async () => { await wait(0); });
+    submitButton = wrapper.find("ButtonToolbar")
+      .at(0)
+      .find("Button")
+      .filterWhere((element: ReactWrapper) => element.text()
+        .includes("Submit"));
+    expect(submitButton.prop("disabled"))
+      .toEqual(true);
+  });
+
+  it("should handle submit errors", async () => {
+    const submitErrorMock: Readonly<MockedResponse> = {
+      request: {
+        query: SUBMIT_DRAFT_MUTATION,
+        variables: {
+          findingId: "438679960",
+        },
+      },
+      result: {
+        errors: [
+          new GraphQLError("Exception - This draft has missing fields"),
+          new GraphQLError("Exception - This draft has already been submitted"),
+          new GraphQLError("Exception - This draft has already been approved"),
+          new GraphQLError("Unexpected error"),
+        ],
+      },
+    };
+    (window as typeof window & { userRole: string }).userRole = "analyst";
+    const wrapper: ReactWrapper = mount(
+      <MemoryRouter initialEntries={["/project/TEST/findings/438679960/description"]}>
+        <Provider store={store}>
+          <MockedProvider mocks={[draftMock, submitErrorMock, findingMock]} addTypename={false}>
+            <FindingContent {...mockProps} />
+          </MockedProvider>
+        </Provider>
+      </MemoryRouter>,
+    );
+    await act(async () => { await wait(0); wrapper.update(); });
+    const submitButton: ReactWrapper = wrapper.find("ButtonToolbar")
+      .at(0)
+      .find("Button")
+      .filterWhere((element: ReactWrapper) => element.text()
+        .includes("Submit"));
+    submitButton.simulate("click");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(msgError)
+      .toHaveBeenCalled();
   });
 
   it("should render alert", async () => {
