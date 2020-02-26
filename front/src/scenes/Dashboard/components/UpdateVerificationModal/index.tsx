af6a4a491c44f8c1aa01cc020403d80b871e6d37
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-02-26 11:04:40 -0500

    refac(front): #1637.3 add own css file

--- front/src/scenes/Dashboard/components/UpdateVerificationModal/index.tsx
+++ front/src/scenes/Dashboard/components/UpdateVerificationModal/index.tsx
@@ -12,7 +12,7 @@ import { msgError, msgSuccess } from "../../../../utils/notifications";
 import rollbar from "../../../../utils/rollbar";
 import translate from "../../../../utils/translations/translate";
 import { remediationModal as RemediationModal } from "../RemediationModal/index";
-import { default as style } from "../Vulnerabilities/index.css";
+import { default as style } from "./index.css";
 import { REQUEST_VERIFICATION_VULN, VERIFY_VULNERABILITIES } from "./queries";
 import { IRequestVerificationVulnResult, IVerifyRequestVulnResult } from "./types";
 
