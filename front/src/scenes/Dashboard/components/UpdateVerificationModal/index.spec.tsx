80e79151ec287ce02c46be832bd221f3397bd91b
Sebastian Lizcano <slizcano@fluidattacks.com>
2020-02-26 11:07:26 -0500

    test(front): #1637.4 add verify_request test

--- front/src/scenes/Dashboard/components/UpdateVerificationModal/index.spec.tsx
+++ front/src/scenes/Dashboard/components/UpdateVerificationModal/index.spec.tsx
@@ -11,7 +11,7 @@ import { Provider } from "react-redux";
 import wait from "waait";
 import store from "../../../../store";
 import { UpdateVerificationModal } from "./index";
-import { REQUEST_VERIFICATION_VULN } from "./queries";
+import { REQUEST_VERIFICATION_VULN, VERIFY_VULNERABILITIES } from "./queries";
 
 configure({ adapter: new ReactSixteenAdapter() });
 
@@ -92,6 +92,97 @@ describe("update verification component", () => {
     const justification: ReactWrapper = wrapper.find("textarea");
     justification.simulate("change", { target:
       { value: "This is a commenting test of a request verification in vulns" } });
+    const form: ReactWrapper = wrapper.find("form");
+    form.simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(wrapper)
+      .toHaveLength(1);
+    expect(handleOnClose)
+      .toHaveBeenCalled();
+  });
+
+  it("should handle verify a request", async () => {
+    const handleOnClose: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [{
+      request: {
+        query: VERIFY_VULNERABILITIES,
+        variables: {
+          closedVulns: ["test"],
+          findingId: "",
+          justification: "This is a commenting test of a verifying request verification in vulns",
+          openVulns: [],
+        },
+      },
+      result: { data: { verifyRequestVuln : { success: true } } },
+    }];
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <UpdateVerificationModal
+            findingId={""}
+            isOpen={true}
+            remediationType={"verify"}
+            vulns={[{currentState: "open", id: "test", where: ""}]}
+            clearSelected={jest.fn()}
+            handleCloseModal={handleOnClose}
+          />
+        </MockedProvider>
+      </Provider>,
+    );
+    const justification: ReactWrapper = wrapper.find("textarea");
+    justification.simulate("change", { target:
+      { value: "This is a commenting test of a verifying request verification in vulns" } });
+    const switchButton: ReactWrapper = wrapper.find("BootstrapTable")
+      .find("n")
+      .find("div")
+      .first();
+    switchButton.simulate("click");
+    const form: ReactWrapper = wrapper.find("form");
+    form.simulate("submit");
+    await act(async () => { await wait(0); wrapper.update(); });
+    expect(wrapper)
+      .toHaveLength(1);
+    expect(handleOnClose)
+      .toHaveBeenCalled();
+  });
+
+  it("should handle verify a request error", async () => {
+    const handleOnClose: jest.Mock = jest.fn();
+    const mocksMutation: MockedResponse[] = [{
+      request: {
+        query: VERIFY_VULNERABILITIES,
+        variables: {
+          closedVulns: [],
+          findingId: "",
+          justification: "This is a commenting test of a verifying request verification in vulns",
+          openVulns: ["test_error"],
+        },
+      },
+      result: {
+        errors: [
+          new GraphQLError("Exception - Error verification not requested"),
+          new GraphQLError("Exception - Vulnerability not found"),
+          new GraphQLError("Unexpected error"),
+        ],
+      },
+    }];
+    const wrapper: ReactWrapper = mount(
+      <Provider store={store}>
+        <MockedProvider mocks={mocksMutation} addTypename={false}>
+          <UpdateVerificationModal
+            findingId={""}
+            isOpen={true}
+            remediationType={"verify"}
+            vulns={[{currentState: "open", id: "test_error", where: ""}]}
+            clearSelected={jest.fn()}
+            handleCloseModal={handleOnClose}
+          />
+        </MockedProvider>
+      </Provider>,
+    );
+    const justification: ReactWrapper = wrapper.find("textarea");
+    justification.simulate("change", { target:
+      { value: "This is a commenting test of a verifying request verification in vulns" } });
     const form: ReactWrapper = wrapper.find("form");
     form.simulate("submit");
     await act(async () => { await wait(0); wrapper.update(); });
