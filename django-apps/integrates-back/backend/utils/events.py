f7a6d2a859f553c908f82c4be8dff904bee48ad1
Mario Varas <mvaras@fluidattacks.com>
2020-02-27 07:50:56 -0500

    refac(back): #765 utils/event typing

--- django-apps/integrates-back/backend/utils/events.py
+++ django-apps/integrates-back/backend/utils/events.py
@@ -1,4 +1,7 @@
-def format_data(event):
+from typing import Any, Dict
+
+
+def format_data(event: Dict[str, Any]) -> Dict[str, Any]:
     historic_state = event.get('historic_state', [{}, {}])
     event['closing_date'] = '-'
     if historic_state[-1].get('state') == 'SOLVED':
