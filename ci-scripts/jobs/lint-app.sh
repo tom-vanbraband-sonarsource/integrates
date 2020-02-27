ec32587b11a039be11d4499b720f724fb5bd8ef7
Mario Varas <mvaras@fluidattacks.com>
2020-02-27 11:28:15 -0500

    refac(build): #765 mypy paths in var

--- ci-scripts/jobs/lint-app.sh
+++ ci-scripts/jobs/lint-app.sh
@@ -4,22 +4,22 @@
 
 # Linters
 run_lint () {
+    MYPY_PATHS="django-apps/integrates-back/backend/mailer.py \
+            django-apps/integrates-back/backend/scheduler.py \
+            django-apps/integrates-back/backend/services.py \
+            django-apps/integrates-back/backend/util.py \
+            django-apps/integrates-back/backend/exceptions.py \
+            django-apps/integrates-back/backend/decorators.py \
+            django-apps/integrates-back/backend/entity/resource.py \
+            django-apps/integrates-back/backend/entity/user.py \
+            django-apps/integrates-back/backend/entity/vulnerability.py \
+            django-apps/integrates-back/backend/utils/*"
     RETVAL=0
     prospector -F -s high -u django -i node_modules app/ || RETVAL=1
     prospector -F -s high -u django -i node_modules django-apps/ || RETVAL=1
     prospector -F -s veryhigh -u django -i node_modules fluidintegrates/ || RETVAL=1
     prospector -F -s veryhigh lambda/ || RETVAL=1
-    mypy --ignore-missing-imports \
-        django-apps/integrates-back/backend/mailer.py \
-        django-apps/integrates-back/backend/scheduler.py \
-        django-apps/integrates-back/backend/services.py \
-        django-apps/integrates-back/backend/util.py \
-        django-apps/integrates-back/backend/exceptions.py \
-        django-apps/integrates-back/backend/decorators.py \
-        django-apps/integrates-back/backend/entity/resource.py \
-        django-apps/integrates-back/backend/entity/user.py \
-        django-apps/integrates-back/backend/entity/vulnerability.py \
-        django-apps/integrates-back/backend/utils/* || RETVAL=1
+    mypy --ignore-missing-imports $MYPY_PATHS || RETVAL=1
     return $RETVAL
 }
 
