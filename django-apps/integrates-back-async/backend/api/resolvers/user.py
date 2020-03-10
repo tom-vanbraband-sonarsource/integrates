938a89aac45691cf7d7e87a06c8a2658331eb3b9
Andres Roldan <aroldan@fluidattacks.com>
2020-03-10 10:45:27 -0500

    feat(back): #1635 migrated grantuseraccess
    - And added tests

--- django-apps/integrates-back-async/backend/api/resolvers/user.py
+++ django-apps/integrates-back-async/backend/api/resolvers/user.py
@@ -2,19 +2,89 @@
 # pylint: disable=too-many-locals
 
 from datetime import datetime
-from typing import List as _List
+from typing import Union, Dict, List as _List
 import threading
 
 from backend.domain import project as project_domain, user as user_domain
 from backend.exceptions import UserNotFound
 from backend.mailer import send_mail_access_granted
 from backend.services import (
-    has_responsibility, has_phone_number, is_customeradmin,
+    get_user_role, has_responsibility, has_phone_number, is_customeradmin,
     has_access_to_project
 )
+from backend.utils.user import (
+    validate_email_address, validate_field, validate_phone_field
+)
+
 from backend import util
 
+import rollbar
+
 from ariadne import convert_kwargs_to_snake_case
+
+
+def _create_new_user(
+        context: Dict[str, Union[int, _List[str]]],
+        new_user_data: Dict[str, str],
+        project_name: str) -> bool:
+    analizable_list = list(new_user_data.values())[1:-1]
+    if (
+        all(validate_field(field) for field in analizable_list) and
+        validate_phone_field(new_user_data['phone_number']) and
+        validate_email_address(new_user_data['email'])
+    ):
+        email = new_user_data['email']
+        organization = new_user_data['organization']
+        responsibility = new_user_data['responsibility']
+        role = new_user_data['role']
+        phone_number = new_user_data['phone_number']
+    else:
+        return False
+
+    success = False
+
+    if not user_domain.get_data(email, 'email'):
+        user_domain.create(
+            email.lower(), {'company': organization.lower(),
+                            'phone': phone_number})
+    if not user_domain.is_registered(email):
+        user_domain.register(email)
+        user_domain.assign_role(email, role)
+        user_domain.update(email, organization.lower(), 'company')
+    elif user_domain.is_registered(email):
+        user_domain.assign_role(email, role)
+    if project_name and responsibility and len(responsibility) <= 50:
+        project_domain.add_access(email, project_name, 'responsibility', responsibility)
+    else:
+        util.cloudwatch_log(
+            context,
+            'Security: {email} Attempted to add responsibility to project \
+                {project} without validation'.format(email=email,
+                                                     project=project_name)
+        )
+    if phone_number and phone_number[1:].isdigit():
+        user_domain.add_phone_to_user(email, phone_number)
+    if project_name and role == 'customeradmin':
+        project_domain.add_user(project_name.lower(), email.lower(), role)
+    if project_name and user_domain.update_project_access(email, project_name, True):
+        description = project_domain.get_description(project_name.lower())
+        project_url = \
+            'https://fluidattacks.com/integrates/dashboard#!/project/' \
+            + project_name.lower() + '/indicators'
+        mail_to = [email]
+        context = {
+            'admin': email,
+            'project': project_name,
+            'project_description': description,
+            'project_url': project_url,
+        }
+        email_send_thread = \
+            threading.Thread(name='Access granted email thread',
+                             target=send_mail_access_granted,
+                             args=(mail_to, context,))
+        email_send_thread.start()
+        success = True
+    return success
 
 
 @convert_kwargs_to_snake_case
@@ -106,3 +176,47 @@ def resolve_add_user(_, info, **parameters):
         )
         email_send_thread.start()
     return dict(success=success, email=parameters.get('email', '').lower())
+
+
+@convert_kwargs_to_snake_case
+def resolve_grant_user_access(_, info, **query_args):
+    """Resolve grant_user_access mutation."""
+    project_name = query_args.get('project_name')
+    success = False
+    user_data = util.get_jwt_content(info.context)
+    role = get_user_role(user_data)
+    new_user_data = {
+        'email': query_args.get('email'),
+        'organization': query_args.get('organization'),
+        'responsibility': query_args.get('responsibility', '-'),
+        'role': query_args.get('role'),
+        'phone_number': query_args.get('phone_number', '')
+    }
+
+    if (role == 'admin'
+            and new_user_data['role'] in ['admin', 'analyst', 'customer', 'customeradmin']) \
+        or (is_customeradmin(project_name, user_data['user_email'])
+            and new_user_data['role'] in ['customer', 'customeradmin']):
+        if _create_new_user(info.context, new_user_data, project_name):
+            success = True
+        else:
+            rollbar.report_message('Error: Couldn\'t grant access to project',
+                                   'error', info.context)
+    else:
+        rollbar.report_message('Error: Invalid role provided: ' +
+                               new_user_data['role'], 'error', info.context)
+    if success:
+        util.invalidate_cache(project_name)
+        util.invalidate_cache(query_args.get('email'))
+        util.cloudwatch_log(info.context, 'Security: Given grant access to {user} \
+            in {project} project'.format(user=query_args.get('email'), project=project_name))
+    else:
+        util.cloudwatch_log(info.context, 'Security: Attempted to give grant \
+            access to {user} in {project} project'.format(user=query_args.get('email'),
+                                                          project=project_name))
+    return dict(
+        success=success,
+        granted_user=dict(
+            project_name=project_name,
+            email=new_user_data['email'])
+    )
--- test_async/entity_test/test_user.py
+++ test_async/entity_test/test_user.py
@@ -16,7 +16,7 @@ class UserTests(TestCase):
                 user(projectName: "unittesting",
                      userEmail: "continuoushacking@gmail.com") {
                     organization
-                    responsability
+                    responsibility
                     phoneNumber
                 }
             }
@@ -42,7 +42,7 @@ class UserTests(TestCase):
         assert 'errors' not in result
         assert 'user' in result['data']
         assert 'organization' in result['data']['user']
-        assert 'responsability' in result['data']['user']
+        assert 'responsibility' in result['data']['user']
         assert 'phoneNumber' in result['data']['user']
 
     def test_add_user(self):
@@ -80,3 +80,51 @@ class UserTests(TestCase):
         assert 'addUser' in result['data']
         assert 'success' in result['data']['addUser']
         assert 'email' in result['data']['addUser']
+
+    def test_grant_user_access(self):
+        """Check for grantUserAccess mutation."""
+        query = '''
+            mutation {
+                grantUserAccess (
+                email: "test@test.test",
+                organization: "test",
+                phoneNumber: "3453453453"
+                projectName: "unittesting",
+                responsibility: "test",
+                role: "customer") {
+                success
+                grantedUser {
+                    email
+                    role
+                    responsibility
+                    phoneNumber
+                    organization
+                    firstLogin
+                    lastLogin
+                }
+                }
+            }
+        '''
+        data = {'query': query}
+        request = RequestFactory().get('/')
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+        request.session['username'] = 'unittest'
+        request.session['company'] = 'unittest'
+        request.session['role'] = 'admin'
+        request.COOKIES[settings.JWT_COOKIE_NAME] = jwt.encode(
+            {
+                'username': 'unittest',
+                'company': 'unittest',
+                'role': 'admin',
+                'user_email': 'unittest'
+            },
+            algorithm='HS512',
+            key=settings.JWT_SECRET,
+        )
+        _, result = graphql_sync(SCHEMA, data, context_value=request)
+        assert 'errors' not in result
+        assert 'success' in result['data']['grantUserAccess']
+        assert 'grantedUser' in result['data']['grantUserAccess']
+        assert 'email' in result['data']['grantUserAccess']['grantedUser']
