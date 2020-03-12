9073ca5efb721bc3d95db05dc05f60d5a6cde0f3
Kevin Amado <kamado@fluidattacks.com>
2020-03-12 08:59:07 -0500

    feat(front): #1403 rename draft to finding
    - Due to the demo phase on the Breaking Build talk we are changing
      this translation to show how changes are being integrated and
      automatically deployed to production

--- front/src/utils/translations/en.ts
+++ front/src/utils/translations/en.ts
@@ -105,7 +105,7 @@ const enTranslations: Dictionary = {
     drafts: {
       approve: "Approve draft",
       error_submit: "Please provide {{missingFields}} before submitting",
-      new: "New Draft",
+      new: "New Finding",
       reject: "Reject Draft",
       success_create: "Draft created successfully",
       success_submit: "Draft successfully submitted for review",
