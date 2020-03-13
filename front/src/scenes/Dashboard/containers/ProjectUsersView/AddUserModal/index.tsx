50154cd39dd6f877c99bc1a342e01c1072cf1a95
David Acevedo <dacevedo@fluidattacks.com>
2020-03-13 17:53:51 -0500

    style(front): #1839.9 indent code

--- front/src/scenes/Dashboard/containers/ProjectUsersView/AddUserModal/index.tsx
+++ front/src/scenes/Dashboard/containers/ProjectUsersView/AddUserModal/index.tsx
@@ -37,13 +37,13 @@ const renderAllRoles: JSX.Element = (
   </React.Fragment>
 );
 
-const requiredIndicator: JSX.Element = <label style={{ color: "#f22"}}>* </label>;
+const requiredIndicator: JSX.Element = <label style={{ color: "#f22" }}>* </label>;
 
 export const addUserModal: React.FC<IAddUserModalProps> = (props: IAddUserModalProps): JSX.Element => {
   const { onClose, onSubmit } = props;
   let title: string = props.type === "add"
-  ? translate.t("search_findings.tab_users.title")
-  : translate.t("search_findings.tab_users.edit_user_title");
+    ? translate.t("search_findings.tab_users.title")
+    : translate.t("search_findings.tab_users.edit_user_title");
   title = props.projectName === undefined ? translate.t("sidebar.user") : title;
 
   const selector: (state: {}, ...field: string[]) => string = formValueSelector("addUser");
@@ -54,99 +54,99 @@ export const addUserModal: React.FC<IAddUserModalProps> = (props: IAddUserModalP
   return (
     <React.StrictMode>
       <Modal open={props.open} headerTitle={title} footer={<div />}>
-      <GenericForm name="addUser" initialValues={props.initialValues} onSubmit={onSubmit}>
-        {({ change }: InjectedFormProps): React.ReactNode => {
-    const loadAutofillData: (() => void) = (): void => {
-            if (!_.isEmpty(userEmail)) {
-              client.query({
-                query: GET_USER,
-                variables: {
-                  projectName: _.get(props, "projectName", "-"),
-                  userEmail,
-                },
-              })
-      .then(({ data, errors }: ApolloQueryResult<IUserDataAttr>) => {
+        <GenericForm name="addUser" initialValues={props.initialValues} onSubmit={onSubmit}>
+          {({ change }: InjectedFormProps): React.ReactNode => {
+            const loadAutofillData: (() => void) = (): void => {
+              if (!_.isEmpty(userEmail)) {
+                client.query({
+                  query: GET_USER,
+                  variables: {
+                    projectName: _.get(props, "projectName", "-"),
+                    userEmail,
+                  },
+                })
+                  .then(({ data, errors }: ApolloQueryResult<IUserDataAttr>) => {
 
-        if (!_.isUndefined(errors)) {
-          handleErrors("An error occurred getting user information for autofill", errors);
-        }
-        if (!_.isUndefined(data)) {
-          change("organization", data.user.organization);
-          change("phoneNumber", data.user.phoneNumber);
-          change("responsibility", data.user.responsibility);
-        }
-      })
-      .catch((errors: ApolloError) => {
-        errors.graphQLErrors.forEach(({ message }: GraphQLError): void => {
-          if (message !== "Exception - User not Found") {
-            msgError("An error occurred getting user information for autofill");
-          }
-        });
-      });
-    }
-  };
+                    if (!_.isUndefined(errors)) {
+                      handleErrors("An error occurred getting user information for autofill", errors);
+                    }
+                    if (!_.isUndefined(data)) {
+                      change("organization", data.user.organization);
+                      change("phoneNumber", data.user.phoneNumber);
+                      change("responsibility", data.user.responsibility);
+                    }
+                  })
+                  .catch((errors: ApolloError) => {
+                    errors.graphQLErrors.forEach(({ message }: GraphQLError): void => {
+                      if (message !== "Exception - User not Found") {
+                        msgError("An error occurred getting user information for autofill");
+                      }
+                    });
+                  });
+              }
+            };
 
-    return (
-        <Row>
-          <Col md={12} sm={12}>
-          <FormGroup>
-            <ControlLabel>{requiredIndicator}{translate.t("search_findings.tab_users.textbox")}</ControlLabel>
-                <Field
-                  name="email"
-                  component={textField}
-                  type="text"
-                  placeholder={translate.t("search_findings.tab_users.email")}
-                  validate={[required, validEmail]}
-                  disabled={props.type === "edit"}
-                  onBlur={loadAutofillData}
-                />
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>
-              {requiredIndicator}
-              {translate.t("search_findings.tab_users.user_organization")}
-            </ControlLabel>
-            <Field name="organization" component={textField} type="text" validate={[required]}/>
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>{requiredIndicator}{translate.t("search_findings.tab_users.role")}</ControlLabel>
-            <Field name="role" component={dropdownField} validate={[required]}>
-              <option value="" selected={true}/>
-              {props.userRole === "admin" ? renderAllRoles : undefined}
-              {props.userRole === "customeradmin" ? renderManagerRoles : undefined}
-            </Field>
-          </FormGroup>
-          {props.projectName !== undefined ?
-            <FormGroup>
-              <ControlLabel>
-                {requiredIndicator}
-                {translate.t("search_findings.tab_users.user_responsibility")}
-              </ControlLabel>
-              <Field
-                name="responsibility"
-                component={textField}
-                type="text"
-                placeholder={translate.t("search_findings.tab_users.responsibility_placeholder")}
-                validate={[required]}
-              />
-            </FormGroup>
-          : undefined}
-          <FormGroup>
-            <ControlLabel>{translate.t("search_findings.tab_users.phone_number")}</ControlLabel>
-            <Field name="phoneNumber" component={phoneNumberField} type="text"/>
-          </FormGroup>
-          </Col>
-          <Col md={12} sm={12}>
-            <ButtonToolbar className="pull-right">
-              <Button bsStyle="default" onClick={onClose}>{translate.t("confirmmodal.cancel")}</Button>
-              <Button bsStyle="primary" type="submit">{translate.t("confirmmodal.proceed")}</Button>
-            </ButtonToolbar>
-          </Col>
-        </Row>
-        );
-        }}
-      </GenericForm>
+            return (
+              <Row>
+                <Col md={12} sm={12}>
+                  <FormGroup>
+                    <ControlLabel>{requiredIndicator}{translate.t("search_findings.tab_users.textbox")}</ControlLabel>
+                    <Field
+                      name="email"
+                      component={textField}
+                      type="text"
+                      placeholder={translate.t("search_findings.tab_users.email")}
+                      validate={[required, validEmail]}
+                      disabled={props.type === "edit"}
+                      onBlur={loadAutofillData}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>
+                      {requiredIndicator}
+                      {translate.t("search_findings.tab_users.user_organization")}
+                    </ControlLabel>
+                    <Field name="organization" component={textField} type="text" validate={[required]} />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>{requiredIndicator}{translate.t("search_findings.tab_users.role")}</ControlLabel>
+                    <Field name="role" component={dropdownField} validate={[required]}>
+                      <option value="" selected={true} />
+                      {props.userRole === "admin" ? renderAllRoles : undefined}
+                      {props.userRole === "customeradmin" ? renderManagerRoles : undefined}
+                    </Field>
+                  </FormGroup>
+                  {props.projectName !== undefined ? (
+                    <FormGroup>
+                      <ControlLabel>
+                        {requiredIndicator}
+                        {translate.t("search_findings.tab_users.user_responsibility")}
+                      </ControlLabel>
+                      <Field
+                        name="responsibility"
+                        component={textField}
+                        type="text"
+                        placeholder={translate.t("search_findings.tab_users.responsibility_placeholder")}
+                        validate={[required]}
+                      />
+                    </FormGroup>
+                  ) : undefined}
+                  <FormGroup>
+                    <ControlLabel>{translate.t("search_findings.tab_users.phone_number")}</ControlLabel>
+                    <Field name="phoneNumber" component={phoneNumberField} type="text" />
+                  </FormGroup>
+                </Col>
+                <Col md={12} sm={12}>
+                  <ButtonToolbar className="pull-right">
+                    <Button bsStyle="default" onClick={onClose}>{translate.t("confirmmodal.cancel")}</Button>
+                    <Button bsStyle="primary" type="submit">{translate.t("confirmmodal.proceed")}</Button>
+                  </ButtonToolbar>
+                </Col>
+              </Row>
+            );
+          }}
+        </GenericForm>
       </Modal>
     </React.StrictMode>
   );
-  };
+};
