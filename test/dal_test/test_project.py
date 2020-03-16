81b07f21ee8e548df16d7e672880f80ead12a6ed
Mario Varas <mvaras@fluidattacks.com>
2020-03-16 14:07:25 -0500

    test(back): #1836 enable test

--- test/dal_test/test_project.py
+++ test/dal_test/test_project.py
@@ -8,7 +8,6 @@ from backend.dal.project import (
 
 class ProjectTests(TestCase):
 
-    @pytest.mark.skip(reason="https://gitlab.com/fluidattacks/integrates/-/jobs/469890062")
     def test_get_current_month_information(self):
         """makes sure that we are getting the info properly"""
         project_name = 'unittesting'
