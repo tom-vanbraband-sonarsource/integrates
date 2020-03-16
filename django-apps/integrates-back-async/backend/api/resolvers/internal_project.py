6a91b420c3bbe6f242ce52c5d75c45a3c9a9bd1f
Andres Roldan <aroldan@fluidattacks.com>
2020-03-16 15:30:32 -0500

    feat(cross): #1635 migrated operation
    - Migrated InternalProjectName operation

--- django-apps/integrates-back-async/backend/api/resolvers/internal_project.py
+++ django-apps/integrates-back-async/backend/api/resolvers/internal_project.py
@@ -8,4 +8,5 @@ from ariadne import convert_kwargs_to_snake_case
 @convert_kwargs_to_snake_case
 def resolve_project_name(*_):
     """Resolve internalProjectNames query."""
-    return internal_project_domain.get_project_name()
+    name = internal_project_domain.get_project_name()
+    return dict(project_name=name)
