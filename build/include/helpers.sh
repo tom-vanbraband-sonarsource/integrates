741a570fa5cc759874902dedaac53eb7047f28e1
Andres Roldan <aroldan@fluidattacks.com>
2020-03-17 17:55:02 -0500

    fix(build): #1822 fix desync of apiv2 container

--- build/include/helpers.sh
+++ build/include/helpers.sh
@@ -84,56 +84,6 @@ function helper_docker_build_and_push {
   &&  docker image remove "${tag}"
 }
 
-function helper_docker_build_and_push_no_cache {
-  local tag="${1}"
-  local context="${2}"
-  local dockerfile="${3}"
-  local build_arg_1_key="${4:-build_arg_1_key}"
-  local build_arg_1_val="${5:-build_arg_1_val}"
-  local build_arg_2_key="${6:-build_arg_2_key}"
-  local build_arg_2_val="${7:-build_arg_2_val}"
-  local build_arg_3_key="${8:-build_arg_3_key}"
-  local build_arg_3_val="${9:-build_arg_3_val}"
-  local build_arg_4_key="${10:-build_arg_4_key}"
-  local build_arg_4_val="${11:-build_arg_4_val}"
-  local build_arg_5_key="${12:-build_arg_5_key}"
-  local build_arg_5_val="${13:-build_arg_5_val}"
-  local build_arg_6_key="${14:-build_arg_6_key}"
-  local build_arg_6_val="${15:-build_arg_6_val}"
-  local build_arg_7_key="${16:-build_arg_7_key}"
-  local build_arg_7_val="${17:-build_arg_7_val}"
-  local build_arg_8_key="${18:-build_arg_8_key}"
-  local build_arg_8_val="${19:-build_arg_8_val}"
-  local build_arg_9_key="${20:-build_arg_9_key}"
-  local build_arg_9_val="${21:-build_arg_9_val}"
-  local build_args=(
-    --tag "${tag}"
-    --file "${dockerfile}"
-    --build-arg "${build_arg_1_key}=${build_arg_1_val}"
-    --build-arg "${build_arg_2_key}=${build_arg_2_val}"
-    --build-arg "${build_arg_3_key}=${build_arg_3_val}"
-    --build-arg "${build_arg_4_key}=${build_arg_4_val}"
-    --build-arg "${build_arg_5_key}=${build_arg_5_val}"
-    --build-arg "${build_arg_6_key}=${build_arg_6_val}"
-    --build-arg "${build_arg_7_key}=${build_arg_7_val}"
-    --build-arg "${build_arg_8_key}=${build_arg_8_val}"
-    --build-arg "${build_arg_9_key}=${build_arg_9_val}"
-  )
-
-      echo "[INFO] Logging into: ${CI_REGISTRY}" \
-  &&  docker login \
-        --username "${CI_REGISTRY_USER}" \
-        --password "${CI_REGISTRY_PASSWORD}" \
-      "${CI_REGISTRY}" \
-  &&  echo "[INFO] Pulling: ${tag}" \
-  &&  echo "[INFO] Building: ${tag}" \
-  &&  docker build --no-cache "${build_args[@]}" "${context}" \
-  &&  echo "[INFO] Pushing: ${tag}" \
-  &&  docker push "${tag}" \
-  &&  echo "[INFO] Deleting local copy of: ${tag}" \
-  &&  docker image remove "${tag}"
-}
-
 function helper_get_gitlab_var {
   local gitlab_var_name="${1}"
       echo "[INFO] Retrieving var from GitLab: ${gitlab_var_name}" 1>&2 \
